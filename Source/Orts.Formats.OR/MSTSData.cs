6b325d74d0c8ae696274e2e603466059896c874b
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2015-09-01 20:48:16 +0000

    https://blueprints.launchpad.net/or/+spec/separate-formats Move all the LibAE cruft needed by the RouteConfigurationFile (it's a lot!).
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3236 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Source/Orts.Formats.OR/MSTSData.cs
@@ -0,0 +1,54 @@
+﻿// COPYRIGHT 2014, 2015 by the Open Rails project.
+// 
+// This file is part of Open Rails.
+// 
+// Open Rails is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Open Rails is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Open Rails.  If not, see <http://www.gnu.org/licenses/>.
+
+using Orts.Formats.Msts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Orts.Formats.OR
+{
+    public class MSTSData
+    {
+        public TrackDatabaseFile TDB { get; protected set; }
+        public RouteFile TRK { get; protected set; }
+        public TrackSectionsFile TSectionDat { get; protected set; }
+        public SignalConfigurationFile SIGCFG { get; protected set; }
+        public string RoutePath { get; set; }
+        public string MstsPath { get; set; }
+        public AESignals Signals { get; protected set; }
+
+        public MSTSData (string mstsPath, string Route)
+        {
+            MstsPath = mstsPath;
+            RoutePath = Route;
+            TRK = new RouteFile(MSTS.MSTSPath.GetTRKFileName(RoutePath));
+            string routePath = Path.Combine(Route, TRK.Tr_RouteFile.FileName);
+            TDB = new TrackDatabaseFile(RoutePath + @"\" + TRK.Tr_RouteFile.FileName + ".tdb");
+            SIGCFG = new SignalConfigurationFile(RoutePath + @"\sigcfg.dat");
+            if (Directory.Exists(MstsPath + @"\GLOBAL") && File.Exists(MstsPath + @"\GLOBAL\TSECTION.DAT"))
+                TSectionDat = new TrackSectionsFile(MstsPath + @"\GLOBAL\TSECTION.DAT");
+            else
+                TSectionDat = new TrackSectionsFile(RoutePath + @"\GLOBAL\TSECTION.DAT");
+            if (File.Exists(RoutePath + @"\TSECTION.DAT"))
+                TSectionDat.AddRouteTSectionDatFile(RoutePath + @"\TSECTION.DAT");
+            Signals = new AESignals (this, SIGCFG);
+        }
+    }
+}
