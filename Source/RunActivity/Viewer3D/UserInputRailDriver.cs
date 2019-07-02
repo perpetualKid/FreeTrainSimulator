2481b528fda19fc48c5a8db2e4c1641a5de4cba0
Csantucci <carlosanit1@gmail.com>
2019-07-02 19:37:31 +0200

    Dividing RailDriverBase from UserInputRailDriver, not working at 32 bit.

--- Source/RunActivity/Viewer3D/UserInputRailDriver.cs
+++ Source/RunActivity/Viewer3D/UserInputRailDriver.cs
@@ -23,150 +23,11 @@ using Orts.Parsers.Msts;
 using Orts.Simulation.RollingStocks;
 using ORTS.Common;
 using ORTS.Common.Input;
-using PIEHid32Net;
-using PIEHid64Net;
+using Orts.ExternalDevices;
+using Orts.Viewer3D;
 
 namespace Orts.Viewer3D
 {
-    internal delegate void RailDriverDataRead(byte[] data, RailDriverBase sourceDevice);
-
-    internal abstract class RailDriverBase
-    {
-        private static RailDriverBase instance;
-
-        public static RailDriverBase GetInstance()
-        {
-            if (null == instance)
-            {
-                if (Environment.Is64BitProcess)
-                {
-                    instance = new RailDriver64();
-                }
-                else
-                {
-                    instance = new RailDriver32();
-                }
-            }
-            return instance;
-        }
-
-        public abstract int WriteBufferSize { get; }
-
-        public abstract int WriteData(byte[] writeBuffer);
-
-        public abstract int ReadData(ref byte[] readBuffer);
-
-        public abstract event RailDriverDataRead OnDataRead;
-
-    }
-
-    internal class RailDriver32 : RailDriverBase, PIEHid32Net.PIEDataHandler, PIEHid32Net.PIEErrorHandler
-    {
-        private readonly PIEHid32Net.PIEDevice device;                   // Our RailDriver
-
-        public override event RailDriverDataRead OnDataRead;
-
-        public RailDriver32()
-        {
-            try
-            {
-                foreach (PIEHid32Net.PIEDevice currentDevice in PIEHid32Net.PIEDevice.EnumeratePIE())
-                {
-                    if (currentDevice.HidUsagePage == 0xc && currentDevice.Pid == 210)
-                    {
-                        device = currentDevice;
-                        device.SetupInterface();
-                        device.SetErrorCallback(this);
-                        device.SetDataCallback(this);
-                        device.suppressDuplicateReports = true;
-                        break;
-                    }
-                }
-            }
-            catch (Exception error)
-            {
-                device = null;
-                Trace.WriteLine(error);
-            }
-        }
-
-        public void HandlePIEHidData(byte[] data, PIEHid32Net.PIEDevice sourceDevice, int error)
-        {
-            OnDataRead?.Invoke(data, this);
-        }
-
-        public void HandlePIEHidError(PIEHid32Net.PIEDevice sourceDevices, int error)
-        {
-            Trace.TraceWarning("RailDriver Error: {0}", error);
-        }
-
-        public override int WriteBufferSize => device?.WriteLength ?? 0;
-
-        public override int WriteData(byte[] writeBuffer)
-        {
-            return device?.WriteData(writeBuffer) ?? -1;
-        }
-
-        public override int ReadData(ref byte[] readBuffer)
-        {
-            return device?.ReadData(ref readBuffer) ?? -1;
-        }
-    }
-
-    internal class RailDriver64 : RailDriverBase, PIEHid64Net.PIEDataHandler, PIEHid64Net.PIEErrorHandler
-    {
-        private readonly PIEHid64Net.PIEDevice device;                   // Our RailDriver
-
-        public override event RailDriverDataRead OnDataRead;
-
-        public RailDriver64()
-        {
-            try
-            {
-                foreach (PIEHid64Net.PIEDevice currentDevice in PIEHid64Net.PIEDevice.EnumeratePIE())
-                {
-                    if (currentDevice.HidUsagePage == 0xc && currentDevice.Pid == 210)
-                    {
-                        device = currentDevice;
-                        device.SetupInterface();
-                        device.SetErrorCallback(this);
-                        device.SetDataCallback(this);
-                        device.suppressDuplicateReports = true;
-                        break;
-                    }
-                }
-            }
-            catch (Exception error)
-            {
-                device = null;
-                Trace.WriteLine(error);
-            }
-        }
-
-        public void HandlePIEHidData(byte[] data, PIEHid64Net.PIEDevice sourceDevice, int error)
-        {
-            OnDataRead?.Invoke(data, this);
-        }
-
-        public void HandlePIEHidError(PIEHid64Net.PIEDevice sourceDevices, long error)
-        {
-            Trace.TraceWarning("RailDriver Error: {0}", error);
-        }
-
-        public override int WriteBufferSize => (int)(device?.WriteLength ?? 0);
-
-        public override int WriteData(byte[] writeBuffer)
-        {
-            return device?.WriteData(writeBuffer) ?? -1;
-        }
-
-        public override int ReadData(ref byte[] readBuffer)
-        {
-            return device?.ReadData(ref readBuffer) ?? -1;
-        }
-    }
-
-
     /// <summary>
     /// Class to get data from RailDriver and translate it into something useful for UserInput
     /// </summary>
@@ -324,7 +185,7 @@ namespace Orts.Viewer3D
             WriteBuffer[2] = led1;
             WriteBuffer[3] = led2;
             WriteBuffer[4] = led3;
-            Device.WriteData(WriteBuffer);
+            Device?.WriteData(WriteBuffer);
         }
 
         /// <summary>
