90fa61c6bf7b1387b85626a21ceee3678811e352
perpetualKid <perpetualKid@azuros.net>
2019-09-11 20:00:17 +0200

    Updating Test Adapter packages

--- Source/ActivityRunner/Viewer3D/Shapes/FixedWorldPositionSource.cs
+++ Source/ActivityRunner/Viewer3D/Shapes/FixedWorldPositionSource.cs
@@ -5,7 +5,7 @@ namespace Orts.ActivityRunner.Viewer3D.Shapes
     /// <summary>
     /// FixedWorldPositionSource uses a readonly (fixed) WorldPosition to act as IWorldPosition source
     /// Used to simplify cases expecting an IWorldPosition source (for moveable content), but only have a const WorldPosition, 
-    /// i.e. Animated Scenary objects which are static in position but use <cref>AnimatedShape</cref>
+    /// i.e. Animated Scenary objects which are static in position but use <see cref="AnimatedShape">
     /// </summary>
     public class FixedWorldPositionSource : IWorldPosition
     {
