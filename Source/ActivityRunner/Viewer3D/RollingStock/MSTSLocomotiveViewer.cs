openrails/openrails

https://blueprints.launchpad.net/or/+spec/vacuum-brake-update - added large ejector functionality, and some extra brake tokens to simulate different brake control valves

Source/RunActivity/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -1875,6 +1875,7 @@ namespace Orts.Viewer3D.RollingStock
                 case CABViewControlTypes.EMERGENCY_BRAKE:
                 case CABViewControlTypes.DOORS_DISPLAY:
                 case CABViewControlTypes.CYL_COCKS:
+                case CABViewControlTypes.ORTS_LARGE_EJECTOR:
                 case CABViewControlTypes.ORTS_CYL_COMP:
                 case CABViewControlTypes.STEAM_INJ1:
                 case CABViewControlTypes.STEAM_INJ2:
@@ -1992,6 +1993,7 @@ namespace Orts.Viewer3D.RollingStock
                 case CABViewControlTypes.STEAM_INJ1: if (((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 1); break;
                 case CABViewControlTypes.STEAM_INJ2: if (((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 2); break;
                 case CABViewControlTypes.SMALL_EJECTOR: (Locomotive as MSTSSteamLocomotive).SetSmallEjectorValue(ChangedValue((Locomotive as MSTSSteamLocomotive).SmallEjectorController.IntermediateValue)); break;
+                case CABViewControlTypes.ORTS_LARGE_EJECTOR: if (((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0)) new ToggleLargeEjectorCommand(Viewer.Log); break;
                 //
                 case CABViewControlTypes.CAB_RADIO: new CabRadioCommand(Viewer.Log, ChangedValue(Locomotive.CabRadioOn ? 1 : 0) > 0); break;
                 case CABViewControlTypes.WIPERS: new WipersCommand(Viewer.Log, ChangedValue(Locomotive.Wiper ? 1 : 0) > 0); break;

Source/ORTS.Common/Input/UserCommand.cs
@@ -176,6 +176,7 @@
         [GetString("Control Firing Rate Decrease")] ControlFiringRateDecrease,
         [GetString("Control Fire Shovel Full")] ControlFireShovelFull,
         [GetString("Control Cylinder Cocks")] ControlCylinderCocks,
+        [GetString("Control Large Ejector")] ControlLargeEjector,
         [GetString("Control Small Ejector Increase")] ControlSmallEjectorIncrease,
         [GetString("Control Small Ejector Decrease")] ControlSmallEjectorDecrease,
         [GetString("Control Cylinder Compound")] ControlCylinderCompound,

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -59,6 +59,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "epapplystart": Type = ControllerState.EPApply; break;
                 case "epholdstart": Type = ControllerState.SelfLap; break;
                 case "vacuumcontinuousservicestart": Type = ControllerState.VacContServ; break;
+                case "vacuumapplycontinuousservicestart": Type = ControllerState.VacApplyContServ; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;

perpetualKid/FreeTrainSimulator

(cherry picked from commit 6c091dd04516640404920b4ddc2fe7dff4989ddb)

Source/ActivityRunner/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -1835,6 +1835,7 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
                 case CabViewControlType.Doors_Display:
                 case CabViewControlType.Cyl_Cocks:
                 case CabViewControlType.Orts_Cyl_Comp:
+                case CabViewControlType.Orts_Large_Ejector:
                 case CabViewControlType.Steam_Inj1:
                 case CabViewControlType.Steam_Inj2:
                 case CabViewControlType.Small_Ejector:
@@ -1953,6 +1954,7 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
                 case CabViewControlType.Steam_Inj1: if (((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 1); break;
                 case CabViewControlType.Steam_Inj2: if (((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 2); break;
                 case CabViewControlType.Small_Ejector: (Locomotive as MSTSSteamLocomotive).SetSmallEjectorValue(ChangedValue((Locomotive as MSTSSteamLocomotive).SmallEjectorController.IntermediateValue)); break;
+                case CabViewControlType.Orts_Large_Ejector: if (((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0)) new ToggleLargeEjectorCommand(Viewer.Log); break;
                 //
                 case CabViewControlType.Cab_Radio: new CabRadioCommand(Viewer.Log, ChangedValue(Locomotive.CabRadioOn ? 1 : 0) > 0); break;
                 case CabViewControlType.Wipers: new WipersCommand(Viewer.Log, ChangedValue(Locomotive.Wiper ? 1 : 0) > 0); break;

Source/Orts.Common/Input/UserCommand.cs
@@ -182,6 +182,7 @@ namespace Orts.Common.Input
         [Description("Control Firing Rate Decrease")] ControlFiringRateDecrease,
         [Description("Control Fire Shovel Full")] ControlFireShovelFull,
         [Description("Control Cylinder Cocks")] ControlCylinderCocks,
+        [Description("Control Large Ejector")] ControlLargeEjector,
         [Description("Control Small Ejector Increase")] ControlSmallEjectorIncrease,
         [Description("Control Small Ejector Decrease")] ControlSmallEjectorDecrease,
         [Description("Control Cylinder Compound")] ControlCylinderCompound,

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -63,6 +63,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "epapplystart": NotchStateType = ControllerState.EPApply; break;
                 case "epholdstart": NotchStateType = ControllerState.SelfLap; break;
                 case "vacuumcontinuousservicestart": NotchStateType = ControllerState.VacContServ; break;
+                case "vacuumapplycontinuousservicestart": NotchStateType = ControllerState.VacApplyContServ; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;