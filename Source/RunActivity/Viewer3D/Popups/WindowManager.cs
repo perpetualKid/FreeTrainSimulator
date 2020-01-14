9042da93546b23eb5cbcb5e748df20f519837187
Csantucci <carlosanit1@gmail.com>
2020-01-14 14:45:43 +0100

    Mouse wheel scroll and scrollbars dragging, by Reve

--- Source/RunActivity/Viewer3D/Popups/WindowManager.cs
+++ Source/RunActivity/Viewer3D/Popups/WindowManager.cs
@@ -293,7 +293,15 @@ namespace Orts.Viewer3D.Popups
                     BringWindowToTop(mouseActiveWindow);
 			}
 
-			if (mouseActiveWindow != null)
+            if (UserInput.IsMouseWheelChanged)
+            {
+                mouseActiveWindow = VisibleWindows.LastOrDefault(w => w.Interactive && w.Location.Contains(mouseDownPosition));
+                
+                if (mouseActiveWindow != null)
+                    mouseActiveWindow.HandleUserInput();
+            }
+
+            if (mouseActiveWindow != null)
 			{
 				if (UserInput.IsMouseLeftButtonPressed)
 					mouseActiveWindow.MouseDown();
@@ -303,7 +311,7 @@ namespace Orts.Viewer3D.Popups
 				if (UserInput.IsMouseMoved)
 					mouseActiveWindow.MouseMove();
 
-				if (Viewer.RealTime - LastUpdateRealTime >= 0.1)
+                if (Viewer.RealTime - LastUpdateRealTime >= 0.1)
 				{
 					LastUpdateRealTime = Viewer.RealTime;
 					mouseActiveWindow.HandleUserInput();
@@ -311,7 +319,7 @@ namespace Orts.Viewer3D.Popups
 
 				if (UserInput.IsMouseLeftButtonReleased)
 					mouseActiveWindow = null;
-			}
+            }
 		}
 
         public void BringWindowToTop(Window mouseActiveWindow)
