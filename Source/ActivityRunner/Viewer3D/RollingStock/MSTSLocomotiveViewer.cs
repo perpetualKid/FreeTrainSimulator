openrails/openrails

Incorporate working Large Ejector and switch for Simple/Advanced Control/Physics

Source/RunActivity/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -1996,7 +1996,7 @@ namespace Orts.Viewer3D.RollingStock
                 case CABViewControlTypes.STEAM_INJ1: if (((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 1); break;
                 case CABViewControlTypes.STEAM_INJ2: if (((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 2); break;
                 case CABViewControlTypes.SMALL_EJECTOR: (Locomotive as MSTSSteamLocomotive).SetSmallEjectorValue(ChangedValue((Locomotive as MSTSSteamLocomotive).SmallEjectorController.IntermediateValue)); break;
-                case CABViewControlTypes.ORTS_LARGE_EJECTOR: if (((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0)) new ToggleLargeEjectorCommand(Viewer.Log); break;
+                case CABViewControlTypes.ORTS_LARGE_EJECTOR: (Locomotive as MSTSSteamLocomotive).SetLargeEjectorValue(ChangedValue((Locomotive as MSTSSteamLocomotive).LargeEjectorController.IntermediateValue)); break;
                 //
                 case CABViewControlTypes.CAB_RADIO: new CabRadioCommand(Viewer.Log, ChangedValue(Locomotive.CabRadioOn ? 1 : 0) > 0); break;
                 case CABViewControlTypes.WIPERS: new WipersCommand(Viewer.Log, ChangedValue(Locomotive.Wiper ? 1 : 0) > 0); break;

Source/ORTS.Common/Input/UserCommand.cs
@@ -176,7 +176,8 @@
         [GetString("Control Firing Rate Decrease")] ControlFiringRateDecrease,
         [GetString("Control Fire Shovel Full")] ControlFireShovelFull,
         [GetString("Control Cylinder Cocks")] ControlCylinderCocks,
-        [GetString("Control Large Ejector")] ControlLargeEjector,
+        [GetString("Control Large Ejector Increase")] ControlLargeEjectorIncrease,
+        [GetString("Control Large Ejector Decrease")] ControlLargeEjectorDecrease,
         [GetString("Control Small Ejector Increase")] ControlSmallEjectorIncrease,
         [GetString("Control Small Ejector Decrease")] ControlSmallEjectorDecrease,
         [GetString("Control Cylinder Compound")] ControlCylinderCompound,

perpetualKid/FreeTrainSimulator

(cherry picked from commit 5625dddccba2ae130a9ab3403c7e157f3273f7f5)

Source/ActivityRunner/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -1954,7 +1954,7 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
                 case CabViewControlType.Steam_Inj1: if (((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector1IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 1); break;
                 case CabViewControlType.Steam_Inj2: if (((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).Injector2IsOn ? 1 : 0)) new ToggleInjectorCommand(Viewer.Log, 2); break;
                 case CabViewControlType.Small_Ejector: (Locomotive as MSTSSteamLocomotive).SetSmallEjectorValue(ChangedValue((Locomotive as MSTSSteamLocomotive).SmallEjectorController.IntermediateValue)); break;
-                case CabViewControlType.Orts_Large_Ejector: if (((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0) != ChangedValue((Locomotive as MSTSSteamLocomotive).LargeEjectorEnabled ? 1 : 0)) new ToggleLargeEjectorCommand(Viewer.Log); break;
+                case CabViewControlType.Orts_Large_Ejector: (Locomotive as MSTSSteamLocomotive).SetLargeEjectorValue(ChangedValue((Locomotive as MSTSSteamLocomotive).LargeEjectorController.IntermediateValue)); break;
                 //
                 case CabViewControlType.Cab_Radio: new CabRadioCommand(Viewer.Log, ChangedValue(Locomotive.CabRadioOn ? 1 : 0) > 0); break;
                 case CabViewControlType.Wipers: new WipersCommand(Viewer.Log, ChangedValue(Locomotive.Wiper ? 1 : 0) > 0); break;

Source/Orts.Common/Input/UserCommand.cs
@@ -182,7 +182,8 @@ namespace Orts.Common.Input
         [Description("Control Firing Rate Decrease")] ControlFiringRateDecrease,
         [Description("Control Fire Shovel Full")] ControlFireShovelFull,
         [Description("Control Cylinder Cocks")] ControlCylinderCocks,
-        [Description("Control Large Ejector")] ControlLargeEjector,
+        [Description("Control Large Ejector Increase")] ControlLargeEjectorIncrease,
+        [Description("Control Large Ejector Decrease")] ControlLargeEjectorDecrease,
         [Description("Control Small Ejector Increase")] ControlSmallEjectorIncrease,
         [Description("Control Small Ejector Decrease")] ControlSmallEjectorDecrease,
         [Description("Control Cylinder Compound")] ControlCylinderCompound,