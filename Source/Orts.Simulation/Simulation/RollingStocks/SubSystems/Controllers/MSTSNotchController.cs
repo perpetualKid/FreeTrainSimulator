openrails/openrails

Adjusted Engine Brake to conform with BR standards, and added new engine brake notch

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -60,6 +60,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "epholdstart": Type = ControllerState.SelfLap; break;
                 case "vacuumcontinuousservicestart": Type = ControllerState.VacContServ; break;
                 case "vacuumapplycontinuousservicestart": Type = ControllerState.VacApplyContServ; break;
+                case "brakenotchstart": Type = ControllerState.BrakeNotch; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;

perpetualKid/FreeTrainSimulator

(cherry picked from commit aa54d7e2dca9b3dc398a755844831d1219c06e4b)

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -64,6 +64,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "epholdstart": NotchStateType = ControllerState.SelfLap; break;
                 case "vacuumcontinuousservicestart": NotchStateType = ControllerState.VacContServ; break;
                 case "vacuumapplycontinuousservicestart": NotchStateType = ControllerState.VacApplyContServ; break;
+                case "brakenotchstart": NotchStateType = ControllerState.BrakeNotch; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;