40d59eda5aec64eee8abdba634a99bbdd66ea97c
Ryan Young <ryan@youngryan.com>
2020-07-01 23:17:41 -0700

    Delete gearbox debugging statement.
    (cherry picked from commit 9f36ba813458f5d293b82fcf263200c07f05db38)

--- Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -3125,8 +3125,6 @@ namespace Orts.Simulation.RollingStocks
                 //new GarBoxCommand(Simulator.Log, change > 0, controller.CurrentValue, Simulator.ClockTime);
                 SignalEvent(change > 0 ? TrainEvent.GearUp : TrainEvent.GearDown);
                 AlerterReset(TCSEvent.GearBoxChanged);
-
-                Trace.TraceInformation("Gear Box Change - Value {0} oldvalue {1} change {2}", value, oldValue, change);
             }
             if (oldValue != controller.CurrentValue)
                 Simulator.Confirmer.ConfirmWithPerCent(CabControl.GearBox, CabSetting.Decrease, GearBoxController.CurrentNotch);
