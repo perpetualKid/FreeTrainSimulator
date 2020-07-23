openrails/openrails

Add control for vacuum exhauster on diesel locomotives.

Source/ORTS.Common/Input/UserCommand.cs
@@ -180,6 +180,7 @@
         [GetString("Control Large Ejector Decrease")] ControlLargeEjectorDecrease,
         [GetString("Control Small Ejector Increase")] ControlSmallEjectorIncrease,
         [GetString("Control Small Ejector Decrease")] ControlSmallEjectorDecrease,
+        [GetString("Control Vacuum Exhauster")] ControlVacuumExhausterPressed,
         [GetString("Control Cylinder Compound")] ControlCylinderCompound,
         [GetString("Control Firing")] ControlFiring,
         [GetString("Control Refill")] ControlRefill,

Source/Orts.Simulation/Common/Commands.cs
@@ -753,6 +753,36 @@ namespace Orts.Common
     }
 
     [Serializable()]
+    public sealed class VacuumExhausterCommand : BooleanCommand
+    {
+        public static MSTSLocomotive Receiver { get; set; }
+
+        public VacuumExhausterCommand(CommandLog log, bool toState)
+            : base(log, toState)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            if (ToState)
+            {
+                if (!Receiver.VacuumExhausterPressed)
+                    Receiver.Train.SignalEvent(Event.VacuumExhausterOn);
+            }
+            else
+            {
+                Receiver.Train.SignalEvent(Event.VacuumExhausterOff);
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + (ToState ? "on" : "off");
+        }
+    }
+
+    [Serializable()]
     public sealed class HornCommand : BooleanCommand {
         public static MSTSLocomotive Receiver { get; set; }
 

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -115,6 +115,8 @@ namespace Orts.Simulation.RollingStocks
         public bool Bell = false;
         protected bool PreviousBell = false;
 
+        public bool VacuumExhausterPressed = false;
+
         public bool AlerterSnd;
         public bool VigilanceMonitor;
         public bool Sander;
@@ -966,6 +968,7 @@ namespace Orts.Simulation.RollingStocks
             // we won't save the horn state
             outf.Write(Bell);
             outf.Write(Sander);
+            outf.Write(VacuumExhausterPressed);
             outf.Write(Wiper);
             outf.Write(OdometerResetPositionM);
             outf.Write(OdometerCountingUp);
@@ -1001,6 +1004,7 @@ namespace Orts.Simulation.RollingStocks
         {
             if (inf.ReadBoolean()) SignalEvent(Event.BellOn);
             if (inf.ReadBoolean()) SignalEvent(Event.SanderOn);
+            if (inf.ReadBoolean()) SignalEvent(Event.VacuumExhausterOn);
             if (inf.ReadBoolean()) SignalEvent(Event.WiperOn);
             OdometerResetPositionM = inf.ReadSingle();
             OdometerCountingUp = inf.ReadBoolean();
@@ -3635,8 +3639,8 @@ namespace Orts.Simulation.RollingStocks
 
                 case Event.CompressorOn: { CompressorIsOn = true; break; }
                 case Event.CompressorOff: { CompressorIsOn = false; break; }
-                case Event.VacuumExhausterOn: { VacuumExhausterIsOn = true; break; }
-                case Event.VacuumExhausterOff : { VacuumExhausterIsOn = false; break; }
+                case Event.VacuumExhausterOn: { VacuumExhausterPressed = true; if (this.IsLeadLocomotive() && this == Simulator.PlayerLocomotive && Simulator.Confirmer != null) Simulator.Confirmer.Confirm(CabControl.VacuumExhauster, CabSetting.On); break; }
+                case Event.VacuumExhausterOff : { VacuumExhausterPressed = false; if (this.IsLeadLocomotive() && this == Simulator.PlayerLocomotive && Simulator.Confirmer != null) Simulator.Confirmer.Confirm(CabControl.VacuumExhauster, CabSetting.Off); break; }
 
                 case Event._ResetWheelSlip: { LocomotiveAxle.Reset(Simulator.GameTime, SpeedMpS); ThrottleController.SetValue(0.0f); break; }
                 case Event.TrainBrakePressureDecrease:
@@ -3985,6 +3989,12 @@ namespace Orts.Simulation.RollingStocks
                         data = Wiper ? 1 : 0;
                         break;
                     }
+                case CABViewControlTypes.VACUUM_EXHAUSTER:
+                    {
+                        data = VacuumExhausterPressed ? 1 : 0;
+                        break;
+                    }
+
                 case CABViewControlTypes.HORN:
                     {
                         data = Horn ? 1 : 0;

perpetualKid/FreeTrainSimulator

(cherry picked from commit 2ce78cfedcdf3516886d75b1acf37127da9a3abf)

Source/Orts.Common/Input/UserCommand.cs
@@ -186,6 +186,7 @@ namespace Orts.Common.Input
         [Description("Control Large Ejector Decrease")] ControlLargeEjectorDecrease,
         [Description("Control Small Ejector Increase")] ControlSmallEjectorIncrease,
         [Description("Control Small Ejector Decrease")] ControlSmallEjectorDecrease,
+        [Description("Control Vacuum Exhauster")] ControlVacuumExhausterPressed,
         [Description("Control Cylinder Compound")] ControlCylinderCompound,
         [Description("Control Firing")] ControlFiring,
         [Description("Control Refill")] ControlRefill,

Source/Orts.Simulation/Commanding/Commands.cs
@@ -758,8 +758,37 @@ namespace Orts.Simulation.Commanding
     }
 
     [Serializable()]
-    public sealed class HornCommand : BooleanCommand
+    public sealed class VacuumExhausterCommand : BooleanCommand
     {
+        public static MSTSLocomotive Receiver { get; set; }
+
+        public VacuumExhausterCommand(CommandLog log, bool targetState)
+            : base(log, targetState)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            if (targetState)
+            {
+                if (!Receiver.VacuumExhausterPressed)
+                    Receiver.Train.SignalEvent(TrainEvent.VacuumExhausterOn);
+            }
+            else
+            {
+                Receiver.Train.SignalEvent(TrainEvent.VacuumExhausterOff);
+            }
+        }
+
+        public override string ToString()
+        {
+            return ToString(targetState ? "on" : "off");
+        }
+    }
+
+    [Serializable()]
+    public sealed class HornCommand : BooleanCommand {
         public static MSTSLocomotive Receiver { get; set; }
 
         public HornCommand(CommandLog log, bool targetState)

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -117,6 +117,8 @@ namespace Orts.Simulation.RollingStocks
         public bool Bell = false;
         protected bool PreviousBell = false;
 
+        public bool VacuumExhausterPressed = false;
+
         public bool AlerterSnd;
         public bool VigilanceMonitor;
         public bool Sander;
@@ -972,6 +974,7 @@ namespace Orts.Simulation.RollingStocks
             // we won't save the horn state
             outf.Write(Bell);
             outf.Write(Sander);
+            outf.Write(VacuumExhausterPressed);
             outf.Write(Wiper);
             outf.Write(OdometerResetPositionM);
             outf.Write(OdometerCountingUp);
@@ -1007,6 +1010,7 @@ namespace Orts.Simulation.RollingStocks
         {
             if (inf.ReadBoolean()) SignalEvent(TrainEvent.BellOn);
             if (inf.ReadBoolean()) SignalEvent(TrainEvent.SanderOn);
+            if (inf.ReadBoolean()) SignalEvent(TrainEvent.VacuumExhausterOn);
             if (inf.ReadBoolean()) SignalEvent(TrainEvent.WiperOn);
             OdometerResetPositionM = inf.ReadSingle();
             OdometerCountingUp = inf.ReadBoolean();
@@ -3669,8 +3673,8 @@ namespace Orts.Simulation.RollingStocks
 
                 case TrainEvent.CompressorOn: { CompressorIsOn = true; break; }
                 case TrainEvent.CompressorOff: { CompressorIsOn = false; break; }
-                case TrainEvent.VacuumExhausterOn: { VacuumExhausterIsOn = true; break; }
-                case TrainEvent.VacuumExhausterOff: { VacuumExhausterIsOn = false; break; }
+                case TrainEvent.VacuumExhausterOn: { VacuumExhausterPressed = true; if (this.IsLeadLocomotive() && this == Simulator.PlayerLocomotive && Simulator.Confirmer != null) Simulator.Confirmer.Confirm(CabControl.VacuumExhauster, CabSetting.On); break; }
+                case TrainEvent.VacuumExhausterOff : { VacuumExhausterPressed = false; if (this.IsLeadLocomotive() && this == Simulator.PlayerLocomotive && Simulator.Confirmer != null) Simulator.Confirmer.Confirm(CabControl.VacuumExhauster, CabSetting.Off); break; }
 
                 case TrainEvent._ResetWheelSlip: { LocomotiveAxle.Reset(Simulator.GameTime, SpeedMpS); ThrottleController.SetValue(0.0f); break; }
                 case TrainEvent.TrainBrakePressureDecrease:
@@ -4023,6 +4027,12 @@ namespace Orts.Simulation.RollingStocks
                         data = Wiper ? 1 : 0;
                         break;
                     }
+                case CabViewControlType.Vacuum_Exhauster:
+                    {
+                        data = VacuumExhausterPressed ? 1 : 0;
+                        break;
+                    }
+
                 case CabViewControlType.Horn:
                     {
                         data = Horn ? 1 : 0;