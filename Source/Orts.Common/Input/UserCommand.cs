Csantucci/openrails

Implement letterboxing of non-fitting 2D cabs, toggleable with Ctrl+1.

Source/ORTS.Common/Input/UserCommand.cs
@@ -77,6 +77,7 @@
         [GetString("Camera Change Passenger Viewpoint")] CameraChangePassengerViewPoint,
         [GetString("Camera 3D Cab")] CameraThreeDimensionalCab,
         [GetString("Camera Toggle Show Cab")] CameraToggleShowCab,
+        [GetString("Camera Toggle Letterbox Cab")] CameraToggleLetterboxCab,
         [GetString("Camera Head Out Forward")] CameraHeadOutForward,
         [GetString("Camera Head Out Backward")] CameraHeadOutBackward,
         [GetString("Camera Outside Front")] CameraOutsideFront,

perpetualKid/FreeTrainSimulator

(cherry picked from commit 7ac20fac44d9984a00536e29ae147ff182c1f431)

Source/Orts.Common/Input/UserCommand.cs
@@ -80,6 +80,7 @@ namespace Orts.Common.Input
         [Description("Camera Change Passenger Viewpoint")] CameraChangePassengerViewPoint,
         [Description("Camera 3D Cab")] CameraThreeDimensionalCab,
         [Description("Camera Toggle Show Cab")] CameraToggleShowCab,
+        [Description("Camera Toggle Letterbox Cab")] CameraToggleLetterboxCab,
         [Description("Camera Head Out Forward")] CameraHeadOutForward,
         [Description("Camera Head Out Backward")] CameraHeadOutBackward,
         [Description("Camera Outside Front")] CameraOutsideFront,