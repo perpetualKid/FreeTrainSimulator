openrails/openrails

Add a new ORTS sigcfg.dat property for signal light transition time.

Source/RunActivity/Viewer3D/Signals.cs
@@ -294,7 +294,7 @@ namespace Orts.Viewer3D
 
                 lightStates = new SignalLightState[SignalTypeData.Lights.Count];
                 for (var i = 0; i < SignalTypeData.Lights.Count; i++)
-                    lightStates[i] = new SignalLightState();
+                    lightStates[i] = new SignalLightState(SignalTypeData.OnOffTime);
 
 #if DEBUG_SIGNAL_SHAPES
                 Console.Write("  HEAD type={0,-8} lights={1,-2} sem={2}", SignalTypeData.Type, SignalTypeData.Lights.Count, SignalTypeData.Semaphore);
@@ -428,6 +428,7 @@ namespace Orts.Viewer3D
             public readonly Dictionary<int, SignalAspectData> DrawAspects = new Dictionary<int, SignalAspectData>();
             public readonly float FlashTimeOn;
             public readonly float FlashTimeTotal;
+            public readonly float OnOffTime;
             public readonly bool Semaphore;
             public readonly bool DayLight = true;
             public readonly float SemaphoreAnimationTime;
@@ -498,6 +499,8 @@ namespace Orts.Viewer3D
                     SemaphoreAnimationTime = mstsSignalType.SemaphoreInfo;
                     DayLight = mstsSignalType.DayLight;
                 }
+
+                OnOffTime = mstsSignalType.OnOffTime;
             }
         }
 
@@ -552,8 +555,13 @@ namespace Orts.Viewer3D
     /// </summary>
     internal class SignalLightState
     {
-        static readonly float LightOnOffTime = 0.5f; // Transition time in seconds.
+        private readonly float onOffTime;
         private float intensity = 0;
+
+        public SignalLightState(float onOffTime)
+        {
+            this.onOffTime = onOffTime;
+        }
 
         public float GetIntensity()
         {
@@ -562,10 +570,12 @@ namespace Orts.Viewer3D
 
         public void SetIntensity(float target, ElapsedTime et)
         {
-            if (target > intensity)
-                intensity = Math.Min(intensity + et.ClockSeconds / LightOnOffTime, target);
+            if (onOffTime == 0)
+                intensity = target;
+            else if (target > intensity)
+                intensity = Math.Min(intensity + et.ClockSeconds / onOffTime, target);
             else if (target < intensity)
-                intensity = Math.Max(intensity - et.ClockSeconds / LightOnOffTime, target);
+                intensity = Math.Max(intensity - et.ClockSeconds / onOffTime, target);
         }
 
         public bool IsIlluminated()

perpetualKid/FreeTrainSimulator

(cherry picked from commit 79f6c6fb6b411ea402635276055adbea40e966e3)

Source/ActivityRunner/Viewer3D/Signals.cs
@@ -299,7 +299,7 @@ namespace Orts.ActivityRunner.Viewer3D
 
                 lightStates = new SignalLightState[SignalTypeData.Lights.Count];
                 for (var i = 0; i < SignalTypeData.Lights.Count; i++)
-                    lightStates[i] = new SignalLightState();
+                    lightStates[i] = new SignalLightState(SignalTypeData.TransitionTime);
 
 #if DEBUG_SIGNAL_SHAPES
                 Console.Write("  HEAD type={0,-8} lights={1,-2} sem={2}", SignalTypeData.Type, SignalTypeData.Lights.Count, SignalTypeData.Semaphore);
@@ -428,6 +428,7 @@ namespace Orts.ActivityRunner.Viewer3D
             public readonly Dictionary<int, SignalAspectData> DrawAspects = new Dictionary<int, SignalAspectData>();
             public readonly float FlashTimeOn;
             public readonly float FlashTimeTotal;
+            public readonly float TransitionTime;
             public readonly bool Semaphore;
             public readonly bool DayLight = true;
             public readonly float SemaphoreAnimationTime;
@@ -498,6 +499,8 @@ namespace Orts.ActivityRunner.Viewer3D
                     SemaphoreAnimationTime = mstsSignalType.SemaphoreInfo;
                     DayLight = mstsSignalType.DayLight;
                 }
+
+                TransitionTime = mstsSignalType.TransitionTime;
             }
         }
 
@@ -552,20 +555,26 @@ namespace Orts.ActivityRunner.Viewer3D
     /// </summary>
     internal class SignalLightState
     {
-        private static readonly float duration = 0.5f; // Transition time in seconds.
+        private readonly float transitionTime; // Transition time in seconds.
         private double intensity = 0;
+
+        public SignalLightState(float transitionTime)
+        {
+            this.transitionTime = transitionTime;
+        }
 
         public double Intensity => intensity;
         public bool Illuminated => intensity > 0;
 
         public void SetIntensity(float target, in ElapsedTime elapsedTime)
         {
-            if (target > intensity)
-                intensity = Math.Min(intensity + elapsedTime.ClockSeconds / duration, target);
+            if (transitionTime == 0)
+                intensity = target;
+            else if (target > intensity)
+                intensity = Math.Min(intensity + elapsedTime.ClockSeconds / transitionTime, target);
             else if (target < intensity)
-                intensity = Math.Max(intensity - elapsedTime.ClockSeconds / duration, target);
+                intensity = Math.Max(intensity - elapsedTime.ClockSeconds / transitionTime, target);
         }
-
     }
 
     public class SignalLightPrimitive : RenderPrimitive