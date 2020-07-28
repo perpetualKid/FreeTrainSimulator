openrails/openrails

Don't fade in signals when starting the game.

Source/RunActivity/Viewer3D/Signals.cs
@@ -352,7 +352,7 @@ namespace Orts.Viewer3D
                     bool semaphoreDark = SemaphorePos != SemaphoreTarget && SignalTypeData.LightsSemaphoreChange[i];
                     bool constantDark = !SignalTypeData.DrawAspects[DisplayState].DrawLights[i];
                     bool flashingDark = SignalTypeData.DrawAspects[DisplayState].FlashLights[i] && (CumulativeTime > SignalTypeData.FlashTimeOn);
-                    state.SetIntensity(semaphoreDark || constantDark || flashingDark ? 0 : 1, elapsedTime);
+                    state.UpdateIntensity(semaphoreDark || constantDark || flashingDark ? 0 : 1, elapsedTime);
                     if (!state.IsIlluminated())
                         continue;
 
@@ -557,6 +557,7 @@ namespace Orts.Viewer3D
     {
         private readonly float onOffTime;
         private float intensity = 0;
+        private bool firstUpdate = true;
 
         public SignalLightState(float onOffTime)
         {
@@ -568,14 +569,15 @@ namespace Orts.Viewer3D
             return intensity;
         }
 
-        public void SetIntensity(float target, ElapsedTime et)
+        public void UpdateIntensity(float target, ElapsedTime et)
         {
-            if (onOffTime == 0)
+            if (firstUpdate || onOffTime == 0)
                 intensity = target;
             else if (target > intensity)
                 intensity = Math.Min(intensity + et.ClockSeconds / onOffTime, target);
             else if (target < intensity)
                 intensity = Math.Max(intensity - et.ClockSeconds / onOffTime, target);
+            firstUpdate = false;
         }
 
         public bool IsIlluminated()

perpetualKid/FreeTrainSimulator

(cherry picked from commit 25132f7b2ba2f113a80d88faa73b60c3ad464da3)

Source/ActivityRunner/Viewer3D/Signals.cs
@@ -357,7 +357,7 @@ namespace Orts.ActivityRunner.Viewer3D
                     bool semaphoreDark = SemaphorePos != SemaphoreTarget && SignalTypeData.LightsSemaphoreChange[i];
                     bool constantDark = !SignalTypeData.DrawAspects[DisplayState].DrawLights[i];
                     bool flashingDark = SignalTypeData.DrawAspects[DisplayState].FlashLights[i] && (CumulativeTime > SignalTypeData.FlashTimeOn);
-                    state.SetIntensity(semaphoreDark || constantDark || flashingDark ? 0 : 1, elapsedTime);
+                    state.UpdateIntensity(semaphoreDark || constantDark || flashingDark ? 0 : 1, elapsedTime);
                     if (!state.Illuminated)
                         continue;
 
@@ -557,6 +557,7 @@ namespace Orts.ActivityRunner.Viewer3D
     {
         private readonly float transitionTime; // Transition time in seconds.
         private double intensity = 0;
+        private bool firstUpdate = true;
 
         public SignalLightState(float transitionTime)
         {
@@ -566,14 +567,15 @@ namespace Orts.ActivityRunner.Viewer3D
         public double Intensity => intensity;
         public bool Illuminated => intensity > 0;
 
-        public void SetIntensity(float target, in ElapsedTime elapsedTime)
+        public void UpdateIntensity(float target, in ElapsedTime elapsedTime)
         {
-            if (transitionTime == 0)
+            if (firstUpdate || transitionTime == 0)
                 intensity = target;
             else if (target > intensity)
                 intensity = Math.Min(intensity + elapsedTime.ClockSeconds / transitionTime, target);
             else if (target < intensity)
                 intensity = Math.Max(intensity - elapsedTime.ClockSeconds / transitionTime, target);
+            firstUpdate = false;
         }
     }
 