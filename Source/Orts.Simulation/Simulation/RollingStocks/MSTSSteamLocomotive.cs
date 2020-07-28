openrails/openrails

Add steam triggers for steam boiler blowdown

Source/Orts.Simulation/Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -7178,6 +7178,10 @@ public void SteamStartGearBoxIncrease()
         {
             BlowdownValveOpen = !BlowdownValveOpen;
             SignalEvent(Event.BlowdownValveToggle);
+            if (BlowdownValveOpen)
+                SignalEvent(Event.BoilerBlowdownOn);
+            else
+                SignalEvent(Event.BoilerBlowdownOff);
 
             if (IsPlayerTrain)
                 Simulator.Confirmer.Confirm(CabControl.BlowdownValve, BlowdownValveOpen? CabSetting.On : CabSetting.Off);

perpetualKid/FreeTrainSimulator

(cherry picked from commit 41e9d58159303613a17ad5ce85ec1e62118deaf8)

Source/Orts.Simulation/Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -7287,6 +7287,10 @@ public void ToggleCylinderCocks()
         {
             BlowdownValveOpen = !BlowdownValveOpen;
             SignalEvent(TrainEvent.BlowdownValveToggle);
+            if (BlowdownValveOpen)
+                SignalEvent(TrainEvent.BoilerBlowdownOn);
+            else
+                SignalEvent(TrainEvent.BoilerBlowdownOff);
 
             if (IsPlayerTrain)
                 Simulator.Confirmer.Confirm(CabControl.BlowdownValve, BlowdownValveOpen? CabSetting.On : CabSetting.Off);