openrails/openrails

TCS extensions: add 8 generic sound events

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/TrainControlSystem.cs
@@ -580,7 +580,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
         }
 
 
-        private void SignalEvent(Event evt, TrainControlSystem script)
+        public void SignalEvent(Event evt, TrainControlSystem script)
         {
             try
             { 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 13f48a3e5099793829a500a48281f38586c46bc0)

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/TrainControlSystem.cs
@@ -580,7 +580,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
         }
 
 
-        private void SignalEvent(TrainEvent evt, TrainControlSystem script)
+        public void SignalEvent(TrainEvent evt, TrainControlSystem script)
         {
             try
             { 