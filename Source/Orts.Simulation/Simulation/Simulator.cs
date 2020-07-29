openrails/openrails

Restore code parts needed to start explore mode

Source/Orts.Simulation/Simulation/Simulator.cs
@@ -426,8 +426,15 @@ namespace Orts.Simulation
             // define style of passing path and process player passing paths as required
             Signals.UseLocationPassingPaths = Settings.UseLocationPassingPaths;
 
-            playerTrain = InitializeAPTrains(cancellation);
- 
+            switch (IsAutopilotMode)
+            {
+                case true:
+                    playerTrain = InitializeAPTrains(cancellation);
+                    break;
+                default:
+                    playerTrain = InitializeTrains(cancellation);
+                    break;
+            }
             MPManager.Instance().RememberOriginalSwitchState();
 
             // start activity logging if required
@@ -537,6 +544,19 @@ namespace Orts.Simulation
                 foreach (var movingtable in MovingTables) movingtable.Save(outf);
 
             Orts.Simulation.Activity.Save(outf, ActivityRun);
+        }
+
+        Train InitializeTrains(CancellationToken cancellation)
+        {
+            Train playerTrain = InitializePlayerTrain();
+            InitializeStaticConsists();
+            if (playerTrain != null)
+            {
+                var validPosition = playerTrain.PostInit();
+                TrainDictionary.Add(playerTrain.Number, playerTrain);
+                NameDictionary.Add(playerTrain.Name, playerTrain);
+            }
+            return (playerTrain);
         }
 
         AITrain InitializeAPTrains(CancellationToken cancellation)
@@ -1051,6 +1071,132 @@ namespace Orts.Simulation
             }
         }
 
+        //  Used for explore mode; creates the player train within the Train class
+        private Train InitializePlayerTrain()
+        {
+            Debug.Assert(Trains != null, "Cannot InitializePlayerTrain() without Simulator.Trains.");
+            // set up the player locomotive
+
+            Train train = new Train(this);
+            train.TrainType = Train.TRAINTYPE.PLAYER;
+            train.Number = 0;
+            train.Name = "PLAYER";
+
+            string playerServiceFileName;
+            ServiceFile srvFile;
+            playerServiceFileName = Path.GetFileNameWithoutExtension(ExploreConFile);
+            srvFile = new ServiceFile();
+            srvFile.Name = playerServiceFileName;
+            srvFile.Train_Config = playerServiceFileName;
+            srvFile.PathID = Path.GetFileNameWithoutExtension(ExplorePathFile);
+            conFileName = BasePath + @"\TRAINS\CONSISTS\" + srvFile.Train_Config + ".CON";
+            patFileName = RoutePath + @"\PATHS\" + srvFile.PathID + ".PAT";
+            OriginalPlayerTrain = train;
+
+            if (conFileName.Contains("tilted")) train.IsTilting = true;
+
+#if ACTIVITY_EDITOR
+            AIPath aiPath = new AIPath(TDB, TSectionDat, patFileName, TimetableMode, orRouteConfig);
+#else
+            AIPath aiPath = new AIPath(TDB, TSectionDat, patFileName);
+#endif
+            PathName = aiPath.pathName;
+
+            if (aiPath.Nodes == null)
+            {
+                throw new InvalidDataException("Broken path " + patFileName + " for Player train - activity cannot be started");
+            }
+
+            // place rear of train on starting location of aiPath.
+            train.RearTDBTraveller = new Traveller(TSectionDat, TDB.TrackDB.TrackNodes, aiPath);
+
+            ConsistFile conFile = new ConsistFile(conFileName);
+            CurveDurability = conFile.Train.TrainCfg.Durability;   // Finds curve durability of consist based upon the value in consist file
+
+            // add wagons
+            foreach (Wagon wagon in conFile.Train.TrainCfg.WagonList)
+            {
+                string wagonFolder = BasePath + @"\trains\trainset\" + wagon.Folder;
+                string wagonFilePath = wagonFolder + @"\" + wagon.Name + ".wag"; ;
+                if (wagon.IsEngine)
+                    wagonFilePath = Path.ChangeExtension(wagonFilePath, ".eng");
+
+                if (!File.Exists(wagonFilePath))
+                {
+                    // First wagon is the player's loco and required, so issue a fatal error message
+                    if (wagon == conFile.Train.TrainCfg.WagonList[0])
+                        Trace.TraceError("Player's locomotive {0} cannot be loaded in {1}", wagonFilePath, conFileName);
+                    Trace.TraceWarning("Ignored missing wagon {0} in consist {1}", wagonFilePath, conFileName);
+                    continue;
+                }
+
+                try
+                {
+                    TrainCar car = RollingStock.Load(this, wagonFilePath);
+                    car.Flipped = wagon.Flip;
+                    car.UiD = wagon.UiD;
+                    if (MPManager.IsMultiPlayer()) car.CarID = MPManager.GetUserName() + " - " + car.UiD; //player's train is always named train 0.
+                    else car.CarID = "0 - " + car.UiD; //player's train is always named train 0.
+                    train.Cars.Add(car);
+                    car.Train = train;
+                    train.Length += car.CarLengthM;
+
+                    var mstsDieselLocomotive = car as MSTSDieselLocomotive;
+                    if (Activity != null && mstsDieselLocomotive != null)
+                        mstsDieselLocomotive.DieselLevelL = mstsDieselLocomotive.MaxDieselLevelL * Activity.Tr_Activity.Tr_Activity_Header.FuelDiesel / 100.0f;
+
+                    var mstsSteamLocomotive = car as MSTSSteamLocomotive;
+                    if (Activity != null && mstsSteamLocomotive != null)
+                    {
+                        mstsSteamLocomotive.CombinedTenderWaterVolumeUKG = (Kg.ToLb(mstsSteamLocomotive.MaxLocoTenderWaterMassKG) / 10.0f) * Activity.Tr_Activity.Tr_Activity_Header.FuelWater / 100.0f;
+                        mstsSteamLocomotive.TenderCoalMassKG = mstsSteamLocomotive.MaxTenderCoalMassKG * Activity.Tr_Activity.Tr_Activity_Header.FuelCoal / 100.0f;
+                    }
+                }
+                catch (Exception error)
+                {
+                    // First wagon is the player's loco and required, so issue a fatal error message
+                    if (wagon == conFile.Train.TrainCfg.WagonList[0])
+                        throw new FileLoadException(wagonFilePath, error);
+                    Trace.WriteLine(new FileLoadException(wagonFilePath, error));
+                }
+            }// for each rail car
+
+            train.CheckFreight();
+
+            train.PresetExplorerPath(aiPath, Signals);
+            train.ControlMode = Train.TRAIN_CONTROL.EXPLORER;
+
+            bool canPlace = true;
+            Train.TCSubpathRoute tempRoute = train.CalculateInitialTrainPosition(ref canPlace);
+            if (tempRoute.Count == 0 || !canPlace)
+            {
+                throw new InvalidDataException("Player train original position not clear");
+            }
+
+            train.SetInitialTrainRoute(tempRoute);
+            train.CalculatePositionOfCars();
+            train.ResetInitialTrainRoute(tempRoute);
+
+            train.CalculatePositionOfCars();
+            Trains.Add(train);
+
+            // Note the initial position to be stored by a Save and used in Menu.exe to calculate DistanceFromStartM 
+            InitialTileX = Trains[0].FrontTDBTraveller.TileX + (Trains[0].FrontTDBTraveller.X / 2048);
+            InitialTileZ = Trains[0].FrontTDBTraveller.TileZ + (Trains[0].FrontTDBTraveller.Z / 2048);
+
+            PlayerLocomotive = InitialPlayerLocomotive();
+            if ((conFile.Train.TrainCfg.MaxVelocity == null) ||
+                ((conFile.Train.TrainCfg.MaxVelocity != null) && ((conFile.Train.TrainCfg.MaxVelocity.A <= 0f) || (conFile.Train.TrainCfg.MaxVelocity.A == 40f))))
+                train.TrainMaxSpeedMpS = Math.Min((float)TRK.Tr_RouteFile.SpeedLimit, ((MSTSLocomotive)PlayerLocomotive).MaxSpeedMpS);
+            else
+                train.TrainMaxSpeedMpS = Math.Min((float)TRK.Tr_RouteFile.SpeedLimit, conFile.Train.TrainCfg.MaxVelocity.A);
+
+
+            train.AITrainBrakePercent = 100; //<CSComment> This seems a tricky way for the brake modules to test if it is an AI train or not
+            return (train);
+        }
+
+        // used for activity and activity in explore mode; creates the train within the AITrain class
         private AITrain InitializeAPPlayerTrain()
         {
             string playerServiceFileName;

perpetualKid/FreeTrainSimulator

(cherry picked from commit a579994e8f7f99bec6200f44ed3811a89e6b9849)

Source/Orts.Simulation/Simulation/Simulator.cs
@@ -438,8 +438,15 @@ namespace Orts.Simulation
             // define style of passing path and process player passing paths as required
             Signals.UseLocationPassingPaths = Settings.UseLocationPassingPaths;
 
-            playerTrain = InitializeAPTrains(cancellation);
- 
+            switch (IsAutopilotMode)
+            {
+                case true:
+                    playerTrain = InitializeAPTrains(cancellation);
+                    break;
+                default:
+                    playerTrain = InitializeTrains(cancellation);
+                    break;
+            }
             MPManager.Instance().RememberOriginalSwitchState();
 
             // start activity logging if required
@@ -549,6 +556,19 @@ namespace Orts.Simulation
                 foreach (var movingtable in MovingTables) movingtable.Save(outf);
 
             Orts.Simulation.Activity.Save(outf, ActivityRun);
+        }
+
+        Train InitializeTrains(CancellationToken cancellation)
+        {
+            Train playerTrain = InitializePlayerTrain();
+            InitializeStaticConsists();
+            if (playerTrain != null)
+            {
+                var validPosition = playerTrain.PostInit();
+                TrainDictionary.Add(playerTrain.Number, playerTrain);
+                NameDictionary.Add(playerTrain.Name, playerTrain);
+            }
+            return (playerTrain);
         }
 
         AITrain InitializeAPTrains(CancellationToken cancellation)
@@ -1063,6 +1083,129 @@ namespace Orts.Simulation
             }
         }
 
+        private Train InitializePlayerTrain()
+        {
+
+            Debug.Assert(Trains != null, "Cannot InitializePlayerTrain() without Simulator.Trains.");
+            // set up the player locomotive
+
+            Train train = new Train(this);
+            train.TrainType = Train.TRAINTYPE.PLAYER;
+            train.Number = 0;
+            train.Name = "PLAYER";
+
+            string playerServiceFileName;
+            ServiceFile srvFile;
+
+            playerServiceFileName = Path.GetFileNameWithoutExtension(ExploreConFile);
+            srvFile = new ServiceFile(playerServiceFileName, playerServiceFileName, Path.GetFileNameWithoutExtension(ExplorePathFile));
+
+            conFileName = BasePath + @"\TRAINS\CONSISTS\" + srvFile.TrainConfig + ".CON";
+            patFileName = RoutePath + @"\PATHS\" + srvFile.PathId + ".PAT";
+            OriginalPlayerTrain = train;
+
+            if (conFileName.Contains("tilted")) train.IsTilting = true;
+
+
+            AIPath aiPath = new AIPath(TDB, TSectionDat, patFileName, TimetableMode);
+            PathName = aiPath.pathName;
+
+            if (aiPath.Nodes == null)
+            {
+                throw new InvalidDataException("Broken path " + patFileName + " for Player train - activity cannot be started");
+            }
+
+            // place rear of train on starting location of aiPath.
+            train.RearTDBTraveller = new Traveller(TSectionDat, TDB.TrackDB.TrackNodes, aiPath);
+
+            ConsistFile conFile = new ConsistFile(conFileName);
+            CurveDurability = conFile.Train.Durability;   // Finds curve durability of consist based upon the value in consist file
+
+            // add wagons
+            foreach (Wagon wagon in conFile.Train.Wagons)
+            {
+
+                string wagonFolder = BasePath + @"\trains\trainset\" + wagon.Folder;
+                string wagonFilePath = wagonFolder + @"\" + wagon.Name + ".wag"; ;
+                if (wagon.IsEngine)
+                    wagonFilePath = Path.ChangeExtension(wagonFilePath, ".eng");
+
+                if (!File.Exists(wagonFilePath))
+                {
+                    // First wagon is the player's loco and required, so issue a fatal error message
+                    if (wagon == conFile.Train.Wagons[0])
+                        Trace.TraceError("Player's locomotive {0} cannot be loaded in {1}", wagonFilePath, conFileName);
+                    Trace.TraceWarning("Ignored missing wagon {0} in consist {1}", wagonFilePath, conFileName);
+                    continue;
+                }
+
+                try
+                {
+                    TrainCar car = RollingStock.Load(this, wagonFilePath);
+                    car.Flipped = wagon.Flip;
+                    car.UiD = wagon.UiD;
+                    if (MPManager.IsMultiPlayer()) car.CarID = MPManager.GetUserName() + " - " + car.UiD; //player's train is always named train 0.
+                    else car.CarID = "0 - " + car.UiD; //player's train is always named train 0.
+                    train.Cars.Add(car);
+                    car.Train = train;
+                    train.Length += car.CarLengthM;
+
+                    var mstsDieselLocomotive = car as MSTSDieselLocomotive;
+                    if (Activity != null && mstsDieselLocomotive != null)
+                        mstsDieselLocomotive.DieselLevelL = mstsDieselLocomotive.MaxDieselLevelL * Activity.Activity.Header.FuelDiesel / 100.0f;
+
+                    var mstsSteamLocomotive = car as MSTSSteamLocomotive;
+                    if (Activity != null && mstsSteamLocomotive != null)
+                    {
+                        mstsSteamLocomotive.CombinedTenderWaterVolumeUKG = (float)(Mass.Kilogram.ToLb(mstsSteamLocomotive.MaxLocoTenderWaterMassKG) / 10.0f) * Activity.Activity.Header.FuelWater / 100.0f;
+                        mstsSteamLocomotive.TenderCoalMassKG = mstsSteamLocomotive.MaxTenderCoalMassKG * Activity.Activity.Header.FuelCoal / 100.0f;
+                    }
+                }
+                catch (Exception error)
+                {
+                    // First wagon is the player's loco and required, so issue a fatal error message
+                    if (wagon == conFile.Train.Wagons[0])
+                        throw new FileLoadException(wagonFilePath, error);
+                    Trace.WriteLine(new FileLoadException(wagonFilePath, error));
+                }
+            }// for each rail car
+
+            train.CheckFreight();
+
+            train.PresetExplorerPath(aiPath, Signals);
+            train.ControlMode = Train.TRAIN_CONTROL.EXPLORER;
+
+            bool canPlace = true;
+            Train.TCSubpathRoute tempRoute = train.CalculateInitialTrainPosition(ref canPlace);
+            if (tempRoute.Count == 0 || !canPlace)
+            {
+                throw new InvalidDataException("Player train original position not clear");
+            }
+
+            train.SetInitialTrainRoute(tempRoute);
+            train.CalculatePositionOfCars();
+            train.ResetInitialTrainRoute(tempRoute);
+
+            train.CalculatePositionOfCars();
+            Trains.Add(train);
+
+            // Note the initial position to be stored by a Save and used in Menu.exe to calculate DistanceFromStartM 
+            InitialTileX = Trains[0].FrontTDBTraveller.TileX + (Trains[0].FrontTDBTraveller.X / 2048);
+            InitialTileZ = Trains[0].FrontTDBTraveller.TileZ + (Trains[0].FrontTDBTraveller.Z / 2048);
+
+            PlayerLocomotive = InitialPlayerLocomotive();
+            if ((conFile.Train.MaxVelocity == null) ||
+                ((conFile.Train.MaxVelocity?.A <= 0f) || (conFile.Train.MaxVelocity?.A == 40f)))
+                train.TrainMaxSpeedMpS = Math.Min((float)TRK.Route.SpeedLimit, ((MSTSLocomotive)PlayerLocomotive).MaxSpeedMpS);
+            else
+                train.TrainMaxSpeedMpS = Math.Min((float)TRK.Route.SpeedLimit, conFile.Train.MaxVelocity.A);
+
+
+            train.AITrainBrakePercent = 100; //<CSComment> This seems a tricky way for the brake modules to test if it is an AI train or not
+            return (train);
+        }
+
+        // used for activity and activity in explore mode; creates the train within the AITrain class
         private AITrain InitializeAPPlayerTrain()
         {
             string playerServiceFileName;