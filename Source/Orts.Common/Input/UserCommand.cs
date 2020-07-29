openrails/openrails

TCS extensions: add two generic keyboard commands

Source/ORTS.Common/Input/UserCommand.cs
@@ -186,9 +186,12 @@
         [GetString("Control ImmediateRefill")] ControlImmediateRefill,
         [GetString("Control Turntable Clockwise")] ControlTurntableClockwise,
         [GetString("Control Turntable Counterclockwise")] ControlTurntableCounterclockwise,
+        [GetString("Control Generic 1")] ControlGeneric1,
+        [GetString("Control Generic 2")] ControlGeneric2,
         [GetString("Control Cab Radio")] ControlCabRadio,
         [GetString("Control AI Fire On")] ControlAIFireOn,
         [GetString("Control AI Fire Off")] ControlAIFireOff,
         [GetString("Control AI Fire Reset")] ControlAIFireReset,
+
     }
 }

perpetualKid/FreeTrainSimulator

(cherry picked from commit 5cbc53275375c0ce49cc4208354f4aaa4a8768eb)

Source/Orts.Common/Input/UserCommand.cs
@@ -196,6 +196,8 @@ namespace Orts.Common.Input
         [Description("Control ImmediateRefill")] ControlImmediateRefill,
         [Description("Control Turntable Clockwise")] ControlTurntableClockwise,
         [Description("Control Turntable Counterclockwise")] ControlTurntableCounterclockwise,
+        [Description("Control Generic 1")] ControlGeneric1,
+        [Description("Control Generic 2")] ControlGeneric2,
         [Description("Control Cab Radio")] ControlCabRadio,
         [Description("Control AI Fire On")] ControlAIFireOn,
         [Description("Control AI Fire Off")] ControlAIFireOff,