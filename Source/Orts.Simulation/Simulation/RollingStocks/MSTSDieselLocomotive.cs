openrails/openrails

Correct issue with water filling, and save/restore

Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -77,12 +77,6 @@ namespace Orts.Simulation.RollingStocks
             set { FuelController.CurrentValue = value / MaxDieselLevelL; }
         }
 
-        public float CurrentLocomotiveSteamHeatBoilerWaterCapacityL
-        {
-            get { return FuelController.CurrentValue * MaximumSteamHeatBoilerWaterTankCapacityL; }
-            set { FuelController.CurrentValue = value / MaximumSteamHeatBoilerWaterTankCapacityL; }
-        }
-
         public float DieselUsedPerHourAtMaxPowerL = 1.0f;
         public float DieselUsedPerHourAtIdleL = 1.0f;
         public float DieselFlowLps;
@@ -481,7 +475,7 @@ namespace Orts.Simulation.RollingStocks
             if (FuelController.UpdateValue > 0.0)
                 Simulator.Confirmer.UpdateWithPerCent(CabControl.DieselFuel, CabSetting.Increase, FuelController.CurrentValue * 100);
 
-            UpdateSteamHeat(elapsedClockSeconds);
+            UpdateCarSteamHeat(elapsedClockSeconds);
         
         }
 
@@ -862,7 +856,7 @@ namespace Orts.Simulation.RollingStocks
             base.SwitchToAutopilotControl();
         }
 
-        private void UpdateSteamHeat(float elapsedClockSeconds)
+        private void UpdateCarSteamHeat(float elapsedClockSeconds)
         {
             // Update Steam Heating System
 

Source/Orts.Simulation/Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -1933,7 +1933,7 @@ namespace Orts.Simulation.RollingStocks
             UpdateWaterGauge();
             UpdateInjectors(elapsedClockSeconds);
             UpdateFiring(absSpeedMpS);
-            UpdateSteamHeat(elapsedClockSeconds);
+            UpdateCarSteamHeat(elapsedClockSeconds);
             #endregion
 
         }
@@ -5571,7 +5571,7 @@ namespace Orts.Simulation.RollingStocks
             #endregion
         }
 
-        private void UpdateSteamHeat(float elapsedClockSeconds)
+        private void UpdateCarSteamHeat(float elapsedClockSeconds)
         {
             // Update Steam Heating System
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 667afd40f65e701ad38f389ce824515a9cacc9d4)

Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -79,12 +79,6 @@ namespace Orts.Simulation.RollingStocks
             set { FuelController.CurrentValue = value / MaxDieselLevelL; }
         }
 
-        public float CurrentLocomotiveSteamHeatBoilerWaterCapacityL
-        {
-            get { return FuelController.CurrentValue * MaximumSteamHeatBoilerWaterTankCapacityL; }
-            set { FuelController.CurrentValue = value / MaximumSteamHeatBoilerWaterTankCapacityL; }
-        }
-
         public float DieselUsedPerHourAtMaxPowerL = 1.0f;
         public float DieselUsedPerHourAtIdleL = 1.0f;
         public float DieselFlowLps;
@@ -504,7 +498,7 @@ namespace Orts.Simulation.RollingStocks
             if (FuelController.UpdateValue > 0.0)
                 Simulator.Confirmer.UpdateWithPerCent(CabControl.DieselFuel, CabSetting.Increase, FuelController.CurrentValue * 100);
 
-            UpdateSteamHeat(elapsedClockSeconds);
+            UpdateCarSteamHeat(elapsedClockSeconds);
         
         }
 
@@ -896,7 +890,7 @@ namespace Orts.Simulation.RollingStocks
             base.SwitchToAutopilotControl();
         }
 
-        private void UpdateSteamHeat(double elapsedClockSeconds)
+        private void UpdateCarSteamHeat(double elapsedClockSeconds)
         {
             // Update Steam Heating System
 

Source/Orts.Simulation/Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -1941,7 +1941,7 @@ namespace Orts.Simulation.RollingStocks
             UpdateWaterGauge();
             UpdateInjectors(elapsedClockSeconds);
             UpdateFiring(absSpeedMpS);
-            UpdateSteamHeat(elapsedClockSeconds);
+            UpdateCarSteamHeat(elapsedClockSeconds);
             #endregion
 
         }
@@ -5607,7 +5607,7 @@ namespace Orts.Simulation.RollingStocks
             #endregion
         }
 
-        private void UpdateSteamHeat(double elapsedClockSeconds)
+        private void UpdateCarSteamHeat(double elapsedClockSeconds)
         {
             // Update Steam Heating System
 