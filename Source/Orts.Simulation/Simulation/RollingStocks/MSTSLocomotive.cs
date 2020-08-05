openrails/openrails

Add each gear positions sound events for diesel with multiple gear positions. Fix steam injector so that it only turns off when injector initally stopped.

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -3101,8 +3101,26 @@ namespace Orts.Simulation.RollingStocks
                     case 1:
                         SignalEvent(Event.GearPosition1);
                         break;
-                    default:
+                    case 2:
                         SignalEvent(Event.GearPosition2);
+                        break;
+                    case 3:
+                        SignalEvent(Event.GearPosition3);
+                        break;
+                    case 4:
+                        SignalEvent(Event.GearPosition4);
+                        break;
+                    case 5:
+                        SignalEvent(Event.GearPosition5);
+                        break;
+                    case 6:
+                        SignalEvent(Event.GearPosition6);
+                        break;
+                    case 7:
+                        SignalEvent(Event.GearPosition7);
+                        break;
+                    default:
+                        SignalEvent(Event.GearPosition8);
                         break;
                 }
                 PreviousGearBoxNotch = GearBoxController.CurrentNotch; // Update previous value for next time around

Source/Orts.Simulation/Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -5441,10 +5441,8 @@ namespace Orts.Simulation.RollingStocks
                     Injector1Fraction = 0.0f;
                     Injector2IsOn = false;
                     Injector2Fraction = 0.0f;
-                    SignalEvent(Event.WaterInjector1Off);
-                    SignalEvent(Event.WaterInjector2Off);
-                    Injector1SoundIsOn = false;
-                    Injector2SoundIsOn = false;
+                    StopInjector1Sound();
+                    StopInjector2Sound();
                 }
                 else if (WaterGlassLevelIN <= 7.0 && WaterGlassLevelIN > 6.875 && !InjectorLockedOut)  // turn injector 1 on 20% if water level in boiler drops below 7.0
                 {
@@ -5689,6 +5687,31 @@ namespace Orts.Simulation.RollingStocks
                 SignalEvent(Event.WaterInjector2On);
             }
         }
+
+        /// <summary>
+        /// Turn off the injector 1 sound only when the injector stops.
+        /// </summary>
+        private void StopInjector1Sound()
+        {
+            if (Injector1SoundIsOn)
+            {
+                Injector1SoundIsOn = false;
+                SignalEvent(Event.WaterInjector1Off);
+            }
+        }
+
+        /// <summary>
+        /// Turn off the injector 2 sound only when the injector stops.
+        /// </summary>
+        private void StopInjector2Sound()
+        {
+            if (Injector2SoundIsOn)
+            {
+                Injector2SoundIsOn = false;
+                SignalEvent(Event.WaterInjector2Off);
+            }
+        }
+
 
         protected override void UpdateCarSteamHeat(float elapsedClockSeconds)
         {

perpetualKid/FreeTrainSimulator

(cherry picked from commit dc59a40787d5e496a254c7527f54032806988dce)

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -3107,8 +3107,26 @@ namespace Orts.Simulation.RollingStocks
                     case 1:
                         SignalEvent(TrainEvent.GearPosition1);
                         break;
-                    default:
+                    case 2:
                         SignalEvent(TrainEvent.GearPosition2);
+                        break;
+                    case 3:
+                        SignalEvent(TrainEvent.GearPosition3);
+                        break;
+                    case 4:
+                        SignalEvent(TrainEvent.GearPosition4);
+                        break;
+                    case 5:
+                        SignalEvent(TrainEvent.GearPosition5);
+                        break;
+                    case 6:
+                        SignalEvent(TrainEvent.GearPosition6);
+                        break;
+                    case 7:
+                        SignalEvent(TrainEvent.GearPosition7);
+                        break;
+                    default:
+                        SignalEvent(TrainEvent.GearPosition8);
                         break;
                 }
                 PreviousGearBoxNotch = GearBoxController.CurrentNotch; // Update previous value for next time around

Source/Orts.Simulation/Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -5443,10 +5443,8 @@ namespace Orts.Simulation.RollingStocks
                     Injector1Fraction = 0.0f;
                     Injector2IsOn = false;
                     Injector2Fraction = 0.0f;
-                    SignalEvent(TrainEvent.WaterInjector1Off);
-                    SignalEvent(TrainEvent.WaterInjector2Off);
-                    Injector1SoundIsOn = false;
-                    Injector2SoundIsOn = false;
+                    StopInjector1Sound();
+                    StopInjector2Sound();
                 }
                 else if (WaterGlassLevelIN <= 7.0 && WaterGlassLevelIN > 6.875 && !InjectorLockedOut)  // turn injector 1 on 20% if water level in boiler drops below 7.0
                 {
@@ -5691,6 +5689,31 @@ namespace Orts.Simulation.RollingStocks
                 SignalEvent(TrainEvent.WaterInjector2On);
             }
         }
+
+        /// <summary>
+        /// Turn off the injector 1 sound only when the injector stops.
+        /// </summary>
+        private void StopInjector1Sound()
+        {
+            if (Injector1SoundIsOn)
+            {
+                Injector1SoundIsOn = false;
+                SignalEvent(TrainEvent.WaterInjector1Off);
+            }
+        }
+
+        /// <summary>
+        /// Turn off the injector 2 sound only when the injector stops.
+        /// </summary>
+        private void StopInjector2Sound()
+        {
+            if (Injector2SoundIsOn)
+            {
+                Injector2SoundIsOn = false;
+                SignalEvent(TrainEvent.WaterInjector2Off);
+            }
+        }
+
 
         protected override void UpdateCarSteamHeat(double elapsedClockSeconds)
         {