openrails/openrails

Save the computed Track Monitor acceleration to the save file.

Source/Orts.Simulation/Simulation/Physics/Train.cs
@@ -647,8 +647,8 @@ namespace Orts.Simulation.Physics
             Number = inf.ReadInt32();
             TotalNumber = Math.Max(Number + 1, TotalNumber);
             Name = inf.ReadString();
-            SpeedMpS = inf.ReadSingle();
-            AccelerationMpSpS.ForceSmoothValue(0f);
+            SpeedMpS = LastSpeedMpS = inf.ReadSingle();
+            AccelerationMpSpS.ForceSmoothValue(inf.ReadSingle());
             TrainType = (TRAINTYPE)inf.ReadInt32();
             if (TrainType == TRAINTYPE.STATIC) ColdStart = true;
             MUDirection = (Direction)inf.ReadInt32();
@@ -986,6 +986,7 @@ namespace Orts.Simulation.Physics
             outf.Write(Number);
             outf.Write(Name);
             outf.Write(SpeedMpS);
+            outf.Write(AccelerationMpSpS.SmoothedValue);
             outf.Write((int)TrainType);
             outf.Write((int)MUDirection);
             outf.Write(MUThrottlePercent);

perpetualKid/FreeTrainSimulator

(cherry picked from commit bb67fb6e658cfb8e93c26d5fc2fd7b5ce0677a67)

Source/Orts.Simulation/Simulation/Physics/Train.cs
@@ -661,8 +661,8 @@ namespace Orts.Simulation.Physics
             Number = inf.ReadInt32();
             TotalNumber = Math.Max(Number + 1, TotalNumber);
             Name = inf.ReadString();
-            SpeedMpS = inf.ReadSingle();
-            AccelerationMpSpS.Preset(0);
+            SpeedMpS = LastSpeedMpS = inf.ReadSingle();
+            AccelerationMpSpS.Preset(inf.ReadSingle());
             TrainType = (TRAINTYPE)inf.ReadInt32();
             if (TrainType == TRAINTYPE.STATIC) ColdStart = true;
             MUDirection = (Direction)inf.ReadInt32();
@@ -995,6 +995,7 @@ namespace Orts.Simulation.Physics
             outf.Write(Number);
             outf.Write(Name);
             outf.Write(SpeedMpS);
+            outf.Write(AccelerationMpSpS.SmoothedValue);
             outf.Write((int)TrainType);
             outf.Write((int)MUDirection);
             outf.Write(MUThrottlePercent);