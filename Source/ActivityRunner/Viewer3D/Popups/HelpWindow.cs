1b7f3e30bdf58827e667b268fd64fc1ea86c1c79
cjakeman <github@jakeman.plus.com>
2020-08-22 20:21:32 +0100

    compile warnings resolved
    mostly by removing redundant debug statements
    
    (cherry picked from commit a7ba852b44dcabb6941db689ebc0ae5347d06d19)

--- Source/ActivityRunner/Viewer3D/Popups/HelpWindow.cs
+++ Source/ActivityRunner/Viewer3D/Popups/HelpWindow.cs
@@ -68,6 +68,9 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
         List<TabData> Tabs = new List<TabData>();
         int ActiveTab;
 
+        private enum FuelTypes { Coal, DieselOil, Kwhr }
+        private FuelTypes FuelType;
+
         public void LogSeparator(int nCols)
         {
             if (!lDebriefEvalFile) wDbfEval.WriteLine(new String('-', nCols));
@@ -723,9 +726,6 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                             float nDieselburned = 0, nCoalburned = 0;
                             float nCoalBurnedPerc = 0, nWaterBurnedPerc = 0;
                             List<string> cEnginetype = new List<string>();
-                            bool lDiesel = false;
-                            bool lSteam = false;
-                            bool lElectric = false;
                             foreach (var item in cars)//Consist engines
                             {
                                 if (item.EngineType == TrainCar.EngineTypes.Diesel)
@@ -734,7 +734,7 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                                     nDiesellevel = nDiesellevel + (item as MSTSDieselLocomotive).DieselLevelL;
                                     nDieselburned = nDieselvolume - nDiesellevel;
                                     cEnginetype.Add("Diesel");
-                                    lDiesel = true;
+                                    FuelType = FuelTypes.DieselOil;
                                 }
 
                                 if (item.EngineType == TrainCar.EngineTypes.Steam && item.AuxWagonType == "Engine")
@@ -746,12 +746,12 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                                     cEnginetype.Add("Steam");
 
                                     nWaterBurnedPerc = 1 - ((item as MSTSSteamLocomotive).CombinedTenderWaterVolumeUKG / (item as MSTSSteamLocomotive).MaxTotalCombinedWaterVolumeUKG);
-                                    lSteam = true;
+                                    FuelType = FuelTypes.Coal;
                                 }
                                 if (item.EngineType == TrainCar.EngineTypes.Electric)
                                 {
                                     cEnginetype.Add("Electric");
-                                    lElectric = true;
+                                    FuelType = FuelTypes.Kwhr;
                                 }
                             }
                             //Consist engine type  
@@ -761,13 +761,13 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                             labeltext = "  Consist engine=" + (ncountdiesel > 0 ? ncountdiesel + " " + Viewer.Catalog.GetString("Diesel.") + " " : "") + (ncountsteam > 0 ? ncountsteam + " " + Viewer.Catalog.GetString("Steam.") + " " : "") + (ncountelectric > 0 ? ncountelectric + " " + Viewer.Catalog.GetString("Electric.") : "");
                             outmesssage(labeltext, colWidth * 3, true, 0);
 
-                            if (lDiesel)//Fuel Diesel
+                            if (FuelType == FuelTypes.DieselOil)
                             {
                                 labeltext = "  Burned Diesel=" + FormatStrings.FormatFuelVolume(nDieselburned, ismetric, isuk);
                                 outmesssage(labeltext, colWidth * 3, true, 0);
                             }
 
-                            if (lSteam)//Fuel Steam
+                            if (FuelType == FuelTypes.Coal)
                             {
                                 labeltext = "  Burned Coal=" + FormatStrings.FormatMass(nCoalburned, ismetric) + " (" + nCoalBurnedPerc.ToString("0.##%") + ")";
                                 outmesssage(labeltext, colWidth * 3, true, 0);
