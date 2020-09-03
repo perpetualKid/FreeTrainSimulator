openrails/openrails

Change behaviour of 3D cab key (Alt + 1 by default)https://blueprints.launchpad.net/or/+spec/change-behaviour-3d-cab-key

Source/ORTS.Common/Input/UserCommand.cs
@@ -73,7 +73,7 @@
 
         [GetString("Camera Cab")] CameraCab,
         [GetString("Camera Change Passenger Viewpoint")] CameraChangePassengerViewPoint,
-        [GetString("Camera 3D Cab")] CameraThreeDimensionalCab,
+        [GetString("Camera Toggle 3D Cab")] CameraToggleThreeDimensionalCab,
         [GetString("Camera Toggle Show Cab")] CameraToggleShowCab,
         [GetString("Camera Toggle Letterbox Cab")] CameraToggleLetterboxCab,
         [GetString("Camera Head Out Forward")] CameraHeadOutForward,

perpetualKid/FreeTrainSimulator

(cherry picked from commit ce8626ef98fb506f6778dfc1fb3d74a78aeafacd)

Source/Orts.Common/Input/UserCommand.cs
@@ -78,7 +78,7 @@ namespace Orts.Common.Input
 
         [Description("Camera Cab")] CameraCab,
         [Description("Camera Change Passenger Viewpoint")] CameraChangePassengerViewPoint,
-        [Description("Camera 3D Cab")] CameraThreeDimensionalCab,
+        [Description("Camera Toggle 3D Cab")] CameraToggleThreeDimensionalCab,
         [Description("Camera Toggle Show Cab")] CameraToggleShowCab,
         [Description("Camera Toggle Letterbox Cab")] CameraToggleLetterboxCab,
         [Description("Camera Head Out Forward")] CameraHeadOutForward,