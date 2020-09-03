openrails/openrails

Fix timetable separator test - Tab is now a valid separator per http://www.elvastower.com/forums/index.php?/topic/30326-update-timetable-mode-signalling/

Source/Tests/Orts.Parsers.OR/TimetableReaderTests.cs
@@ -44,6 +44,12 @@ namespace Tests.Orts.Parsers.OR
             }
             using (var file = new TestFile("\t"))
             {
+                var tr = new TimetableReader(file.FileName);
+                Assert.Equal(1, tr.Strings.Count);
+                Assert.Equal(2, tr.Strings[0].Length);
+            }
+            using (var file = new TestFile(":"))
+            {
                 Assert.Throws(typeof(InvalidDataException), () => {
                     var tr = new TimetableReader(file.FileName);
                 });

perpetualKid/FreeTrainSimulator

Source/UnitTests/Tests.Orts/Formats/OR/Parsers/TimetableReaderTests.cs
@@ -26,7 +26,13 @@ namespace Tests.Orts.Formats.OR.Parsers
                 Assert.AreEqual(1, tr.Strings.Count);
                 Assert.AreEqual(2, tr.Strings[0].Length);
             }
-            using (var file = new TestFile("\n"))
+            using (var file = new TestFile("\t"))
+            {
+                var tr = new TimetableReader(file.FileName);
+                Assert.AreEqual(1, tr.Strings.Count);
+                Assert.AreEqual(2, tr.Strings[0].Length);
+            }
+            using (var file = new TestFile(":"))
             {
                 Assert.ThrowsException<InvalidDataException>(() => {
                     var tr = new TimetableReader(file.FileName);