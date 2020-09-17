2a3fd70416ca5a74c6f2800d1bb29e50fe38b5a2
perpetualKid <perpetualKid@azuros.net>
2020-09-17 08:27:03 +0200

    Regenerated translation templates

--- Source/Locales/Extract-Templates.bat
+++ Source/Locales/Extract-Templates.bat
@@ -6,6 +6,9 @@ dotnet tool install gettext.net.extractor
 PUSHD %~p0
 FOR /D %%M IN (.\..\*) DO (
 	dotnet gettext.extractor -s %%M -t .\%%~nxM\%%~nxM.pot
+	IF EXIST .\%%~nxM\%%~nxM.pot (
+	echo Updated translation template .\%%~nxM\%%~nxM.pot
+	)
 )
 POPD
 IF "%~1"=="" PAUSE
