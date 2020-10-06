openrails/openrails

Add manual braking function

Source/ORTS.Common/Input/UserCommand.cs
@@ -124,6 +124,8 @@
         [GetString("Control Train Brake Zero")] ControlTrainBrakeZero,
         [GetString("Control Engine Brake Increase")] ControlEngineBrakeIncrease,
         [GetString("Control Engine Brake Decrease")] ControlEngineBrakeDecrease,
+        [GetString("Control Brakeman Brake Increase")] ControlBrakemanBrakeIncrease,
+        [GetString("Control Brakeman Brake Decrease")] ControlBrakemanBrakeDecrease,
         [GetString("Control Dynamic Brake Increase")] ControlDynamicBrakeIncrease,
         [GetString("Control Dynamic Brake Decrease")] ControlDynamicBrakeDecrease,
         [GetString("Control Bail Off")] ControlBailOff,

Source/Orts.Simulation/Common/Commands.cs
@@ -467,6 +467,23 @@ namespace Orts.Common
         }
     }
 
+     [Serializable()]
+    public sealed class BrakemanBrakeCommand : ContinuousCommand
+    {
+        public static MSTSLocomotive Receiver { get; set; }
+        public BrakemanBrakeCommand(CommandLog log, bool toState, float? target, double startTime)
+            : base(log, toState, target, startTime)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.BrakemanBrakeChangeTo(ToState, Target);
+            // Report();
+        }
+    }
+
     [Serializable()]
     public sealed class DynamicBrakeCommand : ContinuousCommand {
         public static MSTSLocomotive Receiver { get; set; }

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -37,6 +37,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 lower = lower.Substring(21);
             if (lower.StartsWith("enginebrakescontroller"))
                 lower = lower.Substring(22);
+            if (lower.StartsWith("brakemanbrakescontroller"))
+                lower = lower.Substring(24);
             switch (lower)
             {
                 case "dummy": break;
@@ -60,6 +62,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "epholdstart": Type = ControllerState.SelfLap; break;
                 case "vacuumcontinuousservicestart": Type = ControllerState.VacContServ; break;
                 case "vacuumapplycontinuousservicestart": Type = ControllerState.VacApplyContServ; break;
+                case "manualbrakingstart": Type = ControllerState.ManualBraking; break;
                 case "brakenotchstart": Type = ControllerState.BrakeNotch; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);

perpetualKid/FreeTrainSimulator

(cherry picked from commit c50ff2b62dc6950695a44c431e17968fd11e924c)

Source/Orts.Common/Input/UserCommand.cs
@@ -129,6 +129,8 @@ namespace Orts.Common.Input
         [Description("Control Train Brake Zero")] ControlTrainBrakeZero,
         [Description("Control Engine Brake Increase")] ControlEngineBrakeIncrease,
         [Description("Control Engine Brake Decrease")] ControlEngineBrakeDecrease,
+        [Description("Control Brakeman Brake Increase")] ControlBrakemanBrakeIncrease,
+        [Description("Control Brakeman Brake Decrease")] ControlBrakemanBrakeDecrease,
         [Description("Control Dynamic Brake Increase")] ControlDynamicBrakeIncrease,
         [Description("Control Dynamic Brake Decrease")] ControlDynamicBrakeDecrease,
         [Description("Control Bail Off")] ControlBailOff,

Source/Orts.Simulation/Commanding/Commands.cs
@@ -453,6 +453,23 @@ namespace Orts.Simulation.Commanding
         }
     }
 
+     [Serializable()]
+    public sealed class BrakemanBrakeCommand : ContinuousCommand
+    {
+        public static MSTSLocomotive Receiver { get; set; }
+        public BrakemanBrakeCommand(CommandLog log, bool targetState, float? target, double startTime)
+            : base(log, targetState, target, startTime)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.BrakemanBrakeChangeTo(targetState, target);
+            // Report();
+        }
+    }
+
     [Serializable()]
     public sealed class DynamicBrakeCommand : ContinuousCommand
     {

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -41,6 +41,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 lower = lower.Substring(21);
             if (lower.StartsWith("enginebrakescontroller"))
                 lower = lower.Substring(22);
+            if (lower.StartsWith("brakemanbrakescontroller"))
+                lower = lower.Substring(24);
             switch (lower)
             {
                 case "dummy": break;
@@ -64,6 +66,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "epholdstart": NotchStateType = ControllerState.SelfLap; break;
                 case "vacuumcontinuousservicestart": NotchStateType = ControllerState.VacContServ; break;
                 case "vacuumapplycontinuousservicestart": NotchStateType = ControllerState.VacApplyContServ; break;
+                case "manualbrakingstart": NotchStateType = ControllerState.ManualBraking; break;
                 case "brakenotchstart": NotchStateType = ControllerState.BrakeNotch; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);