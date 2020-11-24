021864f6237d36c455fb44c2bb28da573aa8525a
Csantucci <carlosanit1@gmail.com>
2020-11-24 16:15:09 +0100

    Further renamings
    (cherry picked from commit c02c6670d43fb230b511491ac408fe6c359bb915)

--- Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/TrainControlSystem.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/TrainControlSystem.cs
@@ -314,8 +314,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems
             Script.BrakeCutsPowerAtBrakeCylinderPressureBar = () => (float)Pressure.Atmospheric.FromPSI(Locomotive.BrakeCutsPowerAtBrakeCylinderPressurePSI);
                 Script.TrainBrakeControllerState = () => Locomotive.TrainBrakeController.TrainBrakeControllerState;
                 Script.AccelerationMpSS = () => Locomotive.AccelerationMpSS;
-                Script.ElevationM = () => Locomotive.WorldPosition.Location.Y;
-                Script.CurrentGradientPercent = () => Locomotive.CurrentElevationPercent;
+                Script.AltitudeM = () => Locomotive.WorldPosition.Location.Y;
+                Script.CurrentElevationPercent = () => Locomotive.CurrentElevationPercent;
                 Script.LineSpeedMpS = () => (float)Simulator.TRK.Route.SpeedLimit;
                 Script.DoesStartFromTerminalStation = () => DoesStartFromTerminalStation();
                 Script.IsColdStart = () => Locomotive.Train.ColdStart;
