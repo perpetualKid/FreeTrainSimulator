openrails/openrails

Separate min check distance for explorer mode

Source/Orts.Simulation/Simulation/Physics/Train.cs
@@ -247,6 +247,7 @@ namespace Orts.Simulation.Physics
         public float maxTimeS = 120;                     // check ahead for distance covered in 2 mins.
         public float minCheckDistanceM = 5000;           // minimum distance to check ahead
         public float minCheckDistanceManualM = 3000;     // minimum distance to check ahead in manual mode
+        public float minCheckDistanceExplorerM => Math.Max(AllowedMaxSpeedMpS * 240, 5000);      // minimum distance to check ahead in explorer mode
 
         public float standardOverlapM = 15.0f;           // standard overlap on clearing sections
         public float junctionOverlapM = 75.0f;           // standard overlap on clearing sections
@@ -9012,7 +9013,7 @@ namespace Orts.Simulation.Physics
                 List<int> tempSections = new List<int>();
 
                 tempSections = signalRef.ScanRoute(this, requiredPosition.TCSectionIndex, requiredPosition.TCOffset,
-                        requiredPosition.TCDirection, forward, minCheckDistanceM, true, false,
+                        requiredPosition.TCDirection, forward, minCheckDistanceExplorerM, true, false,
                         true, false, true, false, false, false, false, IsFreight);
 
                 if (tempSections.Count > 0)
@@ -9098,10 +9099,10 @@ namespace Orts.Simulation.Physics
 
             // if route does not end with signal and is too short, extend
 
-            if (!endWithSignal && totalLengthM < minCheckDistanceM)
+            if (!endWithSignal && totalLengthM < minCheckDistanceExplorerM)
             {
 
-                float extendedDistanceM = minCheckDistanceM - totalLengthM;
+                float extendedDistanceM = minCheckDistanceExplorerM - totalLengthM;
                 TCRouteElement lastElement = newRoute[newRoute.Count - 1];
 
                 int lastSectionIndex = lastElement.TCSectionIndex;
@@ -9327,7 +9328,7 @@ namespace Orts.Simulation.Physics
                         int numCleared = 0;
                         totalLengthM = 0;
                         offsetM = direction == 0 ? requiredPosition.TCOffset : thisSection.Length - requiredPosition.TCOffset;
-                        for (int iindex = 0; iindex < newRoute.Count && (firstSignalPassed || totalLengthM < minCheckDistanceM) && (!firstSignalPassed || numCleared != 0); iindex++)
+                        for (int iindex = 0; iindex < newRoute.Count && (firstSignalPassed || totalLengthM < minCheckDistanceExplorerM) && (!firstSignalPassed || numCleared != 0); iindex++)
                         {
 
                             thisSection = signalRef.TrackCircuitList[newRoute[iindex].TCSectionIndex];

perpetualKid/FreeTrainSimulator

(cherry picked from commit 51d721feb9e63df6454b2a49b2854235be705585)

Source/Orts.Simulation/Physics/Train.cs
@@ -100,6 +100,9 @@ namespace Orts.Simulation.Physics
         internal const float MinCheckDistanceM = 5000;           // minimum distance to check ahead
         internal const float MinCheckDistanceManualM = 3000;     // minimum distance to check ahead in manual mode
 
+        internal float MinCheckDistanceExplorerM => Math.Max(AllowedMaxSpeedMpS * MaxTimeS * 2, MinCheckDistanceM);      // minimum distance to check ahead in explorer mode
+
+
         #endregion
 
         public List<TrainCar> Cars { get; } = new List<TrainCar>();           // listed front to back
@@ -6798,7 +6801,7 @@ namespace Orts.Simulation.Physics
 
                 // build new route
                 List<int> tempSections = SignalEnvironment.ScanRoute(this, requiredPosition.TrackCircuitSectionIndex, requiredPosition.Offset,
-                    requiredPosition.Direction, forward, MinCheckDistanceM, true, false, true, false, true, false, false, false, false, IsFreight);
+                    requiredPosition.Direction, forward, MinCheckDistanceExplorerM, true, false, true, false, true, false, false, false, false, IsFreight);
 
                 if (tempSections.Count > 0)
                 {
@@ -6879,10 +6882,10 @@ namespace Orts.Simulation.Physics
             }
 
             // if route does not end with signal and is too short, extend
-            if (!endWithSignal && totalLengthM < MinCheckDistanceM)
+            if (!endWithSignal && totalLengthM < MinCheckDistanceExplorerM)
             {
 
-                float extendedDistanceM = MinCheckDistanceM - totalLengthM;
+                float extendedDistanceM = MinCheckDistanceExplorerM - totalLengthM;
                 TrackCircuitRouteElement lastElement = newRoute[newRoute.Count - 1];
 
                 int lastSectionIndex = lastElement.TrackCircuitSection.Index;
@@ -7084,7 +7087,7 @@ namespace Orts.Simulation.Physics
                         int numCleared = 0;
                         totalLengthM = 0;
                         offsetM = direction == Direction.Forward ? requiredPosition.Offset : section.Length - requiredPosition.Offset;
-                        for (int i = 0; i < newRoute.Count && (firstSignalPassed || totalLengthM < MinCheckDistanceM) && (!firstSignalPassed || numCleared != 0); i++)
+                        for (int i = 0; i < newRoute.Count && (firstSignalPassed || totalLengthM < MinCheckDistanceExplorerM) && (!firstSignalPassed || numCleared != 0); i++)
                         {
                             section = TrackCircuitSection.TrackCircuitList[newRoute[i].TrackCircuitSection.Index];
                             TrackDirection currentDirection = newRoute[i].Direction;