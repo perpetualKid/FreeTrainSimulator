openrails/openrails

Overcharge in MSTS brake controller

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -64,6 +64,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "vacuumapplycontinuousservicestart": Type = ControllerState.VacApplyContServ; break;
                 case "manualbrakingstart": Type = ControllerState.ManualBraking; break;
                 case "brakenotchstart": Type = ControllerState.BrakeNotch; break;
+                case "overchargestart": Type = ControllerState.Overcharge; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;

perpetualKid/FreeTrainSimulator

(cherry picked from commit bf3561ca8b00a2d5777387157821d7ad1a2d6c92)

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -68,6 +68,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "vacuumapplycontinuousservicestart": NotchStateType = ControllerState.VacApplyContServ; break;
                 case "manualbrakingstart": NotchStateType = ControllerState.ManualBraking; break;
                 case "brakenotchstart": NotchStateType = ControllerState.BrakeNotch; break;
+                case "overchargestart": NotchStateType = ControllerState.Overcharge; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;