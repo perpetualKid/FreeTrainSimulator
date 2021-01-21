openrails/openrails

Use a standardized sound for the screenshot event. Sound is a public domain work courtesy of Freesound: https://freesound.org/people/jdaniel1999/sounds/376205/

Source/RunActivity/Viewer3D/Viewer.cs
@@ -69,6 +69,7 @@ namespace Orts.Viewer3D
         public Orts.Viewer3D.Processes.Game Game { get; private set; }
         public Simulator Simulator { get; private set; }
         public World World { get; private set; }
+        private SoundSource ViewerSounds { get; set; }
         /// <summary>
         /// Monotonically increasing time value (in seconds) for the game/viewer. Starts at 0 and only ever increases, at real-time.
         /// </summary>
@@ -432,6 +433,14 @@ namespace Orts.Viewer3D
 
             World = new World(this, Simulator.ClockTime);
 
+            ViewerSounds = new SoundSource(this, soundSource => new[]
+            {
+                new SoundStream(soundSource, soundStream => new[]
+                {
+                    new ORTSDiscreteTrigger(soundStream, Event.TakeScreenshot, ORTSSoundCommand.Precompiled(Path.Combine(ContentPath, "TakeScreenshot.wav"), soundStream)),
+                }),
+            });
+            SoundProcess.AddSoundSource(this, ViewerSounds);
             Simulator.Confirmer.PlayErrorSound += (s, e) =>
             {
                 if (World.GameSounds != null)
@@ -1794,7 +1803,7 @@ namespace Orts.Viewer3D
                 // Hide MessageWindow
                 MessagesWindow.Visible = false;
                 // Audible confirmation that screenshot taken
-                if (World.GameSounds != null) World.GameSounds.HandleEvent(Event.ControlError);
+                ViewerSounds.HandleEvent(Event.TakeScreenshot);
             }
 
             // Use IsDown() not IsPressed() so users can take multiple screenshots as fast as possible by holding down the key.

perpetualKid/FreeTrainSimulator

(cherry picked from commit ae448e36988d761d95f06bba1f27fb861358e55f)

Source/ActivityRunner/Viewer3D/Viewer.cs
@@ -78,6 +78,7 @@ namespace Orts.ActivityRunner.Viewer3D
         public Orts.ActivityRunner.Viewer3D.Processes.Game Game { get; private set; }
         public Simulator Simulator { get; private set; }
         public World World { get; private set; }
+        private SoundSource ViewerSounds { get; set; }
         /// <summary>
         /// Monotonically increasing time value (in seconds) for the game/viewer. Starts at 0 and only ever increases, at real-time.
         /// </summary>
@@ -445,6 +446,14 @@ namespace Orts.ActivityRunner.Viewer3D
 
             World = new World(this, Simulator.ClockTime);
 
+            ViewerSounds = new SoundSource(this, soundSource => new[]
+            {
+                new SoundStream(soundSource, soundStream => new[]
+                {
+                    new ORTSDiscreteTrigger(soundStream, TrainEvent.TakeScreenshot, ORTSSoundCommand.Precompiled(Path.Combine(ContentPath, "TakeScreenshot.wav"), soundStream)),
+                }),
+            });
+            SoundProcess.AddSoundSource(this, ViewerSounds);
             Simulator.Confirmer.PlayErrorSound += (s, e) =>
             {
                 if (World.GameSounds != null)
@@ -1784,7 +1793,7 @@ namespace Orts.ActivityRunner.Viewer3D
                 // Hide MessageWindow
                 MessagesWindow.Visible = false;
                 // Audible confirmation that screenshot taken
-                if (World.GameSounds != null) World.GameSounds.HandleEvent(TrainEvent.ControlError);
+                ViewerSounds.HandleEvent(TrainEvent.TakeScreenshot);
             }
 
             // Use IsDown() not IsPressed() so users can take multiple screenshots as fast as possible by holding down the key.