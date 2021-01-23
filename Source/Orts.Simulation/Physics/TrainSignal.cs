4b8ac5d83c4df920567e3541d625c6c22bc8dad9
perpetualKid <perpetualKid@azuros.net>
2021-01-23 13:26:05 +0100

    Create TrainSignal.cs

---
+++ Source/Orts.Simulation/Physics/TrainSignal.cs
@@ -0,0 +1,20 @@
+﻿using Orts.Simulation.Signalling;
+
+namespace Orts.Simulation.Physics
+{
+    //================================================================================================//
+    /// <summary>
+    /// Generation of Train data useful for TCS and TrackMonitor; player train only
+    /// <\summary>
+    public class TrainSignal
+    {
+        public float DistanceToTrain { get; }
+        public Signal SignalObjectDetails { get; }
+
+        public TrainSignal(float distanceToTrainM, Signal objectDetails)
+        {
+            DistanceToTrain = distanceToTrainM;
+            SignalObjectDetails = objectDetails;
+        }
+    }
+}
