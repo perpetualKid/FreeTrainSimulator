openrails/openrails

First approach to manual callon

Source/Orts.Simulation/Simulation/Physics/Train.cs
@@ -236,6 +236,7 @@ public float TrainCurrentCarriageHeatTempC;     // Current train carriage heat
         public int IndexNextSignal = -1;                 // Index in SignalObjectItems for next signal
         public int IndexNextSpeedlimit = -1;             // Index in SignalObjectItems for next speedpost
         public SignalObject[] NextSignalObject = new SignalObject[2];  // direct reference to next signal
+        public SignalObject AllowedCallOnSignal;         // Signal for which train has call on allowed by dispatcher
 
         public float TrainMaxSpeedMpS;                   // Max speed as set by route (default value)
         public float AllowedMaxSpeedMpS;                 // Max speed as allowed
@@ -2483,6 +2484,9 @@ public float TrainCurrentCarriageHeatTempC;     // Current train carriage heat
                 // system will take back control of the signal
                 if (signalObject.holdState == SignalObject.HoldState.ManualPass ||
                     signalObject.holdState == SignalObject.HoldState.ManualApproach) signalObject.holdState = SignalObject.HoldState.None;
+
+                if (AllowedCallOnSignal == signalObject)
+                    AllowedCallOnSignal = null;
             }
             UpdateSectionStateManual();                                                           // update track occupation          //
             UpdateManualMode(SignalObjIndex);                                                     // update route clearance           //
@@ -2510,6 +2514,9 @@ public float TrainCurrentCarriageHeatTempC;     // Current train carriage heat
                 // system will take back control of the signal
                 if (signalObject.holdState == SignalObject.HoldState.ManualPass ||
                     signalObject.holdState == SignalObject.HoldState.ManualApproach) signalObject.holdState = SignalObject.HoldState.None;
+
+                if (AllowedCallOnSignal == signalObject)
+                    AllowedCallOnSignal = null;
             }
             UpdateSectionStateExplorer();                                                         // update track occupation          //
             UpdateExplorerMode(SignalObjIndex);                                                   // update route clearance           //
@@ -7064,6 +7071,9 @@ public float TrainCurrentCarriageHeatTempC;     // Current train carriage heat
                     signalObject.holdState = SignalObject.HoldState.None;
                 }
 
+                if (AllowedCallOnSignal == signalObject)
+                    AllowedCallOnSignal = null;
+
                 signalObject.resetSignalEnabled();
             }
         }
@@ -7215,6 +7225,9 @@ public float TrainCurrentCarriageHeatTempC;     // Current train carriage heat
 
         public virtual bool TestCallOn(SignalObject thisSignal, bool allowOnNonePlatform, TCSubpathRoute thisRoute, string dumpfile)
         {
+            if (AllowedCallOnSignal == thisSignal)
+                return true;
+
             bool intoPlatform = false;
 
             foreach (Train.TCRouteElement routeElement in thisSignal.signalRoute)
@@ -7558,6 +7571,9 @@ public float TrainCurrentCarriageHeatTempC;     // Current train carriage heat
                 //the following is added by JTang, passing a hold signal, will take back control by the system
                 if (thisSignal.holdState == SignalObject.HoldState.ManualPass ||
                     thisSignal.holdState == SignalObject.HoldState.ManualApproach) thisSignal.holdState = SignalObject.HoldState.None;
+
+                if (AllowedCallOnSignal == thisSignal)
+                    AllowedCallOnSignal = null;
 
                 thisSignal.resetSignalEnabled();
             }

perpetualKid/FreeTrainSimulator

(cherry picked from commit b7123b518ca7d70f1ae0f42db9617f266d1b3aaa)

Source/Orts.Simulation/Physics/Train.cs
@@ -208,6 +208,7 @@ namespace Orts.Simulation.Physics
         private int nextSignalIndex = -1;                 // Index in SignalObjectItems for next signal
         private int nextSpeedLimitIndex = -1;             // Index in SignalObjectItems for next speedpost
         internal Signal[] NextSignalObject { get; } = new Signal[2];  // direct reference to next signal
+        public Signal AllowedCallOnSignal { get; set; }         // Signal for which train has call on allowed by dispatcher
 
         // Local max speed independently from signal and speedpost speed;
         // depends from various parameters like route max speed, overall or section efficiency of service,
@@ -2481,6 +2482,9 @@ namespace Orts.Simulation.Physics
                 // system will take back control of the signal
                 if (signalObject.HoldState == SignalHoldState.ManualPass || signalObject.HoldState == SignalHoldState.ManualApproach)
                     signalObject.HoldState = SignalHoldState.None;
+
+                if (AllowedCallOnSignal == signalObject)
+                    AllowedCallOnSignal = null;
             }
             UpdateSectionStateManual();                                                           // update track occupation          //
             UpdateManualMode(SignalObjIndex);                                                     // update route clearance           //
@@ -2505,6 +2509,9 @@ namespace Orts.Simulation.Physics
                 // system will take back control of the signal
                 if (signalObject.HoldState == SignalHoldState.ManualPass || signalObject.HoldState == SignalHoldState.ManualApproach)
                     signalObject.HoldState = SignalHoldState.None;
+
+                if (AllowedCallOnSignal == signalObject)
+                    AllowedCallOnSignal = null;
             }
             UpdateSectionStateExplorer();                                                         // update track occupation          //
             UpdateExplorerMode(SignalObjIndex);                                                   // update route clearance           //
@@ -5319,6 +5326,9 @@ namespace Orts.Simulation.Physics
                     signalObject.HoldState = SignalHoldState.None;
                 }
 
+                if (AllowedCallOnSignal == signalObject)
+                    AllowedCallOnSignal = null;
+
                 signalObject.ResetSignalEnabled();
             }
         }
@@ -5420,6 +5430,9 @@ namespace Orts.Simulation.Physics
         /// </summary>
         internal virtual bool TestCallOn(Signal signal, bool allowOnNonePlatform, TrackCircuitPartialPathRoute route)
         {
+            if (AllowedCallOnSignal == signal)
+                return true;
+
             bool intoPlatform = false;
 
             foreach (TrackCircuitRouteElement routeElement in signal.SignalRoute)
@@ -5678,6 +5691,9 @@ namespace Orts.Simulation.Physics
                 //the following is added by JTang, passing a hold signal, will take back control by the system
                 if (signal.HoldState == SignalHoldState.ManualPass || signal.HoldState == SignalHoldState.ManualApproach)
                     signal.HoldState = SignalHoldState.None;
+
+                if (AllowedCallOnSignal == signal)
+                    AllowedCallOnSignal = null;
 
                 signal.ResetSignalEnabled();
             }