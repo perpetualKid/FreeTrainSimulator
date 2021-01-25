openrails/openrails

Add slow service brake application

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -67,6 +67,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "manualbrakingstart": Type = ControllerState.ManualBraking; break;
                 case "brakenotchstart": Type = ControllerState.BrakeNotch; break;
                 case "overchargestart": Type = ControllerState.Overcharge; break;
+                case "slowservicestart": Type = ControllerState.SlowService; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;

perpetualKid/FreeTrainSimulator

(cherry picked from commit fac43c10049fdbed7187450d15bb0031f51ebb36)

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -69,6 +69,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "manualbrakingstart": NotchStateType = ControllerState.ManualBraking; break;
                 case "brakenotchstart": NotchStateType = ControllerState.BrakeNotch; break;
                 case "overchargestart": NotchStateType = ControllerState.Overcharge; break;
+                case "slowservicestart": NotchStateType = ControllerState.SlowService; break;
                 default:
                     STFException.TraceInformation(stf, "Skipped unknown notch type " + type);
                     break;