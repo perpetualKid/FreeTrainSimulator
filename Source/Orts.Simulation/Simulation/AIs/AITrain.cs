openrails/openrails

Refine names and null check

Source/Orts.Simulation/Simulation/AIs/AITrain.cs
@@ -703,19 +703,13 @@ namespace Orts.Simulation.AIs
                     return;
                 }
 
-                var leadingloco = Cars[0] as MSTSLocomotive;
-                if (leadingloco != null)
+                if (Cars[0] is MSTSLocomotive leadingLoco)
                 {
-                    if (leadingloco.Wiper) // wipers on and no rain, turn them off
-                    {
-                        if (Simulator.Weather.PricipitationIntensityPPSPM2 == 0)
-                            leadingloco.SignalEvent(Event.WiperOff);
-                    }
-                    else // wipers off and some rain, turn them on
-                    {
-                        if (Simulator.Weather.PricipitationIntensityPPSPM2 > 0)
-                            leadingloco.SignalEvent(Event.WiperOn);
-                    }
+                    var isRainingOrSnowing = Simulator.Weather.PricipitationIntensityPPSPM2 > 0;
+                    if (leadingLoco.Wiper && !isRainingOrSnowing)
+                        leadingLoco.SignalEvent(Event.WiperOff);
+                    else if (!leadingLoco.Wiper && isRainingOrSnowing)
+                        leadingLoco.SignalEvent(Event.WiperOn);
                 }
             }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 2da2976f831bc63cf443cce37400de0efac4ec73)

Source/Orts.Simulation/Simulation/AIs/AITrain.cs
@@ -575,16 +575,13 @@ namespace Orts.Simulation.AIs
                 }
 
                 // set wipers on or off
-                if (Cars[0] is MSTSLocomotive leadingloco)
+                if (Cars[0] is MSTSLocomotive leadingLoco)
                 {
-                    if (simulator.Weather.PrecipitationIntensityPPSPM2 > 0 && !leadingloco.Wiper)
-                    {
-                        leadingloco.SignalEvent(TrainEvent.WiperOn);
-                    }
-                    else if (simulator.Weather.PrecipitationIntensityPPSPM2 == 0 && leadingloco.Wiper)
-                    {
-                        leadingloco.SignalEvent(TrainEvent.WiperOff);
-                    }
+                    bool rainingOrSnowing = simulator.Weather.PrecipitationIntensityPPSPM2 > 0;
+                    if (leadingLoco.Wiper && !rainingOrSnowing)
+                        leadingLoco.SignalEvent(TrainEvent.WiperOff);
+                    else if (!leadingLoco.Wiper && rainingOrSnowing)
+                        leadingLoco.SignalEvent(TrainEvent.WiperOn);
                 }
             }
 