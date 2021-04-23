openrails/openrails

Further modification to straight vacuum brake

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -48,8 +48,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "runningstart": Type = ControllerState.Running; break;
                 case "selflapstart": Type = ControllerState.SelfLap; break;
                 case "holdstart": Type = ControllerState.Hold; break;
-                case "straightbrakingapplyonstart": Type = ControllerState.StrBrkApplyOn; break;
-                case "straightbrakingapplyoffstart": Type = ControllerState.StrBrkApplyOff; break;
+                case "straightbrakingreleaseonstart": Type = ControllerState.StrBrkReleaseOn; break;
+                case "straightbrakingreleaseoffstart": Type = ControllerState.StrBrkReleaseOff; break;
                 case "holdlappedstart": Type = ControllerState.Lap; break;
                 case "neutralhandleoffstart": Type = ControllerState.Neutral; break;
                 case "graduatedselflaplimitedstart": Type = ControllerState.GSelfLap; break;

perpetualKid/FreeTrainSimulator

(cherry picked from commit 84c1aac82ce44aaf0de60dcb5ef3e202772a4e93)

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -52,8 +52,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "runningstart": NotchStateType = ControllerState.Running; break;
                 case "selflapstart": NotchStateType = ControllerState.SelfLap; break;
                 case "holdstart": NotchStateType = ControllerState.Hold; break;
-                case "straightbrakingapplyonstart": NotchStateType = ControllerState.StraightApplyOn; break;
-                case "straightbrakingapplyoffstart": NotchStateType = ControllerState.StraightApplyOff; break;
+                case "straightbrakingreleaseonstart": NotchStateType = ControllerState.StraightReleaseOn; break;
+                case "straightbrakingreleaseoffstart": NotchStateType = ControllerState.StraightReleaseOff; break;
                 case "holdlappedstart": NotchStateType = ControllerState.Lap; break;
                 case "neutralhandleoffstart": NotchStateType = ControllerState.Neutral; break;
                 case "graduatedselflaplimitedstart": NotchStateType = ControllerState.GSelfLap; break;