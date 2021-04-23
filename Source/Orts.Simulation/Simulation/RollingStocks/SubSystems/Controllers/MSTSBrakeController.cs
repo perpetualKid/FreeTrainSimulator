openrails/openrails

Initial functionality for Eames (straight) vacuum brake.

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSBrakeController.cs
@@ -124,6 +124,11 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                             if (pressureBar > MaxPressureBar() - MinReductionBar()) pressureBar = MaxPressureBar() - MinReductionBar();
                             DecreasePressure(ref pressureBar, MaxPressureBar() - FullServReductionBar(), SlowApplicationRateBarpS(), elapsedClockSeconds);
                             break;
+                       case ControllerState.StrBrkApplyOn:
+                            // This position is an on position so pressure will be zero (reversed due to vacuum brake operation)
+                            pressureBar = 0;
+                            break;
+                        case ControllerState.StrBrkApplyOff: //(reversed due to vacuum brake operation)
                         case ControllerState.Apply:
                             pressureBar -= x * ApplyRateBarpS() * elapsedClockSeconds;
                             break;

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -48,6 +48,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "runningstart": Type = ControllerState.Running; break;
                 case "selflapstart": Type = ControllerState.SelfLap; break;
                 case "holdstart": Type = ControllerState.Hold; break;
+                case "straightbrakingapplyonstart": Type = ControllerState.StrBrkApplyOn; break;
+                case "straightbrakingapplyoffstart": Type = ControllerState.StrBrkApplyOff; break;
                 case "holdlappedstart": Type = ControllerState.Lap; break;
                 case "neutralhandleoffstart": Type = ControllerState.Neutral; break;
                 case "graduatedselflaplimitedstart": Type = ControllerState.GSelfLap; break;

perpetualKid/FreeTrainSimulator

(cherry picked from commit be90eff9e8851ea87f4c0b3049b6c519883e080f)

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSBrakeController.cs
@@ -141,6 +141,11 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                                 pressureBar = MaxPressureBar() - MinReductionBar();
                             pressureBar = DecreasePressure(pressureBar, MaxPressureBar() - FullServReductionBar(), SlowApplicationRateBarpS(), elapsedClockSeconds);
                             break;
+                       case ControllerState.StraightApplyOn:
+                            // This position is an on position so pressure will be zero (reversed due to vacuum brake operation)
+                            pressureBar = 0;
+                            break;
+                        case ControllerState.StraightApplyOff: //(reversed due to vacuum brake operation)
                         case ControllerState.Apply:
                             pressureBar -= x * ApplyRateBarpS() * elapsedClockSeconds;
                             break;

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -52,6 +52,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "runningstart": NotchStateType = ControllerState.Running; break;
                 case "selflapstart": NotchStateType = ControllerState.SelfLap; break;
                 case "holdstart": NotchStateType = ControllerState.Hold; break;
+                case "straightbrakingapplyonstart": NotchStateType = ControllerState.StraightApplyOn; break;
+                case "straightbrakingapplyoffstart": NotchStateType = ControllerState.StraightApplyOff; break;
                 case "holdlappedstart": NotchStateType = ControllerState.Lap; break;
                 case "neutralhandleoffstart": NotchStateType = ControllerState.Neutral; break;
                 case "graduatedselflaplimitedstart": NotchStateType = ControllerState.GSelfLap; break;