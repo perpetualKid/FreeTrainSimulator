026cdb0d30cea853be9cb504e70620d1b684fb8b
perpetualKid <perpetualKid@azuros.net>
2021-04-26 11:50:28 +0200

    fixing a merge issue

--- Source/Menu/Options.cs
+++ Source/Menu/Options.cs
@@ -98,7 +98,7 @@ namespace Orts.Menu
             checkAlerterExternal.Checked = this.settings.Alerter && !this.settings.AlerterDisableExternal;
             checkSpeedControl.Checked = this.settings.SpeedControl;
             checkConfirmations.Checked = !this.settings.SuppressConfirmations;
-            checkViewMapWindow.Checked = Settings.ViewDispatcher;
+            checkViewMapWindow.Checked = this.settings.ViewDispatcher;
             checkRetainers.Checked = this.settings.RetainersOnAllCars;
             checkGraduatedRelease.Checked = this.settings.GraduatedRelease;
             numericBrakePipeChargingRate.Value = this.settings.BrakePipeChargingRate;
@@ -286,7 +286,7 @@ namespace Orts.Menu
             settings.AlerterDisableExternal = !checkAlerterExternal.Checked;
             settings.SpeedControl = checkSpeedControl.Checked;
             settings.SuppressConfirmations = !checkConfirmations.Checked;
-            Settings.ViewDispatcher = checkViewMapWindow.Checked;
+            settings.ViewDispatcher = checkViewMapWindow.Checked;
             settings.RetainersOnAllCars = checkRetainers.Checked;
             settings.GraduatedRelease = checkGraduatedRelease.Checked;
             settings.BrakePipeChargingRate = (int)numericBrakePipeChargingRate.Value;
