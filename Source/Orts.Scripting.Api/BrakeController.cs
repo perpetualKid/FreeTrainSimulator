openrails/openrails

Move signal scripts to ORTS.Scripting.Script namespace

Source/Orts.Simulation/Common/Scripting/BrakeController.cs
@@ -22,7 +22,7 @@ using System.Collections.Generic;
 
 namespace ORTS.Scripting.Api
 {
-    public abstract class BrakeController : AbstractScriptClass
+    public abstract class BrakeController : AbstractTrainScriptClass
     {
         /// <summary>
         /// True if the driver has asked for an emergency braking (push button)

Source/Orts.Simulation/Common/Scripting/TrainControlSystem.cs
@@ -23,7 +23,7 @@ using Orts.Simulation.RollingStocks;
 
 namespace ORTS.Scripting.Api
 {
-    public abstract class TrainControlSystem : AbstractScriptClass
+    public abstract class TrainControlSystem : AbstractTrainScriptClass
     {
         public bool Activated { get; set; }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 4eb0bd73cf28881e4aa8a71364d486dafee37af2)

Source/Orts.Scripting.Api/BrakeController.cs
@@ -7,7 +7,7 @@ using Orts.Common;
 
 namespace Orts.Scripting.Api
 {
-    public abstract class BrakeController : ScriptBase
+    public abstract class BrakeController : TrainScriptBase
     {
         /// <summary>
         /// True if the driver has asked for an emergency braking (push button)

Source/Orts.Scripting.Api/TrainControlSystem.cs
@@ -5,7 +5,7 @@ using Orts.Common;
 
 namespace Orts.Scripting.Api
 {
-    public abstract class TrainControlSystem : ScriptBase
+    public abstract class TrainControlSystem : TrainScriptBase
     {
         public bool Activated { get; set; }
 