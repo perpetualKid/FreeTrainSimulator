openrails/openrails

Restore direction setting code

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -1587,7 +1587,7 @@ namespace Orts.Simulation.RollingStocks
                 if (f > 0 && PowerOn)
                 {
                     DynamicBrakeForceN = f * (1 - PowerReduction);
-                    MotiveForceN -= (AbsTractionSpeedMpS > 0 ? 1 : AbsTractionSpeedMpS < 0 ? -1 : Direction == Direction.Reverse ? -1 : 1) * DynamicBrakeForceN;                 
+                    MotiveForceN -= (SpeedMpS > 0 ? 1 : SpeedMpS < 0 ? -1 : Direction == Direction.Reverse ? -1 : 1) * DynamicBrakeForceN;                 
                 }
                 else
                 {

perpetualKid/FreeTrainSimulator

(cherry picked from commit 45115e476c17208c9c0e5e28678bd0cde481da8a)

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -1619,7 +1619,7 @@ namespace Orts.Simulation.RollingStocks
                 if (f > 0 && PowerOn)
                 {
                     DynamicBrakeForceN = f * (1 - PowerReduction);
-                    MotiveForceN -= (AbsTractionSpeedMpS > 0 ? 1 : AbsTractionSpeedMpS < 0 ? -1 : Direction == MidpointDirection.Reverse ? -1 : 1) * DynamicBrakeForceN;
+                    MotiveForceN -= (SpeedMpS > 0 ? 1 : SpeedMpS < 0 ? -1 : Direction == MidpointDirection.Reverse ? -1 : 1) * DynamicBrakeForceN;
                 }
                 else
                 {