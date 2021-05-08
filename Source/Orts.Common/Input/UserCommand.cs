openrails/openrails

Battery and PowerKey cab controls, https://trello.com/c/G4tuC0wc/468-ortsbattery https://trello.com/c/JbeOzUT7/247-power-key-cab-view-control

Source/ORTS.Common/Input/UserCommand.cs
@@ -199,5 +199,7 @@
         [GetString("Control AI Fire On")] ControlAIFireOn,
         [GetString("Control AI Fire Off")] ControlAIFireOff,
         [GetString("Control AI Fire Reset")] ControlAIFireReset,
+        [GetString("Control Battery")] ControlBattery,
+        [GetString("Control PowerKey")] ControlPowerKey,
     }
 }

perpetualKid/FreeTrainSimulator

(cherry picked from commit dd2cdf513b54f5d02657f2a7ac0c4c65daa3b267)

Source/Orts.Common/Input/UserCommand.cs
@@ -204,6 +204,8 @@ namespace Orts.Common.Input
         [Description("Control AI Fire On")] ControlAIFireOn,
         [Description("Control AI Fire Off")] ControlAIFireOff,
         [Description("Control AI Fire Reset")] ControlAIFireReset,
+        [Description("Control Battery")] ControlBattery,
+        [Description("Control PowerKey")] ControlPowerKey,
     }
 
     /// <summary>