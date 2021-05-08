openrails/openrails

slight tweaks to text aspect addition
Use 'var', a more descriptive variable name, and the newer string interpolation syntax.

Source/RunActivity/Viewer3D/Popups/SignallingDebugWindow.cs
@@ -215,14 +215,14 @@ namespace Orts.Viewer3D.Popups
                             }
                             else if (signalObj != null)
                             {
-                                string aspects = string.Join(" / ", signalObj.Signal.SignalHeads.Select(
-                                    x => x.state.ToString() + (x.TextSignalAspect.Length > 0 ? $" ({x.TextSignalAspect})" : string.Empty)
+                                var aspects = string.Join(" / ", signalObj.Signal.SignalHeads.Select(
+                                    head => $"{head.state}" + (head.TextSignalAspect.Length > 0 ? $" ({head.TextSignalAspect})" : string.Empty)
                                     ));
                                 primitives.Add(new DispatcherLabel(currentPosition.WorldLocation,
                                            GetAspect(signalObj.Signal) == DebugWindowSignalAspect.Stop ? Color.Red :
                                                GetAspect(signalObj.Signal) == DebugWindowSignalAspect.Warning ? Color.Yellow :
                                                Color.Green,
-                                           String.Format("Signal {0} ({1})", signalObj.Signal.thisRef, aspects),
+                                           $"Signal {signalObj.Signal.thisRef} ({aspects})",
                                            Owner.TextFontDefaultOutlined));
                             }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 43629bd36e1949f407f837984bd03caa4519a372)

Source/ActivityRunner/Viewer3D/Popups/SignallingDebugWindow.cs
@@ -221,8 +221,8 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                             }
                             else if (signalObj != null)
                             {
-                                string aspects = string.Join(" / ", signalObj.Signal.SignalHeads.Select(
-                                    x => x.SignalIndicationState.ToString() + (x.TextSignalAspect.Length > 0 ? $" ({x.TextSignalAspect})" : string.Empty)
+                                var aspects = string.Join(" / ", signalObj.Signal.SignalHeads.Select(
+                                    head => $"{head.SignalIndicationState}" + (head.TextSignalAspect.Length > 0 ? $" ({head.TextSignalAspect})" : string.Empty)
                                     ));
                                 primitives.Add(new DispatcherLabel(currentPosition.WorldLocation,
                                            GetAspect(signalObj.Signal) == DebugWindowSignalAspect.Stop ? Color.Red :