openrails/openrails

Bug fix for https://bugs.launchpad.net/or/+bug/1927811 Dynamic weather not working

Source/RunActivity/Viewer3D/Weather.cs
@@ -517,8 +517,8 @@ namespace Orts.Viewer3D
                 {
                     dynamicWeather = new DynamicWeather();
                 }
-                var weatherChange = Program.Simulator.ActivityRun.triggeredEventWrapper.ParsedObject.Outcomes.ORTSWeatherChange != null ?
-                    Program.Simulator.ActivityRun.triggeredEventWrapper.ParsedObject.Outcomes.ORTSWeatherChange : Program.Simulator.ActivityRun.triggeredEventWrapper.ParsedObject.ORTSWeatherChange;
+                var weatherChange = Program.Simulator.ActivityRun.triggeredEventWrapper.ParsedObject.ORTSWeatherChange != null ?
+                    Program.Simulator.ActivityRun.triggeredEventWrapper.ParsedObject.ORTSWeatherChange : Program.Simulator.ActivityRun.triggeredEventWrapper.ParsedObject.Outcomes.ORTSWeatherChange;
                 dynamicWeather.WeatherChange_Init(weatherChange, this);
                 Program.Simulator.ActivityRun.triggeredEventWrapper = null;               
             }

perpetualKid/FreeTrainSimulator

(cherry picked from commit 5ff818fc65b2721737a6cbcd85bb3d17977c9bb4)

Source/ActivityRunner/Viewer3D/Weather.cs
@@ -532,8 +532,8 @@ namespace Orts.ActivityRunner.Viewer3D
                 {
                     dynamicWeather = new DynamicWeather();
                 }
-                var weatherChange = Simulator.Instance.ActivityRun.triggeredEventWrapper.ParsedObject.Outcomes.WeatherChange != null ?
-                    Simulator.Instance.ActivityRun.triggeredEventWrapper.ParsedObject.Outcomes.WeatherChange : Simulator.Instance.ActivityRun.triggeredEventWrapper.ParsedObject.WeatherChange;
+                var weatherChange = Simulator.Instance.ActivityRun.triggeredEventWrapper.ParsedObject.WeatherChange != null ?
+                    Simulator.Instance.ActivityRun.triggeredEventWrapper.ParsedObject.WeatherChange : Simulator.Instance.ActivityRun.triggeredEventWrapper.ParsedObject.Outcomes.WeatherChange;
                 dynamicWeather.WeatherChange_Init(weatherChange, this);
                 Simulator.Instance.ActivityRun.triggeredEventWrapper = null;
             }