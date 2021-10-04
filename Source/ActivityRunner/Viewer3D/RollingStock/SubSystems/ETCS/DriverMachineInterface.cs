openrails/openrails

Added DisabledIfCabPowerSupplyOff and DisabledIfLowVoltagePowerSupplyOff parameters to ETCS DMI

Source/RunActivity/Viewer3D/RollingStock/SubSystems/ETCS/DriverMachineInterface.cs
@@ -765,28 +765,32 @@ namespace Orts.Viewer3D.RollingStock.Subsystems.ETCS
 
         public override void PrepareFrame(RenderFrame frame, ElapsedTime elapsedTime)
         {
-            base.PrepareFrame(frame, elapsedTime);
-            var xScale = Viewer.CabWidthPixels / 640f;
-            var yScale = Viewer.CabHeightPixels / 480f;
-            DrawPosition.X = (int)(Position.X * xScale) - Viewer.CabXOffsetPixels + Viewer.CabXLetterboxPixels;
-            DrawPosition.Y = (int)(Position.Y * yScale) + Viewer.CabYOffsetPixels + Viewer.CabYLetterboxPixels;
-            DrawPosition.Width = (int)(Control.Width * xScale);
-            DrawPosition.Height = (int)(Control.Height * yScale);
-            if (Zoomed)
+            if ((!Control.DisabledIfLowVoltagePowerSupplyOff || Locomotive.LocomotivePowerSupply.LowVoltagePowerSupplyOn) &&
+                (!Control.DisabledIfCabPowerSupplyOff || Locomotive.LocomotivePowerSupply.CabPowerSupplyOn))
             {
-                DrawPosition.Width = 640;
-                DrawPosition.Height = 480;
-                DMI.SizeTo(DrawPosition.Width, DrawPosition.Height);
-                DrawPosition.X -= 320;
-                DrawPosition.Y -= 240;
-                DMI.ETCSDefaultWindow.BackgroundColor = ColorBackground;
+                base.PrepareFrame(frame, elapsedTime);
+                var xScale = Viewer.CabWidthPixels / 640f;
+                var yScale = Viewer.CabHeightPixels / 480f;
+                DrawPosition.X = (int)(Position.X * xScale) - Viewer.CabXOffsetPixels + Viewer.CabXLetterboxPixels;
+                DrawPosition.Y = (int)(Position.Y * yScale) + Viewer.CabYOffsetPixels + Viewer.CabYLetterboxPixels;
+                DrawPosition.Width = (int)(Control.Width * xScale);
+                DrawPosition.Height = (int)(Control.Height * yScale);
+                if (Zoomed)
+                {
+                    DrawPosition.Width = 640;
+                    DrawPosition.Height = 480;
+                    DMI.SizeTo(DrawPosition.Width, DrawPosition.Height);
+                    DrawPosition.X -= 320;
+                    DrawPosition.Y -= 240;
+                    DMI.ETCSDefaultWindow.BackgroundColor = ColorBackground;
+                }
+                else
+                {
+                    DMI.SizeTo(DrawPosition.Width, DrawPosition.Height);
+                    DMI.ETCSDefaultWindow.BackgroundColor = Color.Transparent;
+                }
+                DMI.PrepareFrame(elapsedTime.ClockSeconds);
             }
-            else
-            {
-                DMI.SizeTo(DrawPosition.Width, DrawPosition.Height);
-                DMI.ETCSDefaultWindow.BackgroundColor = Color.Transparent;
-            }
-            DMI.PrepareFrame(elapsedTime.ClockSeconds);
         }
 
         public bool IsMouseWithin()

perpetualKid/FreeTrainSimulator

(cherry picked from commit b3f26c826be39da0096828e2a95efb0260dda715)

Source/ActivityRunner/Viewer3D/RollingStock/SubSystems/ETCS/DriverMachineInterface.cs
@@ -1,4 +1,4 @@
-﻿// COPYRIGHT 2014 by the Open Rails project.
+// COPYRIGHT 2014 by the Open Rails project.
 // 
 // This file is part of Open Rails.
 // 
@@ -827,26 +827,31 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock.SubSystems.Etcs
 
         public override void PrepareFrame(RenderFrame frame, in ElapsedTime elapsedTime)
         {
-            base.PrepareFrame(frame, elapsedTime);
-            var xScale = Viewer.CabWidthPixels / 640f;
-            var yScale = Viewer.CabHeightPixels / 480f;
-            DrawPosition.X = (int)(Position.X * xScale) - Viewer.CabXOffsetPixels + Viewer.CabXLetterboxPixels;
-            DrawPosition.Y = (int)(Position.Y * yScale) + Viewer.CabYOffsetPixels + Viewer.CabYLetterboxPixels;
-            DrawPosition.Width = (int)(Control.Bounds.Width * xScale);
-            DrawPosition.Height = (int)(Control.Bounds.Height * yScale);
-            if (Zoomed)
+            if ((!Control.DisabledIfLowVoltagePowerSupplyOff || Locomotive.LocomotivePowerSupply.LowVoltagePowerSupplyOn) &&
+                (!Control.DisabledIfCabPowerSupplyOff || Locomotive.LocomotivePowerSupply.CabPowerSupplyOn))
             {
-                DrawPosition.Width = 640;
-                DrawPosition.Height = 480;
+                base.PrepareFrame(frame, elapsedTime);
+                var xScale = Viewer.CabWidthPixels / 640f;
+                var yScale = Viewer.CabHeightPixels / 480f;
+                DrawPosition.X = (int)(Position.X * xScale) - Viewer.CabXOffsetPixels + Viewer.CabXLetterboxPixels;
+                DrawPosition.Y = (int)(Position.Y * yScale) + Viewer.CabYOffsetPixels + Viewer.CabYLetterboxPixels;
+                DrawPosition.Width = (int)(Control.Bounds.Width * xScale);
+                DrawPosition.Height = (int)(Control.Bounds.Height * yScale);
+                if (Zoomed)
+                {
+                    DrawPosition.Width = 640;
+                    DrawPosition.Height = 480;
                 driverMachineInterface.SizeTo(DrawPosition.Width, DrawPosition.Height);
-                DrawPosition.X -= 320;
-                DrawPosition.Y -= 240;
+                    DrawPosition.X -= 320;
+                    DrawPosition.Y -= 240;
                 driverMachineInterface.ETCSDefaultWindow.BackgroundColor = ColorBackground;
-            }
-            else
-            {
+                }
+                else
+                {
                 driverMachineInterface.SizeTo(DrawPosition.Width, DrawPosition.Height);
                 driverMachineInterface.ETCSDefaultWindow.BackgroundColor = Color.Transparent;
+                }
+                driverMachineInterface.PrepareFrame(elapsedTime.ClockSeconds);
             }
         }
 