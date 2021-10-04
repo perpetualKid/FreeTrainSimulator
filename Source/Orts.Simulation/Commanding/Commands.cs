openrails/openrails

refactor: use briefer ternary operators

Source/Orts.Simulation/Common/Commands.cs
@@ -1153,29 +1153,13 @@ namespace Orts.Common
         {
             if (Receiver?.Mode == BatterySwitch.ModeType.Switch)
             {
-                if (ToState)
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.CloseBatterySwitch);
-                }
-                else
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.OpenBatterySwitch);
-                }
+                Receiver?.HandleEvent(ToState ? PowerSupplyEvent.CloseBatterySwitch : PowerSupplyEvent.OpenBatterySwitch);
             }
             else if (Receiver?.Mode == BatterySwitch.ModeType.PushButtons)
             {
-                if (ToState)
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.CloseBatterySwitchButtonPressed);
-                    Receiver?.Update(0f);
-                    Receiver?.HandleEvent(PowerSupplyEvent.CloseBatterySwitchButtonReleased);
-                }
-                else
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.OpenBatterySwitchButtonPressed);
-                    Receiver?.Update(0f);
-                    Receiver?.HandleEvent(PowerSupplyEvent.OpenBatterySwitchButtonReleased);
-                }
+                Receiver?.HandleEvent(ToState ? PowerSupplyEvent.CloseBatterySwitchButtonPressed : PowerSupplyEvent.OpenBatterySwitchButtonPressed);
+                Receiver?.Update(0f);
+                Receiver?.HandleEvent(ToState ? PowerSupplyEvent.CloseBatterySwitchButtonReleased : PowerSupplyEvent.OpenBatterySwitchButtonReleased);
             }
         }
     }

perpetualKid/FreeTrainSimulator

(cherry picked from commit ff1e7280dc1bb6b26319ff6fb363c24a848e1428)

Source/Orts.Simulation/Commanding/Commands.cs
@@ -1199,29 +1199,13 @@ namespace Orts.Simulation.Commanding
         {
             if (Receiver?.Mode == BatterySwitch.ModeType.Switch)
             {
-                if (targetState)
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.CloseBatterySwitch);
-                }
-                else
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.OpenBatterySwitch);
-                }
+                Receiver?.HandleEvent(targetState ? PowerSupplyEvent.CloseBatterySwitch : PowerSupplyEvent.OpenBatterySwitch);
             }
             else if (Receiver?.Mode == BatterySwitch.ModeType.PushButtons)
             {
-                if (targetState)
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.CloseBatterySwitchButtonPressed);
-                    Receiver?.Update(0f);
-                    Receiver?.HandleEvent(PowerSupplyEvent.CloseBatterySwitchButtonReleased);
-                }
-                else
-                {
-                    Receiver?.HandleEvent(PowerSupplyEvent.OpenBatterySwitchButtonPressed);
-                    Receiver?.Update(0f);
-                    Receiver?.HandleEvent(PowerSupplyEvent.OpenBatterySwitchButtonReleased);
-                }
+                Receiver?.HandleEvent(targetState ? PowerSupplyEvent.CloseBatterySwitchButtonPressed : PowerSupplyEvent.OpenBatterySwitchButtonPressed);
+                Receiver?.Update(0f);
+                Receiver?.HandleEvent(targetState ? PowerSupplyEvent.CloseBatterySwitchButtonReleased : PowerSupplyEvent.OpenBatterySwitchButtonReleased);
             }
         }
     }