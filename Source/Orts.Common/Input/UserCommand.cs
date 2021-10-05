openrails/openrails

Added a reset command in order to reset a TCS emergency braking triggered by the simulator https://trello.com/c/QvCgMnVW/256-command-to-release-train-control-system-emergency-braking

Source/ORTS.Common/Input/UserCommand.cs
@@ -25,6 +25,7 @@
         [GetString("Game Multi Player Dispatcher")] GameMultiPlayerDispatcher,
         [GetString("Game Multi Player Texting")] GameMultiPlayerTexting,
         [GetString("Game Switch Manual Mode")] GameSwitchManualMode,
+        [GetString("Game Reset Out Of Control Mode")] GameResetOutOfControlMode,
         [GetString("Game Clear Signal Forward")] GameClearSignalForward,
         [GetString("Game Clear Signal Backward")] GameClearSignalBackward,
         [GetString("Game Reset Signal Forward")] GameResetSignalForward,

Source/Orts.Simulation/Common/Commands.cs
@@ -510,26 +510,40 @@ namespace Orts.Common
 
         public override void Redo() {
             Receiver.UnconditionalInitializeBrakes();
-            // Report();
         }
     }
 
     [Serializable()]
-    public sealed class EmergencyPushButtonCommand : Command
+    public sealed class ResetOutOfControlModeCommand : Command
     {
-        public static MSTSLocomotive Receiver { get; set; }
-
-        public EmergencyPushButtonCommand(CommandLog log)
-            : base(log)
+        public static Train Receiver { get; set; }
+        public ResetOutOfControlModeCommand(CommandLog log) : base(log)
         {
             Redo();
         }
 
         public override void Redo()
         {
-            Receiver.EmergencyButtonPressed = !Receiver.EmergencyButtonPressed;
-            Receiver.TrainBrakeController.EmergencyBrakingPushButton = Receiver.EmergencyButtonPressed;
-            // Report();
+            Receiver.ManualResetOutOfControlMode();
+            Receiver.SignalEvent(TCSEvent.ManualResetOutOfControlMode);
+        }
+    }
+
+    [Serializable()]
+    public sealed class EmergencyPushButtonCommand : BooleanCommand
+    {
+        public static MSTSLocomotive Receiver { get; set; }
+
+        public EmergencyPushButtonCommand(CommandLog log, bool toState)
+            : base(log, toState)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.EmergencyButtonPressed = ToState;
+            Receiver.TrainBrakeController.EmergencyBrakingPushButton = ToState;
         }
     }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 81e0339e2ba87b0548d1f39c95436fd78925ae10)

Source/Orts.Common/Input/UserCommand.cs
@@ -29,6 +29,7 @@ namespace Orts.Common.Input
         [Description("Game Multi Player Dispatcher")] GameMultiPlayerDispatcher,
         [Description("Game Multi Player Texting")] GameMultiPlayerTexting,
         [Description("Game Switch Manual Mode")] GameSwitchManualMode,
+        [Description("Game Reset Out Of Control Mode")] GameResetOutOfControlMode,
         [Description("Game Clear Signal Forward")] GameClearSignalForward,
         [Description("Game Clear Signal Backward")] GameClearSignalBackward,
         [Description("Game Reset Signal Forward")] GameResetSignalForward,

Source/Orts.Simulation/Commanding/Commands.cs
@@ -647,20 +647,36 @@ namespace Orts.Simulation.Commanding
     }
 
     [Serializable()]
-    public sealed class EmergencyPushButtonCommand : Command
+    public sealed class ResetOutOfControlModeCommand : Command
     {
-        public static MSTSLocomotive Receiver { get; set; }
-
-        public EmergencyPushButtonCommand(CommandLog log)
-            : base(log)
+        public static Train Receiver { get; set; }
+        public ResetOutOfControlModeCommand(CommandLog log) : base(log)
         {
             Redo();
         }
 
         public override void Redo()
         {
-            Receiver.EmergencyButtonPressed = !Receiver.EmergencyButtonPressed;
-            Receiver.TrainBrakeController.EmergencyBrakingPushButton = Receiver.EmergencyButtonPressed;
+            Receiver.ManualResetOutOfControlMode();
+            Receiver.SignalEvent(TCSEvent.ManualResetOutOfControlMode);
+        }
+    }
+
+    [Serializable()]
+    public sealed class EmergencyPushButtonCommand : BooleanCommand
+    {
+        public static MSTSLocomotive Receiver { get; set; }
+
+        public EmergencyPushButtonCommand(CommandLog log, bool toState)
+            : base(log, toState)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.EmergencyButtonPressed = targetState;
+            Receiver.TrainBrakeController.EmergencyBrakingPushButton = targetState;
         }
     }
 