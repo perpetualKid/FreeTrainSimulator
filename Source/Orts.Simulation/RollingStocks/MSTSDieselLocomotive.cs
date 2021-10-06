f909a9eff84a6bf17bc83547d793cc571594b8e2
perpetualKid <perpetualKid@azuros.net>
2021-10-06 14:41:35 +0200

    Update MSTSDieselLocomotive.cs

--- Source/Orts.Simulation/RollingStocks/MSTSDieselLocomotive.cs
+++ Source/Orts.Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -572,7 +572,7 @@ namespace Orts.Simulation.RollingStocks
             // Advanced configuration (TF table) - use a user defined tractive force table
             // With Simple adhesion apart from correction for rail adhesion, there is no further variation to the motive force. 
             // With Advanced adhesion the raw motive force is fed into the advanced (axle) adhesion model, and is corrected for wheel slip and rail adhesion
-            if (LocomotivePowerSupply.MainPowerSupplyOn && Direction != Direction.N)
+            if (LocomotivePowerSupply.MainPowerSupplyOn && Direction != MidpointDirection.N)
             {
                 // Appartent throttle setting is a reverse lookup of the throttletab vs rpm, hence motive force increase will be related to increase in rpm. The minimum of the two values
                 // is checked to enable fast reduction in tractive force when decreasing the throttle. Typically it will take longer for the prime mover to decrease rpm then drop motive force.
