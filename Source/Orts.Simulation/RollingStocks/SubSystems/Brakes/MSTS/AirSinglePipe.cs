openrails/openrails

Add sound event for brake overcharging

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Brakes/MSTS/AirSinglePipe.cs
@@ -608,6 +608,23 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Brakes.MSTS
                 }
                 prevCylPressurePSI = AutoCylPressurePSI;
                 prevBrakePipePressurePSI = BrakeLine1PressurePSI;
+
+                var lead = Car as MSTSLocomotive;
+
+                if (lead != null && Car.WagonType == MSTSWagon.WagonTypes.Engine)
+                {
+                    if (lead.TrainBrakeController.TrainBrakeControllerState == ControllerState.Overcharge && !lead.BrakeOverchargeSoundOn)
+                    {
+                        Car.SignalEvent(Event.OverchargeBrakingOn);
+                        lead.BrakeOverchargeSoundOn = true;
+                    }
+                    else if (lead.TrainBrakeController.TrainBrakeControllerState != ControllerState.Overcharge && lead.BrakeOverchargeSoundOn)
+                    {
+                        Car.SignalEvent(Event.OverchargeBrakingOff);
+                        lead.BrakeOverchargeSoundOn = false;
+                    }
+                }
+
             }
             SoundTriggerCounter = SoundTriggerCounter + elapsedClockSeconds;
         }

perpetualKid/FreeTrainSimulator

(cherry picked from commit ee6ca687ca483e25faccc80ba26378fb12924496)

Source/Orts.Simulation/RollingStocks/SubSystems/Brakes/MSTS/AirSinglePipe.cs
@@ -611,6 +611,23 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Brakes.MSTS
                 }
                 prevCylPressurePSI = AutoCylPressurePSI;
                 prevBrakePipePressurePSI = BrakeLine1PressurePSI;
+
+                var lead = Car as MSTSLocomotive;
+
+                if (lead != null && Car.WagonType == MSTSWagon.WagonTypes.Engine)
+                {
+                    if (lead.TrainBrakeController.TrainBrakeControllerState == ControllerState.Overcharge && !lead.BrakeOverchargeSoundOn)
+                    {
+                        Car.SignalEvent(TrainEvent.OverchargeBrakingOn);
+                        lead.BrakeOverchargeSoundOn = true;
+                    }
+                    else if (lead.TrainBrakeController.TrainBrakeControllerState != ControllerState.Overcharge && lead.BrakeOverchargeSoundOn)
+                    {
+                        Car.SignalEvent(TrainEvent.OverchargeBrakingOff);
+                        lead.BrakeOverchargeSoundOn = false;
+                    }
+                }
+
             }
             SoundTriggerCounter = SoundTriggerCounter + (float)elapsedClockSeconds;
         }