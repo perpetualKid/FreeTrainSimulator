openrails/openrails

Adjusts the window bottom limit.

Source/RunActivity/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -408,7 +408,8 @@ namespace Orts.Viewer3D.Popups
                 FirstColLenght = labels.Max(x => x.FirstColWidth);
                 LastColLenght = labels.Max(x => x.LastColWidth);
 
-                var rowCount = labels.Where(x => x.FirstCol == "Sprtr" || x.LastCol != "").Count();
+                // Valid rows
+                var rowCount = labels.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - 1;
                 var desiredHeight = FontToBold ? Owner.TextFontDefaultBold.Height * rowCount
                     : Owner.TextFontDefault.Height * rowCount;
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 6a3f1a60aed353841c228568f09a7694271f5855)

Source/ActivityRunner/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -586,7 +586,8 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                 FirstColLenght = listToLabel.Max(x => x.FirstColWidth);
                 LastColLenght = listToLabel.Max(x => x.LastColWidth);
 
-                var rowCount = listToLabel.Where(x => x.FirstCol == "Sprtr" || !string.IsNullOrEmpty(x.LastCol)).Count();
+                // Valid rows
+                var rowCount = listToLabel.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - 1;
                 var desiredHeight = FontToBold ? Owner.TextFontDefaultBold.Height * rowCount
                     : Owner.TextFontDefault.Height * rowCount;
 