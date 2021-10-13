openrails/openrails

Optimizes the window height using the Windows DPI settings.

Source/RunActivity/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -408,8 +408,9 @@ namespace Orts.Viewer3D.Popups
                 FirstColLenght = labels.Max(x => x.FirstColWidth);
                 LastColLenght = labels.Max(x => x.LastColWidth);
 
-                // Valid rows
-                var rowCount = labels.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - 1;
+                // Validates rows with windows DPI settings
+                var dpiOffset = (System.Drawing.Graphics.FromHwnd(IntPtr.Zero).DpiY / 96) > 1.00f ? 1 : 0;// values from testing
+                var rowCount = labels.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - dpiOffset;
                 var desiredHeight = FontToBold ? Owner.TextFontDefaultBold.Height * rowCount
                     : Owner.TextFontDefault.Height * rowCount;
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 0d9e793610540d5a1705a25a74adf4b95efd7c86)

Source/ActivityRunner/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -587,7 +587,9 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                 LastColLenght = listToLabel.Max(x => x.LastColWidth);
 
                 // Valid rows
-                var rowCount = listToLabel.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - 1;
+                // Validates rows with windows DPI settings
+                var dpiOffset = (System.Drawing.Graphics.FromHwnd(IntPtr.Zero).DpiY / 96) > 1.00f ? 1 : 0;// values from testing
+                var rowCount = listToLabel.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - dpiOffset;
                 var desiredHeight = FontToBold ? Owner.TextFontDefaultBold.Height * rowCount
                     : Owner.TextFontDefault.Height * rowCount;
 