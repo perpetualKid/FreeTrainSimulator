openrails/openrails

Fix crash when a dictionary key is duplicated.

Source/RunActivity/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -25,7 +25,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using System.Windows.Forms;
 
 namespace Orts.Viewer3D.Popups
 {
@@ -106,57 +105,58 @@ namespace Orts.Viewer3D.Popups
 
         private static readonly Dictionary<string, string> FirstColToAbbreviated = new Dictionary<string, string>()
         {
-            {Viewer.Catalog.GetString("AI Fireman"), Viewer.Catalog.GetString("AIFR")},
-            {Viewer.Catalog.GetString("Autopilot"), Viewer.Catalog.GetString("AUTO")},
-            {Viewer.Catalog.GetString("Battery switch"), Viewer.Catalog.GetString("BATT")},
-            {Viewer.Catalog.GetString("Boiler pressure"), Viewer.Catalog.GetString("PRES")},
-            {Viewer.Catalog.GetString("Boiler water glass"), Viewer.Catalog.GetString("WATR")},
-            {Viewer.Catalog.GetString("Boiler water level"), Viewer.Catalog.GetString("LEVL")},
-            {Viewer.Catalog.GetString("Circuit breaker"), Viewer.Catalog.GetString("CIRC")},
-            {Viewer.Catalog.GetString("Cylinder cocks"), Viewer.Catalog.GetString("CCOK")},
-            {Viewer.Catalog.GetString("Direction"), Viewer.Catalog.GetString("DIRC")},
-            {Viewer.Catalog.GetString("Doors open"), Viewer.Catalog.GetString("DOOR")},
-            {Viewer.Catalog.GetString("Dynamic brake"), Viewer.Catalog.GetString("BDYN")},
-            {Viewer.Catalog.GetString("Electric train supply"), Viewer.Catalog.GetString("TSUP")},
-            {Viewer.Catalog.GetString("Engine brake"), Viewer.Catalog.GetString("BLOC")},
-            {Viewer.Catalog.GetString("Engine"), Viewer.Catalog.GetString("ENGN")},
-            {Viewer.Catalog.GetString("Fire mass"), Viewer.Catalog.GetString("FIRE")},
-            {Viewer.Catalog.GetString("Fixed gear"), Viewer.Catalog.GetString("GEAR")},
-            {Viewer.Catalog.GetString("Fuel levels"), Viewer.Catalog.GetString("FUEL")},
-            {Viewer.Catalog.GetString("Gear"), Viewer.Catalog.GetString("GEAR")},
-            {Viewer.Catalog.GetString("Gradient"), Viewer.Catalog.GetString("GRAD")},
-            {Viewer.Catalog.GetString("Grate limit"), Viewer.Catalog.GetString("GRAT")},
-            {Viewer.Catalog.GetString("Master key"), Viewer.Catalog.GetString("MAST")},
-            {Viewer.Catalog.GetString("Pantographs"), Viewer.Catalog.GetString("PANT")},
-            {Viewer.Catalog.GetString("Power"), Viewer.Catalog.GetString("POWR")},
-            {Viewer.Catalog.GetString("Regulator"), Viewer.Catalog.GetString("REGL")},
-            {Viewer.Catalog.GetString("Replay"), Viewer.Catalog.GetString("RPLY")},
-            {Viewer.Catalog.GetString("Retainers"), Viewer.Catalog.GetString("RETN")},
-            {Viewer.Catalog.GetString("Reverser"), Viewer.Catalog.GetString("REVR")},
-            {Viewer.Catalog.GetString("Sander"), Viewer.Catalog.GetString("SAND")},
-            {Viewer.Catalog.GetString("Speed"), Viewer.Catalog.GetString("SPED")},
-            {Viewer.Catalog.GetString("Steam usage"), Viewer.Catalog.GetString("STEM")},
-            {Viewer.Catalog.GetString("Throttle"), Viewer.Catalog.GetString("THRO")},
-            {Viewer.Catalog.GetString("Time"), Viewer.Catalog.GetString("TIME")},
-            {Viewer.Catalog.GetString("Traction cut-off relay"), Viewer.Catalog.GetString("TRAC")},
-            {Viewer.Catalog.GetString("Train brake"), Viewer.Catalog.GetString("BTRN")},
-            {Viewer.Catalog.GetString("Wheel"), Viewer.Catalog.GetString("WHEL")}
+            [Viewer.Catalog.GetString("AI Fireman")] = Viewer.Catalog.GetString("AIFR"),
+            [Viewer.Catalog.GetString("Autopilot")] = Viewer.Catalog.GetString("AUTO"),
+            [Viewer.Catalog.GetString("Battery switch")] = Viewer.Catalog.GetString("BATT"),
+            [Viewer.Catalog.GetString("Boiler pressure")] = Viewer.Catalog.GetString("PRES"),
+            [Viewer.Catalog.GetString("Boiler water glass")] = Viewer.Catalog.GetString("WATR"),
+            [Viewer.Catalog.GetString("Boiler water level")] = Viewer.Catalog.GetString("LEVL"),
+            [Viewer.Catalog.GetString("Circuit breaker")] = Viewer.Catalog.GetString("CIRC"),
+            [Viewer.Catalog.GetString("Cylinder cocks")] = Viewer.Catalog.GetString("CCOK"),
+            [Viewer.Catalog.GetString("Direction")] = Viewer.Catalog.GetString("DIRC"),
+            [Viewer.Catalog.GetString("Doors open")] = Viewer.Catalog.GetString("DOOR"),
+            [Viewer.Catalog.GetString("Dynamic brake")] = Viewer.Catalog.GetString("BDYN"),
+            [Viewer.Catalog.GetString("Electric train supply")] = Viewer.Catalog.GetString("TSUP"),
+            [Viewer.Catalog.GetString("Engine brake")] = Viewer.Catalog.GetString("BLOC"),
+            [Viewer.Catalog.GetString("Engine")] = Viewer.Catalog.GetString("ENGN"),
+            [Viewer.Catalog.GetString("Fire mass")] = Viewer.Catalog.GetString("FIRE"),
+            [Viewer.Catalog.GetString("Fixed gear")] = Viewer.Catalog.GetString("GEAR"),
+            [Viewer.Catalog.GetString("Fuel levels")] = Viewer.Catalog.GetString("FUEL"),
+            [Viewer.Catalog.GetString("Gear")] = Viewer.Catalog.GetString("GEAR"),
+            [Viewer.Catalog.GetString("Gradient")] = Viewer.Catalog.GetString("GRAD"),
+            [Viewer.Catalog.GetString("Grate limit")] = Viewer.Catalog.GetString("GRAT"),
+            [Viewer.Catalog.GetString("Master key")] = Viewer.Catalog.GetString("MAST"),
+            [Viewer.Catalog.GetString("Pantographs")] = Viewer.Catalog.GetString("PANT"),
+            [Viewer.Catalog.GetString("Power")] = Viewer.Catalog.GetString("POWR"),
+            [Viewer.Catalog.GetString("Regulator")] = Viewer.Catalog.GetString("REGL"),
+            [Viewer.Catalog.GetString("Replay")] = Viewer.Catalog.GetString("RPLY"),
+            [Viewer.Catalog.GetString("Retainers")] = Viewer.Catalog.GetString("RETN"),
+            [Viewer.Catalog.GetString("Reverser")] = Viewer.Catalog.GetString("REVR"),
+            [Viewer.Catalog.GetString("Sander")] = Viewer.Catalog.GetString("SAND"),
+            [Viewer.Catalog.GetString("Speed")] = Viewer.Catalog.GetString("SPED"),
+            [Viewer.Catalog.GetString("Steam usage")] = Viewer.Catalog.GetString("STEM"),
+            [Viewer.Catalog.GetString("Throttle")] = Viewer.Catalog.GetString("THRO"),
+            [Viewer.Catalog.GetString("Time")] = Viewer.Catalog.GetString("TIME"),
+            [Viewer.Catalog.GetString("Traction cut-off relay")] = Viewer.Catalog.GetString("TRAC"),
+            [Viewer.Catalog.GetString("Train brake")] = Viewer.Catalog.GetString("BTRN"),
+            [Viewer.Catalog.GetString("Wheel")] = Viewer.Catalog.GetString("WHEL")
         };
 
         private static readonly Dictionary<string, string> LastColToAbbreviated = new Dictionary<string, string>()
         {
-            {Viewer.Catalog.GetString("apply Service"), Viewer.Catalog.GetString("Apply")},
-            {Viewer.Catalog.GetString("Apply Quick"), Viewer.Catalog.GetString("ApplQ")},
-            {Viewer.Catalog.GetString("Apply Slow"), Viewer.Catalog.GetString("ApplS")},
-            {Viewer.Catalog.GetString("coal"), Viewer.Catalog.GetString("c")},
-            {Viewer.Catalog.GetString("Emergency Braking Push Button"), Viewer.Catalog.GetString("EmerBPB")},
-            {Viewer.Catalog.GetString("Lap Self"), Viewer.Catalog.GetString("LapS")},
-            {Viewer.Catalog.GetString("Minimum Reduction"), Viewer.Catalog.GetString("MRedc")},
-            {Viewer.Catalog.GetString("safe range"), Viewer.Catalog.GetString("safe")},
-            {Viewer.Catalog.GetString("skid"), Viewer.Catalog.GetString("Skid")},
-            {Viewer.Catalog.GetString("slip warning"), Viewer.Catalog.GetString("Warning")},
-            {Viewer.Catalog.GetString("slip"), Viewer.Catalog.GetString("Slip")},
-            {Viewer.Catalog.GetString("water"), Viewer.Catalog.GetString("w")},
+            [Viewer.Catalog.GetString("(absolute)")] = Viewer.Catalog.GetString("(Abs.)"),
+            [Viewer.Catalog.GetString("apply Service")] = Viewer.Catalog.GetString("Apply"),
+            [Viewer.Catalog.GetString("Apply Quick")] = Viewer.Catalog.GetString("ApplQ"),
+            [Viewer.Catalog.GetString("Apply Slow")] = Viewer.Catalog.GetString("ApplS"),
+            [Viewer.Catalog.GetString("coal")] = Viewer.Catalog.GetString("c"),
+            [Viewer.Catalog.GetString("Emergency Braking Push Button")] = Viewer.Catalog.GetString("EmerBPB"),
+            [Viewer.Catalog.GetString("Lap Self")] = Viewer.Catalog.GetString("LapS"),
+            [Viewer.Catalog.GetString("Minimum Reduction")] = Viewer.Catalog.GetString("MRedc"),
+            [Viewer.Catalog.GetString("(safe range)")] = Viewer.Catalog.GetString("(safe)"),
+            [Viewer.Catalog.GetString("skid")] = Viewer.Catalog.GetString("Skid"),
+            [Viewer.Catalog.GetString("slip warning")] = Viewer.Catalog.GetString("Warning"),
+            [Viewer.Catalog.GetString("slip")] = Viewer.Catalog.GetString("Slip"),
+            [Viewer.Catalog.GetString("water")] = Viewer.Catalog.GetString("w")
         };
 
         bool ctrlAIFiremanOn = false; //AIFireman On

perpetualKid/FreeTrainSimulator

(cherry picked from commit 6d12de0c13c5a793f4e5ed0d57118f6c0744b011)

Source/ActivityRunner/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -144,52 +144,54 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
         };
         private readonly Dictionary<string, string> FirstColToAbbreviated = new Dictionary<string, string>()
         {
-            {Viewer.Catalog.GetString("AI Fireman"), Viewer.Catalog.GetString("AIFR")},
-            {Viewer.Catalog.GetString("Autopilot"), Viewer.Catalog.GetString("AUTO")},
-            {Viewer.Catalog.GetString("Boiler pressure"), Viewer.Catalog.GetString("PRES")},
-            {Viewer.Catalog.GetString("Boiler water glass"), Viewer.Catalog.GetString("WATR")},
-            {Viewer.Catalog.GetString("Boiler water level"), Viewer.Catalog.GetString("LEVL")},
-            {Viewer.Catalog.GetString("Circuit breaker"), Viewer.Catalog.GetString("CIRC")},
-            {Viewer.Catalog.GetString("Cylinder cocks"), Viewer.Catalog.GetString("CCOK")},
-            {Viewer.Catalog.GetString("Direction"), Viewer.Catalog.GetString("DIRC")},
-            {Viewer.Catalog.GetString("Doors open"), Viewer.Catalog.GetString("DOOR")},
-            {Viewer.Catalog.GetString("Dynamic brake"), Viewer.Catalog.GetString("BDYN")},
-            {Viewer.Catalog.GetString("Engine brake"), Viewer.Catalog.GetString("BLOC")},
-            {Viewer.Catalog.GetString("Engine"), Viewer.Catalog.GetString("ENGN")},
-            {Viewer.Catalog.GetString("Fire mass"), Viewer.Catalog.GetString("FIRE")},
-            {Viewer.Catalog.GetString("Fixed gear"), Viewer.Catalog.GetString("GEAR")},
-            {Viewer.Catalog.GetString("Fuel levels"), Viewer.Catalog.GetString("FUEL")},
-            {Viewer.Catalog.GetString("Gear"), Viewer.Catalog.GetString("GEAR")},
-            {Viewer.Catalog.GetString("Gradient"), Viewer.Catalog.GetString("GRAD")},
-            {Viewer.Catalog.GetString("Grate limit"), Viewer.Catalog.GetString("GRAT")},
-            {Viewer.Catalog.GetString("Pantographs"), Viewer.Catalog.GetString("PANT")},
-            {Viewer.Catalog.GetString("Power"), Viewer.Catalog.GetString("POWR")},
-            {Viewer.Catalog.GetString("Regulator"), Viewer.Catalog.GetString("REGL")},
-            {Viewer.Catalog.GetString("Replay"), Viewer.Catalog.GetString("RPLY")},
-            {Viewer.Catalog.GetString("Retainers"), Viewer.Catalog.GetString("RETN")},
-            {Viewer.Catalog.GetString("Reverser"), Viewer.Catalog.GetString("REVR")},
-            {Viewer.Catalog.GetString("Sander"), Viewer.Catalog.GetString("SAND")},
-            {Viewer.Catalog.GetString("Speed"), Viewer.Catalog.GetString("SPED")},
-            {Viewer.Catalog.GetString("Steam usage"), Viewer.Catalog.GetString("STEM")},
-            {Viewer.Catalog.GetString("Throttle"), Viewer.Catalog.GetString("THRO")},
-            {Viewer.Catalog.GetString("Time"), Viewer.Catalog.GetString("TIME")},
-            {Viewer.Catalog.GetString("Train brake"), Viewer.Catalog.GetString("BTRN")},
-            {Viewer.Catalog.GetString("Wheel"), Viewer.Catalog.GetString("WHEL")},
+            [Viewer.Catalog.GetString("AI Fireman")] = Viewer.Catalog.GetString("AIFR"),
+            [Viewer.Catalog.GetString("Autopilot")] = Viewer.Catalog.GetString("AUTO"),
+            [Viewer.Catalog.GetString("Boiler pressure")] = Viewer.Catalog.GetString("PRES"),
+            [Viewer.Catalog.GetString("Boiler water glass")] = Viewer.Catalog.GetString("WATR"),
+            [Viewer.Catalog.GetString("Boiler water level")] = Viewer.Catalog.GetString("LEVL"),
+            [Viewer.Catalog.GetString("Circuit breaker")] = Viewer.Catalog.GetString("CIRC"),
+            [Viewer.Catalog.GetString("Cylinder cocks")] = Viewer.Catalog.GetString("CCOK"),
+            [Viewer.Catalog.GetString("Direction")] = Viewer.Catalog.GetString("DIRC"),
+            [Viewer.Catalog.GetString("Doors open")] = Viewer.Catalog.GetString("DOOR"),
+            [Viewer.Catalog.GetString("Dynamic brake")] = Viewer.Catalog.GetString("BDYN"),
+            [Viewer.Catalog.GetString("Engine brake")] = Viewer.Catalog.GetString("BLOC"),
+            [Viewer.Catalog.GetString("Engine")] = Viewer.Catalog.GetString("ENGN"),
+            [Viewer.Catalog.GetString("Fire mass")] = Viewer.Catalog.GetString("FIRE"),
+            [Viewer.Catalog.GetString("Fixed gear")] = Viewer.Catalog.GetString("GEAR"),
+            [Viewer.Catalog.GetString("Fuel levels")] = Viewer.Catalog.GetString("FUEL"),
+            [Viewer.Catalog.GetString("Gear")] = Viewer.Catalog.GetString("GEAR"),
+            [Viewer.Catalog.GetString("Gradient")] = Viewer.Catalog.GetString("GRAD"),
+            [Viewer.Catalog.GetString("Grate limit")] = Viewer.Catalog.GetString("GRAT"),
+            [Viewer.Catalog.GetString("Pantographs")] = Viewer.Catalog.GetString("PANT"),
+            [Viewer.Catalog.GetString("Power")] = Viewer.Catalog.GetString("POWR"),
+            [Viewer.Catalog.GetString("Regulator")] = Viewer.Catalog.GetString("REGL"),
+            [Viewer.Catalog.GetString("Replay")] = Viewer.Catalog.GetString("RPLY"),
+            [Viewer.Catalog.GetString("Retainers")] = Viewer.Catalog.GetString("RETN"),
+            [Viewer.Catalog.GetString("Reverser")] = Viewer.Catalog.GetString("REVR"),
+            [Viewer.Catalog.GetString("Sander")] = Viewer.Catalog.GetString("SAND"),
+            [Viewer.Catalog.GetString("Speed")] = Viewer.Catalog.GetString("SPED"),
+            [Viewer.Catalog.GetString("Steam usage")] = Viewer.Catalog.GetString("STEM"),
+            [Viewer.Catalog.GetString("Throttle")] = Viewer.Catalog.GetString("THRO"),
+            [Viewer.Catalog.GetString("Time")] = Viewer.Catalog.GetString("TIME"),
+            [Viewer.Catalog.GetString("Train brake")] = Viewer.Catalog.GetString("BTRN"),
+            [Viewer.Catalog.GetString("Wheel")] = Viewer.Catalog.GetString("WHEL"),
         };
+
         private readonly Dictionary<string, string> LastColToAbbreviated = new Dictionary<string, string>()
         {
-            { Viewer.Catalog.GetString("apply Service"), Viewer.Catalog.GetString("Apply")},
-            {Viewer.Catalog.GetString("Apply Quick"), Viewer.Catalog.GetString("ApplQ")},
-            {Viewer.Catalog.GetString("Apply Slow"), Viewer.Catalog.GetString("ApplS")},
-            {Viewer.Catalog.GetString("coal"), Viewer.Catalog.GetString("c")},
-            {Viewer.Catalog.GetString("Emergency Braking Push Button"), Viewer.Catalog.GetString("EmerBPB")},
-            {Viewer.Catalog.GetString("Lap Self"), Viewer.Catalog.GetString("LapS")},
-            {Viewer.Catalog.GetString("Minimum Reduction"), Viewer.Catalog.GetString("MRedc")},
-            {Viewer.Catalog.GetString("safe range"), Viewer.Catalog.GetString("safe")},
-            {Viewer.Catalog.GetString("skid"), Viewer.Catalog.GetString("Skid")},
-            {Viewer.Catalog.GetString("slip warning"), Viewer.Catalog.GetString("Warning")},
-            {Viewer.Catalog.GetString("slip"), Viewer.Catalog.GetString("Slip")},
-            {Viewer.Catalog.GetString("water"), Viewer.Catalog.GetString("w")},
+            [Viewer.Catalog.GetString("(absolute)")] = Viewer.Catalog.GetString("(Abs.)"),
+            [Viewer.Catalog.GetString("apply Service")] = Viewer.Catalog.GetString("Apply"),
+            [Viewer.Catalog.GetString("Apply Quick")] = Viewer.Catalog.GetString("ApplQ"),
+            [Viewer.Catalog.GetString("Apply Slow")] = Viewer.Catalog.GetString("ApplS"),
+            [Viewer.Catalog.GetString("coal")] = Viewer.Catalog.GetString("c"),
+            [Viewer.Catalog.GetString("Emergency Braking Push Button")] = Viewer.Catalog.GetString("EmerBPB"),
+            [Viewer.Catalog.GetString("Lap Self")] = Viewer.Catalog.GetString("LapS"),
+            [Viewer.Catalog.GetString("Minimum Reduction")] = Viewer.Catalog.GetString("MRedc"),
+            [Viewer.Catalog.GetString("safe range")] = Viewer.Catalog.GetString("safe"),
+            [Viewer.Catalog.GetString("skid")] = Viewer.Catalog.GetString("Skid"),
+            [Viewer.Catalog.GetString("slip warning")] = Viewer.Catalog.GetString("Warning"),
+            [Viewer.Catalog.GetString("slip")] = Viewer.Catalog.GetString("Slip"),
+            [Viewer.Catalog.GetString("water")] = Viewer.Catalog.GetString("w"),
         };
 
         public TrainDrivingWindow(WindowManager owner)
@@ -580,7 +582,7 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                 FirstColLenght = listToLabel.Max(x => x.FirstColWidth);
                 LastColLenght = listToLabel.Max(x => x.LastColWidth);
 
-                var desiredHeight = FontToBold ? Owner.TextFontDefaultBold.Height * listToLabel.Where(x => x.FirstCol == "Sprtr" || !string.IsNullOrEmpty(x.LastCol)).Count() : 
+                var desiredHeight = FontToBold ? Owner.TextFontDefaultBold.Height * listToLabel.Where(x => x.FirstCol == "Sprtr" || !string.IsNullOrEmpty(x.LastCol)).Count() :
                     Owner.TextFontDefault.Height * listToLabel.Where(x => x.FirstCol == "Sprtr" || !string.IsNullOrEmpty(x.LastCol)).Count();
 
                 var desiredWidth = FirstColLenght + LastColLenght + (standardHUDMode ? FontToBold ? 43 : 41 : 31);