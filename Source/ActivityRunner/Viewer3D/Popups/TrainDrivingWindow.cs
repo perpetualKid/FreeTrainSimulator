openrails/openrails

Modified the sequence keys for the HUD and the Train Driving Info window.

Source/RunActivity/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -363,10 +363,9 @@ namespace Orts.Viewer3D.Popups
                         // Clickable symbol
                         if (hbox.Position.Y == TimeHboxPositionY)
                         {
-                            var expandWindow = '\u23FA';// ⏺ toggle window
+                            var expandWindow = normalTextMode ? '\u25C4' : '\u25BA';// ◀ : ▶
                             hbox.Add(ExpandWindow = new Label(hbox.RemainingWidth - TextSize, 0, TextSize, hbox.RemainingHeight, expandWindow.ToString(), LabelAlignment.Right));
                             ExpandWindow.Color = Color.Yellow;
-                            ExpandWindow.Click += new Action<Control, Point>(ExpandWindow_Click);
                         }
                         // Separator line
                         if (data.FirstCol.Contains("Sprtr"))
@@ -386,7 +385,12 @@ namespace Orts.Viewer3D.Popups
             UpdateWindowSize();
         }
 
-        void ExpandWindow_Click(Control arg1, Point arg2)
+        public override void TabAction() => CycleMode();
+
+        /// <summary>
+        /// Change between full and abbreviated text mode.
+        /// </summary>
+        public void CycleMode()
         {
             normalTextMode = !normalTextMode;
             UpdateWindowSize();

perpetualKid/FreeTrainSimulator

(cherry picked from commit af8657ee7119d997c3f1710c55145407f5c67461)

Source/ActivityRunner/Viewer3D/Popups/TrainDrivingWindow.cs
@@ -543,6 +543,7 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                         // Clickable symbol
                         if (hbox.Position.Y == TimeHboxPositionY)
                         {
+                            var expandWindow = standardHUDMode ? '\u25C4' : '\u25BA';// ◀ : ▶
                             hbox.Add(ExpandWindow = new Label(hbox.RemainingWidth - TextSize, 0, TextSize, hbox.RemainingHeight, expandWindow.ToString(), LabelAlignment.Right));
                             ExpandWindow.Color = Color.Yellow;
                             ExpandWindow.Click += new Action<Control, Point>(ExpandWindow_Click);
@@ -566,6 +567,19 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
         }
 
         private void ExpandWindow_Click(Control arg1, Point arg2)
+        {
+            CycleMode();
+        }
+
+        public override void TabAction()
+        {
+            CycleMode();
+        }
+
+        /// <summary>
+        /// Change between full and abbreviated text mode.
+        /// </summary>
+        public void CycleMode()
         {
             standardHUDMode = !standardHUDMode;
             UpdateWindowSize();
@@ -591,7 +605,7 @@ namespace Orts.ActivityRunner.Viewer3D.Popups
                 // Valid rows
                 // Validates rows with windows DPI settings
                 var dpiOffset = (System.Drawing.Graphics.FromHwnd(IntPtr.Zero).DpiY / 96) > 1.00f ? 1 : 0;// values from testing
-                var rowCount = listToLabel.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - dpiOffset;
+                var rowCount = listToLabel.Where(x => !string.IsNullOrWhiteSpace(x.FirstCol.ToString()) || !string.IsNullOrWhiteSpace(x.LastCol.ToString())).Count() - dpiOffset + 1;
                 var desiredHeight = FontToBold ? Owner.TextFontDefaultBold.Height * rowCount
                     : Owner.TextFontDefault.Height * rowCount;
 