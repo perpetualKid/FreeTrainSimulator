openrails/openrails

Added  Multi-player Info window with the Shift-9 key combination.

Source/ORTS.Common/Input/UserCommand.cs
@@ -38,6 +38,7 @@
         [GetString("Display Track Monitor Window")] DisplayTrackMonitorWindow,
         [GetString("Display HUD")] DisplayHUD,
         [GetString("Display Train Driving Window")] DisplayTrainDrivingWindow,
+        [GetString("Display Multi Player Window")] DisplayMultiPlayerWindow,
         [GetString("Display Car Labels")] DisplayCarLabels,
         [GetString("Display Station Labels")] DisplayStationLabels,
         [GetString("Display Switch Window")] DisplaySwitchWindow,

perpetualKid/FreeTrainSimulator

(cherry picked from commit d8d04ca676c15c3b94fe301e3bcdec8001e7f652)

Source/Orts.Common/Input/UserCommand.cs
@@ -42,6 +42,7 @@ namespace Orts.Common.Input
         [Description("Display Track Monitor Window")] DisplayTrackMonitorWindow,
         [Description("Display HUD")] DisplayHUD,
         [Description("Display Train Driving Window")] DisplayTrainDrivingWindow,
+        [Description("Display Multi Player Window")] DisplayMultiPlayerWindow,
         [Description("Display Car Labels")] DisplayCarLabels,
         [Description("Display Station Labels")] DisplayStationLabels,
         [Description("Display Switch Window")] DisplaySwitchWindow,