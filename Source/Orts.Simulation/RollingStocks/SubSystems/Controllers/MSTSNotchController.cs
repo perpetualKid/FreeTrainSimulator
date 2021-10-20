openrails/openrails

Add SME brak function

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -69,6 +69,10 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "eponlystart": Type = ControllerState.EPOnly; break;
                 case "epfullservicestart": Type = ControllerState.EPFullServ; break;
                 case "epholdstart": Type = ControllerState.SelfLap; break;
+                case "smeholdstart": Type = ControllerState.SMESelfLap; break;
+                case "smeonlystart": Type = ControllerState.SMEOnly; break;
+                case "smefullservicestart": Type = ControllerState.SMEFullServ; break;
+                case "smereleasestart": Type = ControllerState.SMEReleaseStart; break;
                 case "vacuumcontinuousservicestart": Type = ControllerState.VacContServ; break;
                 case "vacuumapplycontinuousservicestart": Type = ControllerState.VacApplyContServ; break;
                 case "manualbrakingstart": Type = ControllerState.ManualBraking; break;

perpetualKid/FreeTrainSimulator

(cherry picked from commit f1c627e3214a8a5d307708f3c445dc02e0a4b3fb)

Source/Orts.Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -1,4 +1,4 @@
-﻿// COPYRIGHT 2010, 2011, 2012, 2013 by the Open Rails project.
+// COPYRIGHT 2010, 2011, 2012, 2013 by the Open Rails project.
 // 
 // This file is part of Open Rails.
 // 
@@ -71,6 +71,10 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
                 case "minimalreductionstart": NotchStateType = ControllerState.MinimalReduction; break;
                 case "epapplystart": NotchStateType = ControllerState.EPApply; break;
                 case "epholdstart": NotchStateType = ControllerState.SelfLap; break;
+                case "smeholdstart": NotchStateType = ControllerState.SMESelfLap; break;
+                case "smeonlystart": NotchStateType = ControllerState.SMEOnly; break;
+                case "smefullservicestart": NotchStateType = ControllerState.SMEFullServ; break;
+                case "smereleasestart": NotchStateType = ControllerState.SMEReleaseStart; break;
                 case "vacuumcontinuousservicestart": NotchStateType = ControllerState.VacContServ; break;
                 case "vacuumapplycontinuousservicestart": NotchStateType = ControllerState.VacApplyContServ; break;
                 case "manualbrakingstart": NotchStateType = ControllerState.ManualBraking; break;