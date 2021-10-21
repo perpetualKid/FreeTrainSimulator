openrails/openrails

Blueprint https://blueprints.launchpad.net/or/+spec/odometer-cabview-control Cabview controls for odometer

Source/Orts.Simulation/Common/Commands.cs
@@ -1608,19 +1608,19 @@ namespace Orts.Common
     }
 
     [Serializable()]
-    public sealed class ResetOdometerCommand : Command
+    public sealed class ResetOdometerCommand : BooleanCommand
     {
         public static MSTSLocomotive Receiver { get; set; }
 
-        public ResetOdometerCommand(CommandLog log)
-            : base(log)
+        public ResetOdometerCommand(CommandLog log, bool toState)
+            : base(log, toState)
         {
             Redo();
         }
 
         public override void Redo()
         {
-            Receiver.OdometerReset();
+            Receiver.OdometerReset(ToState);
             // Report();
         }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit ed80ad6b03a3d2ce9df5f1c3b673abdc5a5281a3)

Source/Orts.Simulation/Commanding/Commands.cs
@@ -1669,19 +1669,19 @@ namespace Orts.Simulation.Commanding
     }
 
     [Serializable()]
-    public sealed class ResetOdometerCommand : Command
+    public sealed class ResetOdometerCommand : BooleanCommand
     {
         public static MSTSLocomotive Receiver { get; set; }
 
-        public ResetOdometerCommand(CommandLog log)
-            : base(log)
+        public ResetOdometerCommand(CommandLog log, bool targetState)
+            : base(log, targetState)
         {
             Redo();
         }
 
         public override void Redo()
         {
-            Receiver.OdometerReset();
+            Receiver.OdometerReset(targetState);
         }
 
         public override string ToString()