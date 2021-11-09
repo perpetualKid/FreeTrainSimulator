79e7825d4be63b0985b4fd7e9a3c324304e9c60e
perpetualKid <perpetualKid@azuros.net>
2021-11-09 12:12:37 +0100

    Update GameWindow.UserActivity.cs

--- Source/TrackViewer/GameWindow.UserActivity.cs
+++ Source/TrackViewer/GameWindow.UserActivity.cs
@@ -18,6 +18,8 @@ namespace Orts.TrackViewer
         private static readonly Vector2 moveUp = new Vector2(0, 1);
         private static readonly Vector2 moveDown = new Vector2(0, -1);
 
+        private const int zoomAmplifier = 3;
+
         public void ChangeScreenMode()
         {
             SetScreenMode(currentScreenMode.Next());
@@ -77,9 +79,9 @@ namespace Orts.TrackViewer
             contentArea?.UpdatePosition(moveDown * MovementAmplifier(commandArgs));
         }
 
-        private static float MovementAmplifier(UserCommandArgs commandArgs)
+        private static int MovementAmplifier(UserCommandArgs commandArgs)
         {
-            float amplifier = 5;
+            int amplifier = 5;
             if (commandArgs is ModifiableKeyCommandArgs modifiableKeyCommand)
             {
                 if ((modifiableKeyCommand.AdditionalModifiers & KeyModifiers.Control) == KeyModifiers.Control)
@@ -92,7 +94,7 @@ namespace Orts.TrackViewer
 
         private static int ZoomAmplifier(KeyModifiers modifiers)
         {
-            int amplifier = 3;
+            int amplifier = zoomAmplifier;
             if ((modifiers & KeyModifiers.Control) == KeyModifiers.Control)
                 amplifier = 1;
             else if ((modifiers & KeyModifiers.Shift) == KeyModifiers.Shift)
@@ -102,15 +104,7 @@ namespace Orts.TrackViewer
 
         private static int ZoomAmplifier(UserCommandArgs commandArgs)
         {
-            int amplifier = 3;
-            if (commandArgs is ModifiableKeyCommandArgs modifiableKeyCommand)
-            {
-                if ((modifiableKeyCommand.AdditionalModifiers & KeyModifiers.Control) == KeyModifiers.Control)
-                    amplifier = 1;
-                else if ((modifiableKeyCommand.AdditionalModifiers & KeyModifiers.Shift) == KeyModifiers.Shift)
-                    amplifier = 5;
-            }
-            return amplifier;
+            return commandArgs is ModifiableKeyCommandArgs modifiableKeyCommand ? ZoomAmplifier(modifiableKeyCommand.AdditionalModifiers) : zoomAmplifier;
         }
 
         private void ZoomIn(UserCommandArgs commandArgs)
