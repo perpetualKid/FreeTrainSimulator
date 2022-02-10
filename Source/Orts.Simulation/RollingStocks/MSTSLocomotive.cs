7e0b90a9d3eae83addab5337f2bdf32d8da6c61b
Csantucci <46676244+Csantucci@users.noreply.github.com>
2022-02-10 18:24:20 +0100

    Update Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
    Co-authored-by: Cedric GNIEWEK <gniewek.cedric@free.fr>
    (cherry picked from commit e36ed26c591c9d2d35199c57354d125d0232d9be)

--- Source/Orts.Simulation/RollingStocks/MSTSLocomotive.cs
+++ Source/Orts.Simulation/RollingStocks/MSTSLocomotive.cs
@@ -1833,7 +1833,7 @@ namespace Orts.Simulation.RollingStocks
                 TrainControlSystem.InitializeMoving();
                 TrainBrakeController.InitializeMoving();
                 BrakeSystem.LocoInitializeMoving();
-                EngineBrakeController?.SetPercent(0);
+                EngineBrakeController?.InitializeMoving();
             }
         }
 
