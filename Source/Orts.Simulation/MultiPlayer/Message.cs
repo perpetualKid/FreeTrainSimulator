openrails/openrails

Distributed power: recalculate locomotive groups after coupling and uncoupling

Source/Orts.Simulation/MultiPlayer/Message.cs
@@ -1357,6 +1357,7 @@ namespace Orts.MultiPlayer
             train.InitializeBrakes();
             //train.InitializeSignals(false);//client do it won't have impact
             train.CheckFreight();
+            train.SetDPUnitIDs();
             bool canPlace = true;
             Train.TCSubpathRoute tempRoute = train.CalculateInitialTrainPosition(ref canPlace);
 
@@ -1567,6 +1568,7 @@ namespace Orts.MultiPlayer
                 train.CalculatePositionOfCars();
                 train.travelled = Travelled;
                 train.CheckFreight();
+                train.SetDPUnitIDs();
                 return;
             }
             train1.TrainType = Train.TRAINTYPE.REMOTE;
@@ -1601,6 +1603,7 @@ namespace Orts.MultiPlayer
             train1.InitializeBrakes();
             //train1.InitializeSignals(false);
             train1.CheckFreight();
+            train1.SetDPUnitIDs();
             bool canPlace = true;
             Train.TCSubpathRoute tempRoute = train1.CalculateInitialTrainPosition(ref canPlace);
 
@@ -2621,6 +2624,7 @@ namespace Orts.MultiPlayer
                         t.MUDirection = (Direction)mDirection1;
                         train.ControlMode = Train.TRAIN_CONTROL.EXPLORER;
                         train.CheckFreight();
+                        train.SetDPUnitIDs();
                         train.InitializeBrakes();
                         canPlace = true;
                         tempRoute = train.CalculateInitialTrainPosition(ref canPlace);
@@ -2671,6 +2675,7 @@ namespace Orts.MultiPlayer
                 train2.LeadLocomotive = null;
                 train2.LeadNextLocomotive();
                 train2.CheckFreight();
+                train2.SetDPUnitIDs();
 
                 //train2 may contain myself, and no other players, thus will make myself controlling this train
                 /*if (train2.Cars.Contains(MPManager.Simulator.PlayerLocomotive))
@@ -2693,6 +2698,7 @@ namespace Orts.MultiPlayer
                 train2.MUDirection = (Direction)mDirection2;
                 train2.ControlMode = Train.TRAIN_CONTROL.EXPLORER;
                 train2.CheckFreight();
+                train2.SetDPUnitIDs();
                 train2.InitializeBrakes();
                 canPlace = true;
                 tempRoute = train2.CalculateInitialTrainPosition(ref canPlace);
@@ -2967,6 +2973,7 @@ namespace Orts.MultiPlayer
             train.MUDirection = (Direction)mDirection;
             train.RearTDBTraveller = new Traveller(MPManager.Simulator.TSectionDat, MPManager.Simulator.TDB.TrackDB.TrackNodes, TileX, TileZ, X, Z, direction == 0 ? Traveller.TravellerDirection.Forward : Traveller.TravellerDirection.Backward);
             train.CheckFreight();
+            train.SetDPUnitIDs();
             train.CalculatePositionOfCars();
             train.LeadLocomotive = null; train2.LeadLocomotive = null;
             if (Lead != -1 && Lead < train.Cars.Count) train.LeadLocomotive = train.Cars[Lead];

Source/Orts.Simulation/Simulation/Simulator.cs
@@ -1359,6 +1359,7 @@ namespace Orts.Simulation
                     train.InitializeBrakes();
                     train.CheckFreight();
                     train.ReverseFormation(false); // When using autopilot mode this is needed for correct working of train switching
+                    train.SetDPUnitIDs();
                     bool validPosition = train.PostInit();
                     if (validPosition)
                         Trains.Add(train);
@@ -1691,7 +1692,9 @@ namespace Orts.Simulation
 
 
             train.CheckFreight();
+            train.SetDPUnitIDs();
             train2.CheckFreight();
+            train2.SetDPUnitIDs();
 
             train.Update(0);   // stop the wheels from moving etc
             train2.Update(0);  // stop the wheels from moving etc
@@ -1838,6 +1841,7 @@ namespace Orts.Simulation
 
 
                     selectedAsPlayer.CheckFreight();
+                    selectedAsPlayer.SetDPUnitIDs(true);
 
                     selectedAsPlayer.Update(0);  // stop the wheels from moving etc
                     TrainSwitcher.PickedTrainFromList = selectedAsPlayer;
@@ -1869,6 +1873,7 @@ namespace Orts.Simulation
                         playerTrain.SpeedMpS = 0;
                         foreach (TrainCar car in playerTrain.Cars) car.SpeedMpS = 0;
                         playerTrain.CheckFreight();
+                        playerTrain.SetDPUnitIDs();
                         playerTrain.InitializeBrakes();
                     }
                 }

Source/Orts.Simulation/Simulation/Timetables/ProcessTimetable.cs
@@ -2404,6 +2404,7 @@ namespace Orts.Simulation.Timetables
 
                 // set train details
                 TTTrain.CheckFreight();
+                TTTrain.SetDPUnitIDs();
                 TTTrain.SpeedSettings.routeSpeedMpS = (float)simulator.TRK.Tr_RouteFile.SpeedLimit;
 
                 if (!confMaxSpeed.HasValue || confMaxSpeed.Value <= 0f)

perpetualKid/FreeTrainSimulator

(cherry picked from commit 6f74e03314f470d034183c4d8d4f17aa6c31d937)

Source/Orts.Simulation/MultiPlayer/Message.cs
@@ -1269,6 +1269,7 @@ namespace Orts.Simulation.MultiPlayer
             train.InitializeBrakes();
             //train.InitializeSignals(false);//client do it won't have impact
             train.CheckFreight();
+            train.SetDistributedPowerUnitIds();
             TrackCircuitPartialPathRoute tempRoute = train.CalculateInitialTrainPosition();
 
             train.SetInitialTrainRoute(tempRoute);
@@ -1476,6 +1477,7 @@ namespace Orts.Simulation.MultiPlayer
                 train.CalculatePositionOfCars();
                 train.DistanceTravelled = Travelled;
                 train.CheckFreight();
+                train.SetDistributedPowerUnitIds();
                 return;
             }
             train1.TrainType = TrainType.Remote;
@@ -1510,6 +1512,7 @@ namespace Orts.Simulation.MultiPlayer
             train1.InitializeBrakes();
             //train1.InitializeSignals(false);
             train1.CheckFreight();
+            train1.SetDistributedPowerUnitIds();
             TrackCircuitPartialPathRoute tempRoute = train1.CalculateInitialTrainPosition();
 
             train1.SetInitialTrainRoute(tempRoute);
@@ -2538,6 +2541,7 @@ namespace Orts.Simulation.MultiPlayer
                         t.MUDirection = TranslateMidpointDirection(mDirection1);// (MidpointDirection)mDirection1;
                         train.ControlMode = TrainControlMode.Explorer;
                         train.CheckFreight();
+                        train.SetDistributedPowerUnitIds();
                         train.InitializeBrakes();
                         tempRoute = train.CalculateInitialTrainPosition();
                         if (tempRoute.Count == 0)
@@ -2588,6 +2592,7 @@ namespace Orts.Simulation.MultiPlayer
                 train2.LeadLocomotive = null;
                 train2.LeadNextLocomotive();
                 train2.CheckFreight();
+                train2.SetDistributedPowerUnitIds();
 
                 //train2 may contain myself, and no other players, thus will make myself controlling this train
                 /*if (train2.Cars.Contains(MPManager.Simulator.PlayerLocomotive))
@@ -2610,6 +2615,7 @@ namespace Orts.Simulation.MultiPlayer
                 train2.MUDirection = TranslateMidpointDirection(mDirection2); // (MidpointDirection)mDirection2;
                 train2.ControlMode = TrainControlMode.Explorer;
                 train2.CheckFreight();
+                train2.SetDistributedPowerUnitIds();
                 train2.InitializeBrakes();
                 tempRoute = train2.CalculateInitialTrainPosition();
                 if (tempRoute.Count == 0)
@@ -2881,6 +2887,7 @@ namespace Orts.Simulation.MultiPlayer
             train.MUDirection = TranslateMidpointDirection(mDirection); // (MidpointDirection)mDirection;
             train.RearTDBTraveller = new Traveller(location, direction == 0 ? Direction.Forward : Direction.Backward);
             train.CheckFreight();
+            train.SetDistributedPowerUnitIds();
             train.CalculatePositionOfCars();
             train.LeadLocomotive = null; train2.LeadLocomotive = null;
             if (Lead != -1 && Lead < train.Cars.Count) train.LeadLocomotive = train.Cars[Lead];

Source/Orts.Simulation/Simulator.cs
@@ -1254,6 +1254,7 @@ namespace Orts.Simulation
                     train.InitializeBrakes();
                     train.CheckFreight();
                     train.ReverseFormation(false); // When using autopilot mode this is needed for correct working of train switching
+                    train.SetDistributedPowerUnitIds();
                     bool validPosition = train.PostInit();
                     if (validPosition)
                         Trains.Add(train);
@@ -1588,7 +1589,9 @@ namespace Orts.Simulation
 
 
             train.CheckFreight();
+            train.SetDistributedPowerUnitIds();
             train2.CheckFreight();
+            train2.SetDistributedPowerUnitIds();
 
             train.Update(0);   // stop the wheels from moving etc
             train2.Update(0);  // stop the wheels from moving etc
@@ -1730,6 +1733,7 @@ namespace Orts.Simulation
 
 
                     selectedAsPlayer.CheckFreight();
+                    selectedAsPlayer.SetDistributedPowerUnitIds(true);
 
                     selectedAsPlayer.Update(0);  // stop the wheels from moving etc
                     TrainSwitcher.PickedTrainFromList = selectedAsPlayer;
@@ -1761,6 +1765,7 @@ namespace Orts.Simulation
                         playerTrain.SpeedMpS = 0;
                         foreach (TrainCar car in playerTrain.Cars) car.SpeedMpS = 0;
                         playerTrain.CheckFreight();
+                        playerTrain.SetDistributedPowerUnitIds();
                         playerTrain.InitializeBrakes();
                     }
                 }

Source/Orts.Simulation/Timetables/ProcessTimetable.cs
@@ -2394,6 +2394,7 @@ namespace Orts.Simulation.Timetables
 
                 // set train details
                 TTTrain.CheckFreight();
+                TTTrain.SetDistributedPowerUnitIds();
                 TTTrain.SpeedSettings.routeSpeedMpS = (float)simulator.Route.SpeedLimit;
 
                 if (!confMaxSpeed.HasValue || confMaxSpeed.Value <= 0f)