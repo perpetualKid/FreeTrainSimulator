openrails/openrails

Blueprint https://blueprints.launchpad.net/or/+spec/distributed-power : manual rebase from Peter Gulyas' work plus change of key assignments

Source/RunActivity/Viewer3D/Viewer.cs
@@ -603,6 +603,15 @@ namespace Orts.Viewer3D
             ElectricTrainSupplyCommand.Receiver = (PlayerLocomotive as MSTSLocomotive).LocomotivePowerSupply;
             TCSButtonCommand.Receiver = (PlayerLocomotive as MSTSLocomotive).TrainControlSystem;
             TCSSwitchCommand.Receiver = (PlayerLocomotive as MSTSLocomotive).TrainControlSystem;
+
+            //Distributed power
+            DPMoveToFrontCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DPMoveToBackCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DPTractionCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DPIdleCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DPDynamicBrakeCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DPMoreCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DPLessCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
         }
 
         public void ChangeToPreviousFreeRoamCamera()

Source/ORTS.Common/Input/UserCommand.cs
@@ -210,5 +210,14 @@
         [GetString("Control AI Fire On")] ControlAIFireOn,
         [GetString("Control AI Fire Off")] ControlAIFireOff,
         [GetString("Control AI Fire Reset")] ControlAIFireReset,
+
+        //Distributed power
+        [GetString("Control DP Move To Front")] ControlDPMoveToFront,
+        [GetString("Control DP Move To Back")] ControlDPMoveToBack,
+        [GetString("Control DP Traction")] ControlDPTraction,
+        [GetString("Control DP Idle")] ControlDPIdle,
+        [GetString("Control DP Brake")] ControlDPBrake,
+        [GetString("Control DP More")] ControlDPMore,
+        [GetString("Control DP Less")] ControlDPLess,
     }
 }

Source/Orts.Simulation/Simulation/AIs/AI.cs
@@ -961,6 +961,7 @@ namespace Orts.Simulation.AIs
 #endif
             train.CreateRoute(false);  // create route without use of FrontTDBtraveller
             train.CheckFreight(); // check if train is freight or passenger
+            train.SetDPUnitIDs(); // distributed power
             if (!isInitialPlayerTrain || train.InitialSpeed != 0) train.AITrainDirectionForward = true;
             train.BrakeLine3PressurePSI = 0;
 

Source/Orts.Simulation/Common/Commands.cs
@@ -1311,6 +1311,134 @@ namespace Orts.Common
         }
     }
 
+    // Distributed Power controls
+    [Serializable()]
+    public sealed class DPMoveToFrontCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DPMoveToFrontCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DPMoveToFront();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DPMoveToBackCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DPMoveToBackCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DPMoveToBack();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DPIdleCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DPIdleCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DPIdle();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DPTractionCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DPTractionCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DPTraction();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DPDynamicBrakeCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DPDynamicBrakeCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DPDynamicBrake();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DPMoreCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DPMoreCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DPMore();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DPLessCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DPLessCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DPLess();
+            // Report();
+        }
+    }
+
+
     // Steam controls
     [Serializable()]
     public sealed class ContinuousSteamHeatCommand : ContinuousCommand

perpetualKid/FreeTrainSimulator

(cherry picked from commit acf84b8bc6c2f78b5445bc432176524106ff99ef)

Source/ActivityRunner/Viewer3D/Viewer.cs
@@ -1073,6 +1073,15 @@ namespace Orts.ActivityRunner.Viewer3D
             ElectricTrainSupplyCommand.Receiver = (PlayerLocomotive as MSTSLocomotive).LocomotivePowerSupply;
             TCSButtonCommand.Receiver = (PlayerLocomotive as MSTSLocomotive).TrainControlSystem;
             TCSSwitchCommand.Receiver = (PlayerLocomotive as MSTSLocomotive).TrainControlSystem;
+
+            //Distributed power
+            DistributedPowerMoveToFrontCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DistributedPowerMoveToBackCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DistributedPowerTractionCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DistributedPowerIdleCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DistributedPowerDynamicBrakeCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DistributedPowerIncreaseCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
+            DistributedPOwerDecreaseCommand.Receiver = (MSTSLocomotive)PlayerLocomotive;
         }
 
         public void ChangeToPreviousFreeRoamCamera()

Source/Orts.Common/Input/UserCommand.cs
@@ -215,6 +215,15 @@ namespace Orts.Common.Input
         [Description("Control AI Fire On")] ControlAIFireOn,
         [Description("Control AI Fire Off")] ControlAIFireOff,
         [Description("Control AI Fire Reset")] ControlAIFireReset,
+        //Distributed power
+        [Description("Control DP Move To Front")] ControlDistributedPowerMoveToFront,
+        [Description("Control DP Move To Back")] ControlDistributedPowerMoveToBack,
+        [Description("Control DP Traction")] ControlDistributedPOwerTraction,
+        [Description("Control DP Idle")] ControlDistributedPowerIdle,
+        [Description("Control DP Brake")] ControlDistributedPowerBrake,
+        [Description("Control DP More")] ControlDistributedIncrease,
+        [Description("Control DP Less")] ControlDistributedPowerDecrease,
+
     }
 
     /// <summary>

Source/Orts.Simulation/AIs/AI.cs
@@ -956,6 +956,7 @@ namespace Orts.Simulation.AIs
 #endif
             train.CreateRoute(false);  // create route without use of FrontTDBtraveller
             train.CheckFreight(); // check if train is freight or passenger
+            train.SetDistributedPowerUnitIds(); // distributed power
             if (!isInitialPlayerTrain || train.InitialSpeed != 0) train.AITrainDirectionForward = true;
             train.BrakeLine3PressurePSI = 0;
 

Source/Orts.Simulation/Commanding/Commands.cs
@@ -1363,6 +1363,134 @@ namespace Orts.Simulation.Commanding
         }
     }
 
+    // Distributed Power controls
+    [Serializable()]
+    public sealed class DistributedPowerMoveToFrontCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DistributedPowerMoveToFrontCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DistributedPowerMoveToFront();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DistributedPowerMoveToBackCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DistributedPowerMoveToBackCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DistributedPowerMoveToBack();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DistributedPowerIdleCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DistributedPowerIdleCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DistributedPowerIdle();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DistributedPowerTractionCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DistributedPowerTractionCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DistributedPowerTraction();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DistributedPowerDynamicBrakeCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DistributedPowerDynamicBrakeCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DistributedPowerDynamicBrake();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DistributedPowerIncreaseCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DistributedPowerIncreaseCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DistributedPowerIncrease();
+            // Report();
+        }
+    }
+
+    [Serializable()]
+    public sealed class DistributedPOwerDecreaseCommand : Command
+    {
+        public static MSTSWagon Receiver { get; set; }
+
+        public DistributedPOwerDecreaseCommand(CommandLog log)
+            : base(log)
+        {
+            Redo();
+        }
+
+        public override void Redo()
+        {
+            Receiver.Train.DistributedPowerDecrease();
+            // Report();
+        }
+    }
+
+
     // Steam controls
     [Serializable()]
     public sealed class ContinuousSteamHeatCommand : ContinuousCommand