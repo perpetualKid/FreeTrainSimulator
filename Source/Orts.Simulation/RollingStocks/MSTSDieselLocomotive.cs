openrails/openrails

Adjust fuel consumption so that it will operate whilever the diesel engine is running

Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -636,18 +636,6 @@ namespace Orts.Simulation.RollingStocks
                         TractiveForceN = 0;
                 }
 
-                DieselFlowLps = DieselEngines.DieselFlowLps;
-                partialFuelConsumption += DieselEngines.DieselFlowLps * elapsedClockSeconds;
-                if (partialFuelConsumption >= 0.1)
-                {
-                    DieselLevelL -= partialFuelConsumption;
-                    partialFuelConsumption = 0;
-                }
-                if (DieselLevelL <= 0.0f)
-                {
-                    SignalEvent(Event.EnginePowerOff);
-                    DieselEngines.HandleEvent(PowerSupplyEvent.StopEngine);
-                }
             }
             else
             {
@@ -661,6 +649,24 @@ namespace Orts.Simulation.RollingStocks
                 if (w < 0)
                     w = 0;
                 AverageForceN = w * AverageForceN + (1 - w) * TractiveForceN;
+            }
+
+            // Calculate fuel consumption will occur unless diesel engine is stopped
+            if (LocomotivePowerSupply.MainPowerSupplyOn)
+            {
+                DieselFlowLps = DieselEngines.DieselFlowLps;
+                partialFuelConsumption += DieselEngines.DieselFlowLps * elapsedClockSeconds;
+                if (partialFuelConsumption >= 0.1)
+                {
+                    DieselLevelL -= partialFuelConsumption;
+                    partialFuelConsumption = 0;
+                }
+                // stall engine if fuel runs out
+                if (DieselLevelL <= 0.0f)
+                {
+                    SignalEvent(Event.EnginePowerOff);
+                    DieselEngines.HandleEvent(PowerSupplyEvent.StopEngine);
+                }
             }
         }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit a1162847057d1d53777ece280e7dbb52ff14ee68)

Source/Orts.Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -688,18 +688,6 @@ namespace Orts.Simulation.RollingStocks
                         TractiveForceN = 0;
                 }
 
-                DieselFlowLps = DieselEngines.DieselFlowLps;
-                partialFuelConsumption += DieselEngines.DieselFlowLps * (float)elapsedClockSeconds;
-                if (partialFuelConsumption >= 0.1)
-                {
-                    DieselLevelL -= partialFuelConsumption;
-                    partialFuelConsumption = 0;
-                }
-                if (DieselLevelL <= 0.0f)
-                {
-                    SignalEvent(TrainEvent.EnginePowerOff);
-                    DieselEngines.HandleEvent(PowerSupplyEvent.StopEngine);
-                }
             }
             else
             {
@@ -713,6 +701,24 @@ namespace Orts.Simulation.RollingStocks
                 if (w < 0)
                     w = 0;
                 AverageForceN = w * AverageForceN + (1 - w) * TractiveForceN;
+            }
+
+            // Calculate fuel consumption will occur unless diesel engine is stopped
+            if (LocomotivePowerSupply.MainPowerSupplyOn)
+            {
+                DieselFlowLps = DieselEngines.DieselFlowLps;
+                partialFuelConsumption += DieselEngines.DieselFlowLps * (float)elapsedClockSeconds;
+                if (partialFuelConsumption >= 0.1)
+                {
+                    DieselLevelL -= partialFuelConsumption;
+                    partialFuelConsumption = 0;
+                }
+                // stall engine if fuel runs out
+                if (DieselLevelL <= 0.0f)
+                {
+                    SignalEvent(TrainEvent.EnginePowerOff);
+                    DieselEngines.HandleEvent(PowerSupplyEvent.StopEngine);
+                }
             }
         }
 