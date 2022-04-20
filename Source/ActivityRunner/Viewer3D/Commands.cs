openrails/openrails

Bug fix for Crash when switching cabs and changing screen page  https://bugs.launchpad.net/or/+bug/1955575

Source/RunActivity/Viewer3D/Commands.cs
@@ -93,7 +93,7 @@ namespace Orts.Viewer3D
     [Serializable()]
     public sealed class SelectScreenCommand : BooleanCommand
     {
-        public static CabRenderer Receiver { get; set; }
+        public static Viewer Receiver { get; set; }
 
         public SelectScreenCommand(CommandLog log, bool toState, string screen, int display)
             : base(log, toState)
@@ -105,7 +105,10 @@ namespace Orts.Viewer3D
         {
             if (ToState)
             {
-                Receiver.ActiveScreen[display] = screen;
+                var finalReceiver = Receiver.Camera  is ThreeDimCabCamera ?
+                    (Receiver.PlayerLocomotiveViewer as MSTSLocomotiveViewer).ThreeDimentionCabRenderer :
+                    (Receiver.PlayerLocomotiveViewer as MSTSLocomotiveViewer)._CabRenderer;
+                finalReceiver.ActiveScreen[display] = screen;
             }
         }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit 33291687717f10f6976e1058c8d1de12e1819430)

Source/ActivityRunner/Viewer3D/Commands.cs
@@ -94,7 +94,7 @@ namespace Orts.ActivityRunner.Viewer3D
     [Serializable()]
     public sealed class SelectScreenCommand : BooleanCommand
     {
-        public static CabRenderer Receiver { get; set; }
+        public static Viewer Receiver { get; set; }
 
         private readonly string screen;
         private readonly int display;
@@ -112,7 +112,10 @@ namespace Orts.ActivityRunner.Viewer3D
         {
             if (targetState)
             {
-                Receiver.ActiveScreen[display] = screen;
+                var finalReceiver = Receiver.Camera  is CabCamera3D ?
+                    (Receiver.PlayerLocomotiveViewer as MSTSLocomotiveViewer).CabRenderer3D:
+                    (Receiver.PlayerLocomotiveViewer as MSTSLocomotiveViewer).CabRenderer;
+                finalReceiver.ActiveScreen[display] = screen;
             }
         }
 