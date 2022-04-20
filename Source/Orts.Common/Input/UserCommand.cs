openrails/openrails

Blueprint https://blueprints.launchpad.net/or/+spec/cabview-controls-for-generic-items

Source/ORTS.Common/Input/UserCommand.cs
@@ -205,8 +205,10 @@
         [GetString("Control ImmediateRefill")] ControlImmediateRefill,
         [GetString("Control Turntable Clockwise")] ControlTurntableClockwise,
         [GetString("Control Turntable Counterclockwise")] ControlTurntableCounterclockwise,
-        [GetString("Control Generic 1")] ControlGeneric1,
-        [GetString("Control Generic 2")] ControlGeneric2,
+        [GetString("Control Generic Item 1")] ControlGenericItem1,
+        [GetString("Control Generic Item 2")] ControlGenericItem2,
+        [GetString("Control TCS Generic 1")] ControlTCSGeneric1,
+        [GetString("Control TCS Generic 2")] ControlTCSGeneric2,
         [GetString("Control Cab Radio")] ControlCabRadio,
         [GetString("Control AI Fire On")] ControlAIFireOn,
         [GetString("Control AI Fire Off")] ControlAIFireOff,

perpetualKid/FreeTrainSimulator

(cherry picked from commit 81172506dec89fdd8bfff09a28a31198374f4cf5)

Source/Orts.Common/Input/UserCommand.cs
@@ -210,8 +210,10 @@ namespace Orts.Common.Input
         [Description("Control ImmediateRefill")] ControlImmediateRefill,
         [Description("Control Turntable Clockwise")] ControlTurntableClockwise,
         [Description("Control Turntable Counterclockwise")] ControlTurntableCounterclockwise,
-        [Description("Control Generic 1")] ControlGeneric1,
-        [Description("Control Generic 2")] ControlGeneric2,
+        [Description("Control Generic Item 1")] ControlGenericItem1,
+        [Description("Control Generic Item 2")] ControlGenericItem2,
+        [Description("Control TCS Generic 1")] ControlTCSGeneric1,
+        [Description("Control TCS Generic 2")] ControlTCSGeneric2,
         [Description("Control Cab Radio")] ControlCabRadio,
         [Description("Control AI Fire On")] ControlAIFireOn,
         [Description("Control AI Fire Off")] ControlAIFireOff,