openrails/openrails

Distributed Power: pass LoadUnits to GetDPuStatus()

Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -930,7 +930,7 @@ namespace Orts.Simulation.RollingStocks
             return status.ToString();
         }
 
-        public string GetDpuStatus(bool dataDpu)// used by the TrainDpuInfo window
+        public string GetDpuStatus(bool dataDpu, CABViewControlUnits loadUnits = CABViewControlUnits.KILO_LBS)// used by the TrainDpuInfo window
         {
             string throttle = "";
             if (ThrottlePercent > 0)

perpetualKid/FreeTrainSimulator

(cherry picked from commit 3d3e37aff8d9876bf7ef30c2ad8c5d3245b7b8ba)

Source/Orts.Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -957,7 +957,7 @@ namespace Orts.Simulation.RollingStocks
             return status.ToString();
         }
 
-        public string GetDpuStatus(bool dataDpu)// used by the TrainDpuInfo window
+        public string GetDpuStatus(bool dataDpu, CabViewControlUnit loadUnits = CabViewControlUnit.Kilo_Lbs)// used by the TrainDpuInfo window
         {
             string throttle = "";
             if (ThrottlePercent > 0)