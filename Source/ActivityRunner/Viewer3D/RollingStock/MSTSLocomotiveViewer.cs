openrails/openrails

Fix 3D cabview renderer not marking its shapes and textures

Source/RunActivity/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -350,8 +350,10 @@ namespace Orts.Viewer3D.RollingStock
             foreach (var pdl in ParticleDrawers.Values)
                 foreach (var pd in pdl)
                     pd.Mark();
-            if (_CabRenderer != null)
-                _CabRenderer.Mark();
+
+            _CabRenderer?.Mark();
+            ThreeDimentionCabViewer?.Mark();
+
             base.Mark();
         }
 
@@ -2914,9 +2916,13 @@ namespace Orts.Viewer3D.RollingStock
 
         internal override void Mark()
         {
-            // TODO: This is likely wrong; we should mark textures, shapes and other graphical resources here.
+            TrainCarShape?.Mark();
+            foreach (ThreeDimCabDigit threeDimCabDigit in DigitParts3D.Values)
+            {
+                threeDimCabDigit.Mark();
+            }
         }
-    } // Class ThreeDimentionCabViewer
+    }
 
     public class ThreeDimCabDigit
     {

perpetualKid/FreeTrainSimulator

(cherry picked from commit 5d0c86f9356a7dc26732dec95c99eae6a14f2b72)

Source/ActivityRunner/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -612,8 +612,8 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
             foreach (var pdl in ParticleDrawers.Values)
                 foreach (var pd in pdl)
                     pd.Mark();
-            if (CabRenderer != null)
-                CabRenderer.Mark();
+            CabRenderer?.Mark();
+            CabRenderer3D?.Mark();
             base.Mark();
         }
 
@@ -3511,7 +3511,11 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
 
         internal override void Mark()
         {
-            // TODO: This is likely wrong; we should mark textures, shapes and other graphical resources here.
+            TrainCarShape?.Mark();
+            foreach (ThreeDimCabDigit threeDimCabDigit in DigitParts3D.Values)
+            {
+                threeDimCabDigit.Mark();
+            }
         }
 
         public override void HandleUserInput(in ElapsedTime elapsedTime)