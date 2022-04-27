openrails/openrails

Bug fix for https://bugs.launchpad.net/or/+bug/1959617 Positive bars can't have two colours

Source/RunActivity/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -1827,9 +1827,9 @@ namespace Orts.Viewer3D.RollingStock
                 if (Gauge is CVCFirebox)
                     destH = Math.Min(destH, (int)(yratio * (Control.PositionY + 0.5 * Gauge.Area.Height)) - destY);
             }
-            if (Control.MinValue < 0 && Control.ControlType != CABViewControlTypes.REVERSER_PLATE && Gauge.ControlStyle != CABViewControlStyles.POINTER)
+            if (Control.ControlType != CABViewControlTypes.REVERSER_PLATE && Gauge.ControlStyle != CABViewControlStyles.POINTER)
             {
-                if (Num < 0 && Gauge.NegativeColor.A != 0)
+                if (Num < 0 && Control.MinValue < 0 && Gauge.NegativeColor.A != 0)
                 {
                     if ((Gauge.NumNegativeColors >= 2) && (Num < Gauge.NegativeSwitchVal))
                         DrawColor = new Color(Gauge.SecondNegativeColor.R, Gauge.SecondNegativeColor.G, Gauge.SecondNegativeColor.B, Gauge.SecondNegativeColor.A);

perpetualKid/FreeTrainSimulator

(cherry picked from commit 404028a46b1facec581e7f74dab1ff7e0681c3c9)

Source/ActivityRunner/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -2101,7 +2101,7 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
                 if (Gauge is CabViewFireboxControl)
                     destH = Math.Min(destH, (int)(yratio * (Control.Bounds.Y + 0.5 * Gauge.Area.Height)) - destY);
             }
-            if (Control.ScaleRangeMin < 0 && Control.ControlType != CabViewControlType.Reverser_Plate && Gauge.ControlStyle != CabViewControlStyle.Pointer)
+            if (Control.ControlType != CabViewControlType.Reverser_Plate && Gauge.ControlStyle != CabViewControlStyle.Pointer)
             {
                 if (Num < 0 && Gauge.NegativeColors[0].A != 0)
                 {