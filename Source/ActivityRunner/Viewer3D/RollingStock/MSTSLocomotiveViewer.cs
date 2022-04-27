openrails/openrails

Removed option CircularSpeedGauge
Equivalent to always being true.
Also made changes to Manual missing from previous commit

Source/RunActivity/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -1190,7 +1190,7 @@ namespace Orts.Viewer3D.RollingStock
                         if (digital != null)
                         {
                             CabViewDigitalRenderer cvdr;
-                            if (viewer.Settings.CircularSpeedGauge && digital.ControlStyle == CABViewControlStyles.NEEDLE)
+                            if (digital.ControlStyle == CABViewControlStyles.NEEDLE)
                                 cvdr = new CircularSpeedGaugeRenderer(viewer, car, digital, _Shader);
                             else
                                 cvdr = new CabViewDigitalRenderer(viewer, car, digital, _Shader);
@@ -1311,7 +1311,7 @@ namespace Orts.Viewer3D.RollingStock
                 if (digital != null)
                 {
                     CabViewDigitalRenderer cvdr;
-                    if (viewer.Settings.CircularSpeedGauge && digital.ControlStyle == CABViewControlStyles.NEEDLE)
+                    if (digital.ControlStyle == CABViewControlStyles.NEEDLE)
                         cvdr = new CircularSpeedGaugeRenderer(viewer, car, digital, _Shader);
                     else
                         cvdr = new CabViewDigitalRenderer(viewer, car, digital, _Shader);

perpetualKid/FreeTrainSimulator

(cherry picked from commit 2f1cdf2a3e38bdd9f9ca9deabdcc9edbc572711c)

Source/ActivityRunner/Viewer3D/RollingStock/MSTSLocomotiveViewer.cs
@@ -1446,7 +1446,7 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
                         if (digital != null)
                         {
                             CabViewDigitalRenderer cvdr;
-                            if (viewer.Settings.CircularSpeedGauge && digital.ControlStyle == CabViewControlStyle.Needle)
+                            if (digital.ControlStyle == CabViewControlStyle.Needle)
                                 cvdr = new CircularSpeedGaugeRenderer(viewer, car, digital, _Shader);
                             else
                                 cvdr = new CabViewDigitalRenderer(viewer, car, digital, _Shader);
@@ -1574,7 +1574,7 @@ namespace Orts.ActivityRunner.Viewer3D.RollingStock
                 if (digital != null)
                 {
                     CabViewDigitalRenderer cvdr;
-                    if (viewer.Settings.CircularSpeedGauge && digital.ControlStyle == CabViewControlStyle.Needle)
+                    if (digital.ControlStyle == CabViewControlStyle.Needle)
                         cvdr = new CircularSpeedGaugeRenderer(viewer, car, digital, _Shader);
                     else
                         cvdr = new CabViewDigitalRenderer(viewer, car, digital, _Shader);