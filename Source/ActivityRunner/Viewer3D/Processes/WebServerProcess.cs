openrails/openrails

adds check for webserver port in use
Also adds missing save of webserver port value

Source/RunActivity/Viewer3D/Processes/WebServerProcess.cs
@@ -26,6 +26,9 @@ using Orts.Processes;
 using System.IO;
 using System.Windows.Forms;
 using CancellationTokenSource = System.Threading.CancellationTokenSource;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System;
 
 namespace Orts.Viewer3D.Processes
 {
@@ -63,8 +66,22 @@ namespace Orts.Viewer3D.Processes
 
             string myWebContentPath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Content\\Web");
             EndPointManager.UseIpv6 = true;
-            using (EmbedIO.WebServer server = WebServer.CreateWebServer($"http://*:{Game.Settings.WebServerPort}", myWebContentPath))
-                server.RunAsync(StopServer.Token).Wait();
+            try
+            {
+                using (EmbedIO.WebServer server = WebServer.CreateWebServer($"http://*:{Game.Settings.WebServerPort}", myWebContentPath))
+                    server.RunAsync(StopServer.Token).Wait();
+            }
+            catch(AggregateException ex)
+            {
+                if (ex.InnerException is SocketException)
+                {
+                    Trace.TraceWarning($"Port {Game.Settings.WebServerPort} is already in use. Continuing without webserver");
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
         }
     }
 }

perpetualKid/FreeTrainSimulator

(cherry picked from commit 28320449e6147a2acc47fd61597c0887d03f0b31)

Source/ActivityRunner/Viewer3D/Processes/WebServerProcess.cs
@@ -18,13 +18,17 @@
 // This file is the responsibility of the 3D & Environment Team. 
 
 
-using EmbedIO.Net;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Sockets;
 using System.Threading;
+using System.Windows.Forms;
+
+using EmbedIO.Net;
+
 using Orts.ActivityRunner.Processes;
 using Orts.ActivityRunner.Viewer3D.WebServices;
-using System.IO;
-using System.Windows.Forms;
-using CancellationTokenSource = System.Threading.CancellationTokenSource;
 
 namespace Orts.ActivityRunner.Viewer3D.Processes
 {
@@ -63,8 +67,22 @@ namespace Orts.ActivityRunner.Viewer3D.Processes
 
             string contentPath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Content\\Web");
             EndPointManager.UseIpv6 = true;
-            using (EmbedIO.WebServer server = WebServer.CreateWebServer($"http://*:{Game.Settings.WebServerPort}", contentPath))
-                server.RunAsync(StopServer.Token).Wait();
+            try
+            {
+                using (EmbedIO.WebServer server = WebServer.CreateWebServer($"http://*:{Game.Settings.WebServerPort}", contentPath))
+                    server.RunAsync(StopServer.Token).Wait();
+            }
+            catch (AggregateException ex)
+            {
+                if (ex.InnerException is SocketException)
+                {
+                    Trace.TraceWarning($"Port {Game.Settings.WebServerPort} is already in use. Continuing without webserver");
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
         }
     }
 }