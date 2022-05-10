openrails/openrails

EOT: follow suggestions of reviewer

Source/Orts.Simulation/Common/Commands.cs
@@ -2242,10 +2242,7 @@ namespace Orts.Common
 
         public override void Redo()
         {
-            if (Receiver?.Train?.EOT != null)
-            {
-                Receiver.Train.EOT.EmergencyBrake(!Receiver.Train.EOT.EOTEmergencyBrakingOn);
-            }
+            Receiver.Train.EOT?.EmergencyBrake(!Receiver.Train.EOT.EOTEmergencyBrakingOn);
         }
     }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit f2b6299e2e0c24f605cac29199cefbea57ec8ee4)

Source/Orts.Simulation/Commanding/Commands.cs
@@ -2265,10 +2265,7 @@ namespace Orts.Simulation.Commanding
 
         public override void Redo()
         {
-            if (Receiver?.Train?.EndOfTrainDevice != null)
-            {
-                Receiver.Train.EndOfTrainDevice.EmergencyBrake(!Receiver.Train.EndOfTrainDevice.EOTEmergencyBrakingOn);
-            }
+                Receiver.Train.EndOfTrainDevice?.EmergencyBrake(!Receiver.Train.EndOfTrainDevice.EOTEmergencyBrakingOn);
         }
     }
 