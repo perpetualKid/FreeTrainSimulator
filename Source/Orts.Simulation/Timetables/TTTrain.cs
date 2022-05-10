openrails/openrails

Management of EOT devices  https://blueprints.launchpad.net/or/+spec/eot

Source/Orts.Simulation/Simulation/Timetables/TTTrain.cs
@@ -11666,6 +11666,7 @@ namespace Orts.Simulation.Timetables
             // set various items
             attachTrain.CheckFreight();
             attachTrain.SetDPUnitIDs();
+            attachTrain.ReinitializeEOT();
             attachCar.SignalEvent(Event.Couple);
             attachTrain.ProcessSpeedSettings();
 
@@ -12048,7 +12049,10 @@ namespace Orts.Simulation.Timetables
             // check freight for both trains
             CheckFreight();
             SetDPUnitIDs();
+            ReinitializeEOT();
             newTrain.CheckFreight();
+            newTrain.SetDPUnitIDs();
+            newTrain.ReinitializeEOT();
 
             // check speed values for both trains
             ProcessSpeedSettings();

perpetualKid/FreeTrainSimulator

(cherry picked from commit 34c0dc346d7d878545f8b494b8e5ae192210af3c)

Source/Orts.Simulation/Timetables/TTTrain.cs
@@ -10499,6 +10499,7 @@ namespace Orts.Simulation.Timetables
             // set various items
             attachTrain.CheckFreight();
             attachTrain.SetDistributedPowerUnitIds();
+            attachTrain.ReinitializeEOT();
             attachCar.SignalEvent(TrainEvent.Couple);
             attachTrain.ProcessSpeedSettings();
 
@@ -10867,7 +10868,10 @@ namespace Orts.Simulation.Timetables
             // check freight for both trains
             CheckFreight();
             SetDistributedPowerUnitIds();
+            ReinitializeEOT();
             newTrain.CheckFreight();
+            newTrain.SetDistributedPowerUnitIds();
+            newTrain.ReinitializeEOT();
 
             // check speed values for both trains
             ProcessSpeedSettings();