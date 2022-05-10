openrails/openrails

Update Source/Orts.Simulation/Common/Commands.cs
Co-authored-by: César Benito Lamata <cesarbema2009@hotmail.com>

Source/Orts.Simulation/Common/Commands.cs
@@ -2208,10 +2208,7 @@ namespace Orts.Common
 
         public override void Redo()
         {
-            if (Receiver?.Train?.EOT != null)
-            {
-                Receiver.Train.EOT.ArmTwoWay();
-            }
+            Receiver?.Train?.EOT?.ArmTwoWay();
         }
     }
 

perpetualKid/FreeTrainSimulator

(cherry picked from commit db78287b35d6276d22669ad51f6f3b389f6ea672)

Source/Orts.Simulation/Commanding/Commands.cs
@@ -2231,10 +2231,7 @@ namespace Orts.Simulation.Commanding
 
         public override void Redo()
         {
-            if (Receiver?.Train?.EndOfTrainDevice != null)
-            {
-                Receiver.Train.EndOfTrainDevice.ArmTwoWay();
-            }
+                Receiver?.Train?.EndOfTrainDevice?.ArmTwoWay();
         }
     }
 