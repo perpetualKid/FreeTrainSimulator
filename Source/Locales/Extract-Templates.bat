01bb21cfa6c5211c64271e621747e32d3a097688
perpetualKid <perpetualKid@azuros.net>
2022-08-18 09:04:18 +0200

    some language stuff

--- Source/Locales/Extract-Templates.bat
+++ Source/Locales/Extract-Templates.bat
@@ -1,11 +1,8 @@
 @ECHO OFF
 
-dotnet new tool-manifest --force
-dotnet tool install gettext.net.extractor
-
 PUSHD %~p0
 FOR /D %%M IN (.\..\*) DO (
-	dotnet gettext.extractor -s %%M -t .\%%~nxM\%%~nxM.pot
+	dotnet gettext.extractor -s %%M -t .\%%~nxM\%%~nxM.pot -o
 	IF EXIST .\%%~nxM\%%~nxM.pot (
 	echo Updated translation template .\%%~nxM\%%~nxM.pot
 	)
