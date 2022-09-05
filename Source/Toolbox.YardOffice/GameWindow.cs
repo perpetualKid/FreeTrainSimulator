901be1d138cb72b3d5b31fcd77e0a2f2d3f94b2d
amtrak115 <amtrak115@gmail.com>
2022-09-05 15:04:29 -0500

    several different Myral MML scripts

--- Source/Toolbox.YardOffice/GameWindow.cs
+++ Source/Toolbox.YardOffice/GameWindow.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -7,6 +10,15 @@ using Microsoft.Xna.Framework.Input;
 using Myra;
 using Myra.Graphics2D.UI;
 
+using Orts.Common;
+using Orts.Models.Simplified;
+using Orts.Formats.Msts.Files;
+using Orts.Toolbox.Settings;
+using static System.Windows.Forms.Design.AxImporter;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+using System.Threading;
+using static Orts.Formats.Msts.FolderStructure.ContentFolder;
+
 namespace Toolbox.YardOffice
 {
     public class GameWindow : Game
@@ -14,21 +26,41 @@ namespace Toolbox.YardOffice
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Desktop Mdesktop;
+        
 
+        private Folder selectedFolder;
+        private Route selectedRoute;
+        private Path selectedPath; // going forward, there may be multiple paths selected at once
+        private IOrderedEnumerable<Folder> folders;
+        private IEnumerable<Route> routes;
+        private IEnumerable<Path> paths;
+        private YardOffice ui;
+
+        
         public GameWindow()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             Window.AllowUserResizing = true;
-            
+
+            IEnumerable<string> options = Environment.GetCommandLineArgs().Where(a => a.StartsWith("-", StringComparison.OrdinalIgnoreCase) || a.StartsWith("/", StringComparison.OrdinalIgnoreCase)).Select(a => a[1..]);
+            Settings = new ToolboxSettings(options);
+
         }
 
-        protected override void Initialize()
+        internal ToolboxSettings Settings { get; }
+
+        protected override async void Initialize()
         {
-            // TODO: Add your initialization logic here
+            List<Task> initTasks = new List<Task>()
+            {
+                LoadFolders(),
+            };
 
             base.Initialize();
+
+            await Task.WhenAll(initTasks).ConfigureAwait(false);
         }
 
         protected override void LoadContent()
@@ -42,8 +74,18 @@ namespace Toolbox.YardOffice
 
             UpdateTitle();
 
-            // Load UI
-            var ui = new YardOffice();
+           // Load UI
+            ui = new YardOffice();
+
+            // Populate Routes           
+
+            var selectItem = ui.menuItemSelect;
+            foreach (Folder folder in folders)
+            {
+                var menuItemRouteName = new MenuItem();
+                menuItemRouteName.Text = folder.Name;
+                selectItem.Items.Add(menuItemRouteName);
+            }
 
             // File/Quit
             var quitItem = ui.menuItemQuit;
@@ -52,6 +94,8 @@ namespace Toolbox.YardOffice
             // Help/About
             var aboutItem = ui.menuItemAbout;
             aboutItem.Selected += AboutItemOnDown;
+
+            
 
             Mdesktop = new Desktop
             {
@@ -94,6 +138,21 @@ namespace Toolbox.YardOffice
             GraphicsDevice.Clear(Color.Black);
 
             Mdesktop.Render();
+           
+        }
+
+        internal async Task LoadFolders()
+        {
+          
+            try
+            {
+               this.folders = (await Folder.GetFolders(Settings.UserSettings.FolderSettings.Folders).ConfigureAwait(true)).OrderBy(f => f.Name);
+            }
+
+            catch (TaskCanceledException)
+            {
+            }
+            
         }
     }
 }
