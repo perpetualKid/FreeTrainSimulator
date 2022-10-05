b45b6f2b8cc6a0d4d65aaa5e7a4df21a5fa6266d
perpetualKid <perpetualKid@azuros.net>
2022-10-05 07:39:19 +0200

    Update ImageControl.cs

--- Source/Orts.Graphics/Window/Controls/ImageControl.cs
+++ Source/Orts.Graphics/Window/Controls/ImageControl.cs
@@ -10,12 +10,20 @@ namespace Orts.Graphics.Window.Controls
         public Rectangle? ClippingRectangle { get; set; }
 
         public ImageControl(WindowBase window, Texture2D image, int x, int y) :
-            base(window, x, y, image?.Width ?? throw new ArgumentNullException(nameof(image)), image.Height)
+            base(window ?? throw new ArgumentNullException(nameof(window)), x, y, 
+                (int)((image?.Width ?? throw new ArgumentNullException(nameof(image))) * window.Owner.DpiScaling), (int)(image.Height * window.Owner.DpiScaling))
         {
             texture = image;
         }
+
+        public ImageControl(WindowBase window, Texture2D image, int x, int y, int width, int height) :
+            base(window ?? throw new ArgumentNullException(nameof(window)), x, y, width, height)
+        {
+            texture = image;
+        }
+
         public ImageControl(WindowBase window, Texture2D image, int x, int y, Rectangle clipping) :
-            base(window, x, y, clipping.Width, clipping.Height)
+            base(window ?? throw new ArgumentNullException(nameof(window)), x, y, (int)(clipping.Width * window.Owner.DpiScaling), (int)(clipping.Height * window.Owner.DpiScaling))
         {
             texture = image;
             ClippingRectangle = clipping;
