87289be6dedabd689f2ae61a18ec4412b37e47f4
perpetualKid <perpetualKid@azuros.net>
2022-12-01 08:53:32 +0100

    Update WindowTextureControl.cs

--- Source/Orts.Graphics/Window/Controls/WindowTextureControl.cs
+++ Source/Orts.Graphics/Window/Controls/WindowTextureControl.cs
@@ -17,7 +17,6 @@ namespace Orts.Graphics.Window.Controls
         {
             if (disposing)
             {
-                texture?.Dispose();
             }
             base.Dispose(disposing);
         }
