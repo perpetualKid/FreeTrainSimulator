9f7d49330d388b957ad801b387cb50b05288a8fc
amtrak115 <amtrak115@gmail.com>
2023-02-19 10:08:54 -0600

    added menu options

--- Source/Toolbox/ConsistEditor/CEForm1.cs
+++ Source/Toolbox/ConsistEditor/CEForm1.cs
@@ -15,6 +15,36 @@ namespace Orts.Toolbox.ConsistEditor
         public CEForm1()
         {
             InitializeComponent();
+
+            // Create a MenuStrip control with a new window.
+            MenuStrip ms = new MenuStrip();
+            ToolStripMenuItem windowMenu = new ToolStripMenuItem("File");
+            ToolStripMenuItem windoweXitMenu = new ToolStripMenuItem("eXit", null, new EventHandler(windoweXitMenu_Click));
+            windowMenu.DropDownItems.Add(windoweXitMenu);
+            ((ToolStripDropDownMenu)(windowMenu.DropDown)).ShowImageMargin = false;
+            ((ToolStripDropDownMenu)(windowMenu.DropDown)).ShowCheckMargin = true;
+
+            // Assign the ToolStripMenuItem that displays 
+            // the list of child forms.
+            ms.MdiWindowListItem = windowMenu;
+
+            // Add the window ToolStripMenuItem to the MenuStrip.
+            ms.Items.Add(windowMenu);
+
+            // Dock the MenuStrip to the top of the form.
+            ms.Dock = DockStyle.Top;
+
+            // The Form.MainMenuStrip property determines the merge target.
+            this.MainMenuStrip = ms;
+
+            // Add the MenuStrip last.
+            // This is important for correct placement in the z-order.
+            this.Controls.Add(ms);
+        }
+
+        private void windoweXitMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
