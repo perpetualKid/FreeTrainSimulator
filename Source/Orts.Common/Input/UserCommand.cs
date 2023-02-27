openrails/openrails

Containers: commit to test Json issues

Source/ORTS.Common/Input/UserCommand.cs
@@ -203,6 +203,7 @@
         [GetString("Control Cylinder Compound")] ControlCylinderCompound,
         [GetString("Control Firing")] ControlFiring,
         [GetString("Control Refill")] ControlRefill,
+        [GetString("Control Discrete Unload")] ControlDiscreteUnload,
         [GetString("Control Water Scoop")] ControlWaterScoop,
         [GetString("Control ImmediateRefill")] ControlImmediateRefill,
         [GetString("Control Turntable Clockwise")] ControlTurntableClockwise,

perpetualKid/FreeTrainSimulator

(cherry picked from commit 3a2b356ffb12d6ad4aa86f2b91791bf93357e5d2)

Source/Orts.Common/Input/UserCommand.cs
@@ -213,6 +213,7 @@ namespace Orts.Common.Input
         [Description("Control Cylinder Compound")] ControlCylinderCompound,
         [Description("Control Firing")] ControlFiring,
         [Description("Control Refill")] ControlRefill,
+        [Description("Control Discrete Unload")] ControlDiscreteUnload,
         [Description("Control Water Scoop")] ControlWaterScoop,
         [Description("Control ImmediateRefill")] ControlImmediateRefill,
         [Description("Control Turntable Clockwise")] ControlTurntableClockwise,