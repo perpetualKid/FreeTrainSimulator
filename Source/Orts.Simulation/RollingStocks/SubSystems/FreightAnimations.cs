openrails/openrails

Containers: bug fix for http://www.elvastower.com/forums/index.php?/topic/36316-odd-consist-bug/page__view__findpost__p__286076

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/FreightAnimations.cs
@@ -254,7 +254,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems
             LoadingAreaLength = copyFACollection.LoadingAreaLength;
             AboveLoadingAreaLength = copyFACollection.AboveLoadingAreaLength;
             Offset = copyFACollection.Offset;
-            GeneralIntakePoint = new IntakePoint(copyFACollection.GeneralIntakePoint);
+            if (copyFACollection.GeneralIntakePoint != null)
+                GeneralIntakePoint = new IntakePoint(copyFACollection.GeneralIntakePoint);
             DoubleStacker = copyFACollection.DoubleStacker;
             if (copyFACollection.LoadDataList?.Count >= 0)
             {
@@ -320,6 +321,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems
 
         public void Load(MSTSWagon wagon, List<LoadData> loadDataList, bool listInWagFile = false)
         {
+            if (GeneralIntakePoint == null)
+                return;
             if (loadDataList != null && loadDataList.Count != 0)
             {
                 foreach (var loadData in loadDataList)

perpetualKid/FreeTrainSimulator

(cherry picked from commit 0bc1d9cb52590bc3503b8b04be016c11e7075f09)

Source/Orts.Simulation/RollingStocks/SubSystems/FreightAnimations.cs
@@ -255,7 +255,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems
             LoadingAreaLength = source.LoadingAreaLength;
             AboveLoadingAreaLength = source.AboveLoadingAreaLength;
             Offset = source.Offset;
-            GeneralIntakePoint = new IntakePoint(source.GeneralIntakePoint);
+            if (source.GeneralIntakePoint != null)
+                GeneralIntakePoint = new IntakePoint(source.GeneralIntakePoint);
             DoubleStacker = source.DoubleStacker;
             if (source.LoadDataList?.Count >= 0)
             {
@@ -313,6 +314,8 @@ namespace Orts.Simulation.RollingStocks.SubSystems
 
         public void Load(MSTSWagon wagon, IList<LoadData> loadDataList, bool listInWagFile = false)
         {
+            if (GeneralIntakePoint == null)
+                return;
             string TrainsetFolder = Simulator.Instance.RouteFolder.ContentFolder.TrainSetsFolder;
             if (loadDataList?.Count > 0)
             {