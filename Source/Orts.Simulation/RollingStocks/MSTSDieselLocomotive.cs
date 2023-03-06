openrails/openrails

Change variable name

Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -669,7 +669,7 @@ namespace Orts.Simulation.RollingStocks
                 // its impact. More modern locomotive have a more sophisticated system that eliminates slip in the majority (if not all circumstances).
                 // Simple adhesion control does not have any slip control feature built into it.
                 // TODO - a full review of slip/no slip control.
-                if (ElectricMotorType == ElectricMotorTypes.AC)
+                if (TractionMotorType == TractionMotorTypes.AC)
                 {
                     AbsTractionSpeedMpS = AbsSpeedMpS;
                     if (AbsWheelSpeedMpS > 1.1 * MaxSpeedMpS)

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -432,12 +432,12 @@ public List<CabView> CabViewList = new List<CabView>();
         public float DynamicBrakeIntervention = -1;
         protected float PreviousDynamicBrakeIntervention = -1;
 
-        public enum ElectricMotorTypes
+        public enum TractionMotorTypes
         {
             AC,
             DC
         }
-        public ElectricMotorTypes ElectricMotorType;
+        public TractionMotorTypes TractionMotorType;
 
         public ILocomotivePowerSupply LocomotivePowerSupply => PowerSupply as ILocomotivePowerSupply;
         public ScriptedTrainControlSystem TrainControlSystem;
@@ -899,16 +899,16 @@ public List<CabView> CabViewList = new List<CabView>();
                         STFException.TraceWarning(stf, "Skipped unknown engine type " + engineType);
                     }
                     break;
-                case "engine(ortselectricmotortype":
+                case "engine(ortstractionmotortype":
                     stf.MustMatch("(");
-                    string electricMotorType = stf.ReadString().ToUpper();
+                    string tractionMotorType = stf.ReadString().ToUpper();
                     try
                     {
-                        ElectricMotorType = (ElectricMotorTypes)Enum.Parse(typeof(ElectricMotorTypes), electricMotorType);
+                        TractionMotorType = (TractionMotorTypes)Enum.Parse(typeof(TractionMotorTypes), tractionMotorType);
                     }
                     catch
                     {
-                        STFException.TraceWarning(stf, "Skipped unknown electric motor type " + electricMotorType);
+                        STFException.TraceWarning(stf, "Skipped unknown traction motor type " + tractionMotorType);
                     }
                     break;
 
@@ -1109,7 +1109,7 @@ public List<CabView> CabViewList = new List<CabView>();
             UnloadingSpeedMpS = locoCopy.UnloadingSpeedMpS;
             SlipControlSystem = locoCopy.SlipControlSystem;
             EngineType = locoCopy.EngineType;
-            ElectricMotorType = locoCopy.ElectricMotorType;
+            TractionMotorType = locoCopy.TractionMotorType;
             TractiveForceCurves = locoCopy.TractiveForceCurves;
             MaxContinuousForceN = locoCopy.MaxContinuousForceN;
             SpeedOfMaxContinuousForceMpS = locoCopy.SpeedOfMaxContinuousForceMpS;
@@ -2237,7 +2237,7 @@ public List<CabView> CabViewList = new List<CabView>();
                 // More modern locomotive have a more sophisticated system that eliminates slip in the majority (if not all circumstances).
                 // Simple adhesion control does not have any slip control feature built into it.
                 // TODO - a full review of slip/no slip control.
-                if (ElectricMotorType == ElectricMotorTypes.AC)
+                if (TractionMotorType == TractionMotorTypes.AC)
                 {
                     AbsTractionSpeedMpS = AbsSpeedMpS;
                     if (AbsWheelSpeedMpS > 1.1 * MaxSpeedMpS)

perpetualKid/FreeTrainSimulator

(cherry picked from commit feff20c94d7ad5ffd4fc599c53a1d245cea26818)

Source/Orts.Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -712,7 +712,7 @@ namespace Orts.Simulation.RollingStocks
                 // its impact. More modern locomotive have a more sophisticated system that eliminates slip in the majority (if not all circumstances).
                 // Simple adhesion control does not have any slip control feature built into it.
                 // TODO - a full review of slip/no slip control.
-                if (ElectricMotorType == ElectricMotorType.AC)
+                if (TractionMotorType == TractionMotorType.AC)
                 {
                     AbsTractionSpeedMpS = AbsSpeedMpS;
                     if (AbsWheelSpeedMpS > 1.1 * MaxSpeedMpS)

Source/Orts.Simulation/RollingStocks/MSTSLocomotive.cs
@@ -78,7 +78,7 @@ namespace Orts.Simulation.RollingStocks
         Void = 2
     }
 
-    public enum ElectricMotorType
+    public enum TractionMotorType
     {
         AC,
         DC
@@ -424,7 +424,7 @@ namespace Orts.Simulation.RollingStocks
         public float DynamicBrakeIntervention = -1;
         protected float PreviousDynamicBrakeIntervention = -1;
 
-        public ElectricMotorType ElectricMotorType { get; private set; }
+        public TractionMotorType TractionMotorType { get; private set; }
 
         public ILocomotivePowerSupply LocomotivePowerSupply => PowerSupply as ILocomotivePowerSupply;
         public ScriptedTrainControlSystem TrainControlSystem;
@@ -893,13 +893,13 @@ namespace Orts.Simulation.RollingStocks
                     else
                         STFException.TraceWarning(stf, "Skipped unknown engine type " + engineType);
                     break;
-                case "engine(ortselectricmotortype":
+                case "engine(ortstractionmotortype":
                     stf.MustMatch("(");
-                    string electricMotorType = stf.ReadString();
-                    if (EnumExtension.GetValue(electricMotorType, out ElectricMotorType electricMotorTypeResult))
-                        ElectricMotorType = electricMotorTypeResult;
+                    string tractionMotorType = stf.ReadString();
+                    if (EnumExtension.GetValue(tractionMotorType, out TractionMotorType electricMotorTypeResult))
+                        TractionMotorType = electricMotorTypeResult;
                     else
-                        STFException.TraceWarning(stf, "Skipped unknown electric motor type " + electricMotorType);
+                        STFException.TraceWarning(stf, "Skipped unknown traction motor type " + tractionMotorType);
                     break;
                 case "engine(enginecontrollers(throttle":
                     ThrottleController = new MSTSNotchController(stf);
@@ -1258,7 +1258,7 @@ namespace Orts.Simulation.RollingStocks
             UnloadingSpeedMpS = sourceLocomotive.UnloadingSpeedMpS;
             SlipControlSystem = sourceLocomotive.SlipControlSystem;
             EngineType = sourceLocomotive.EngineType;
-            ElectricMotorType = sourceLocomotive.ElectricMotorType;
+            TractionMotorType = sourceLocomotive.TractionMotorType;
             TractiveForceCurves = sourceLocomotive.TractiveForceCurves;
             MaxContinuousForceN = sourceLocomotive.MaxContinuousForceN;
             SpeedOfMaxContinuousForceMpS = sourceLocomotive.SpeedOfMaxContinuousForceMpS;
@@ -2312,7 +2312,7 @@ namespace Orts.Simulation.RollingStocks
                 // More modern locomotive have a more sophisticated system that eliminates slip in the majority (if not all circumstances).
                 // Simple adhesion control does not have any slip control feature built into it.
                 // TODO - a full review of slip/no slip control.
-                if (ElectricMotorType == ElectricMotorType.AC)
+                if (TractionMotorType == TractionMotorType.AC)
                 {
                     AbsTractionSpeedMpS = AbsSpeedMpS;
                     if (AbsWheelSpeedMpS > 1.1 * MaxSpeedMpS)