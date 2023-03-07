openrails/openrails

Calculate distance to signal in explorer mode

Source/Orts.Simulation/Simulation/Physics/Train.cs
@@ -9226,22 +9226,46 @@ namespace Orts.Simulation.Physics
             // get next signal
 
             // forward
+            float distanceToSignalForward = 0;
+            float lengthOffset = PresentPosition[0].TCOffset;
             NextSignalObject[0] = null;
             for (int iindex = 0; iindex < ValidRoute[0].Count && NextSignalObject[0] == null; iindex++)
             {
                 TCRouteElement thisElement = ValidRoute[0][iindex];
                 TrackCircuitSection thisSection = signalRef.TrackCircuitList[thisElement.TCSectionIndex];
                 NextSignalObject[0] = thisSection.EndSignals[thisElement.Direction];
+                if (iindex >= PresentPosition[0].RouteListIndex)
+                {
+                    distanceToSignalForward += thisSection.Length - lengthOffset;
+                    lengthOffset = 0;
+                }
             }
 
             // backward
+            float distanceToSignalBackward = 0;
+            lengthOffset = 0;
+            int presentIndex = -1;
             NextSignalObject[1] = null;
             for (int iindex = 0; iindex < ValidRoute[1].Count && NextSignalObject[1] == null; iindex++)
             {
                 TCRouteElement thisElement = ValidRoute[1][iindex];
                 TrackCircuitSection thisSection = signalRef.TrackCircuitList[thisElement.TCSectionIndex];
                 NextSignalObject[1] = thisSection.EndSignals[thisElement.Direction];
+                if (presentIndex == -1 && PresentPosition[1].TCSectionIndex == thisElement.TCSectionIndex)
+                {
+                    lengthOffset = -PresentPosition[1].TCOffset + signalRef.TrackCircuitList[PresentPosition[1].TCSectionIndex].Length;
+                    presentIndex = iindex;
+                }
+                if (presentIndex != -1 && presentIndex <= iindex)
+                {
+                    distanceToSignalBackward += thisSection.Length - lengthOffset;
+                    lengthOffset = 0;
+                }
             }
+
+            DistanceToSignal = null;
+            if (MUDirection != Direction.Reverse && NextSignalObject[0] != null) DistanceToSignal = distanceToSignalForward;
+            if (MUDirection == Direction.Reverse && NextSignalObject[1] != null) DistanceToSignal = distanceToSignalBackward;
 
             // clear all build up distance actions
             requiredActions.RemovePendingAIActionItems(true);

perpetualKid/FreeTrainSimulator

(cherry picked from commit e4ba7221a52cec54e18fede94f6af4d6b070290a)

Source/Orts.Simulation/Physics/Train.cs
@@ -49,6 +49,7 @@ using System.Diagnostics.Eventing.Reader;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 using GetText;
@@ -6827,23 +6828,48 @@ namespace Orts.Simulation.Physics
             // get next signal
 
             // forward
+            float distanceToSignalForward = 0;
+            float lengthOffset = PresentPosition[Direction.Forward].Offset;
             NextSignalObject[0] = null;
             for (int i = 0; i < ValidRoute[0].Count && NextSignalObject[0] == null; i++)
             {
                 TrackCircuitRouteElement routeElement = ValidRoute[0][i];
                 TrackCircuitSection section = routeElement.TrackCircuitSection;
                 NextSignalObject[0] = section.EndSignals[routeElement.Direction];
+                if (i >= PresentPosition[Direction.Forward].RouteListIndex)
+                {
+                    distanceToSignalForward += section.Length - lengthOffset;
+                    lengthOffset = 0;
+                }
             }
 
             // backward
+            float distanceToSignalBackward = 0;
+            lengthOffset = 0;
+            int presentIndex = -1;
             NextSignalObject[1] = null;
             for (int i = 0; i < ValidRoute[1].Count && NextSignalObject[1] == null; i++)
             {
                 TrackCircuitRouteElement routeElement = ValidRoute[1][i];
                 TrackCircuitSection section = routeElement.TrackCircuitSection;
                 NextSignalObject[1] = section.EndSignals[routeElement.Direction];
+                if (presentIndex == -1 && PresentPosition[Direction.Backward].TrackCircuitSectionIndex == routeElement.TrackCircuitSection.Index)
+                {
+                    lengthOffset = -PresentPosition[Direction.Backward].Offset + TrackCircuitSection.TrackCircuitList[PresentPosition[Direction.Backward].TrackCircuitSectionIndex].Length;
+                    presentIndex = i;
+                }
+                if (presentIndex != -1 && presentIndex <= i)
+                {
+                    distanceToSignalBackward += section.Length - lengthOffset;
+                    lengthOffset = 0;
+                }
             }
 
+            DistanceToSignal = null;
+            if (MUDirection != MidpointDirection.Reverse && NextSignalObject[0] != null)
+                DistanceToSignal = distanceToSignalForward;
+            if (MUDirection == MidpointDirection.Reverse && NextSignalObject[1] != null)
+                DistanceToSignal = distanceToSignalBackward;
             // clear all build up distance actions
             RequiredActions.RemovePendingAIActionItems(true);
         }