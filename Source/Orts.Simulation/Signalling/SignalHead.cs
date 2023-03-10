openrails/openrails

Moved signals initialization after world info addition

Source/Orts.Simulation/Simulation/Signalling/SignalHead.cs
@@ -143,7 +143,6 @@ namespace Orts.Simulation.Signalling
                 usedCsSignalScript = Signals.CsSignalScripts.LoadSignalScript(signalType.Script)
                     ?? Signals.CsSignalScripts.LoadSignalScript(signalType.Name);
                 usedCsSignalScript?.AttachToHead(this);
-                usedCsSignalScript?.Initialize();
 
                 // set signal speeds
                 foreach (SignalAspect thisAspect in signalType.Aspects)
@@ -188,8 +187,11 @@ namespace Orts.Simulation.Signalling
                 Trace.TraceWarning("SignalObject trItem={0}, trackNode={1} has SignalHead with undefined SignalType {2}.",
                                   mainSignal.trItem, mainSignal.trackNode, sigItem.SignalType);
             }
+        }
 
-
+        public void Initialize()
+        {
+            usedCsSignalScript?.Initialize();
         }
 
         /// <summary>

perpetualKid/FreeTrainSimulator

(cherry picked from commit 6553f9a12203db1a7b30b3c202215caefd239c98)

Source/Orts.Simulation/Signalling/SignalHead.cs
@@ -117,7 +117,6 @@ namespace Orts.Simulation.Signalling
                 if (csSignalScript != null)
                 {
                     csSignalScript.AttachToHead(this);
-                    csSignalScript.Initialize();
                 }
 
                 // set signal speeds
@@ -160,6 +159,11 @@ namespace Orts.Simulation.Signalling
             {
                 Trace.TraceWarning($"SignalObject trItem={MainSignal.TrackItemIndex}, trackNode={MainSignal.TrackNode} has SignalHead with undefined SignalType {signalItem.SignalType}.");
             }
+        }
+
+        public void Initialize()
+        {
+            csSignalScript?.Initialize();
         }
 
         //================================================================================================//