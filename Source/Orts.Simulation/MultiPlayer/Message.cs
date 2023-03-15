openrails/openrails

Added C# signal script functions for setting speed info and refactored signal functions, signal object types and signal speed info Fixed display of SPEED signals on track monitor

Source/Orts.Simulation/MultiPlayer/Message.cs
@@ -3056,7 +3056,7 @@ namespace Orts.MultiPlayer
                 {
                     foreach (var s in MPManager.Simulator.Signals.SignalObjects)
                     {
-                        if (s != null && (s.isSignal || s.isSpeedSignal) && s.SignalHeads != null)
+                        if (s != null && (s.Type == SignalObjectType.Signal || s.Type == SignalObjectType.SpeedSignal) && s.SignalHeads != null)
                             foreach (var h in s.SignalHeads)
                             {
                                 //System.Console.WriteLine(h.TDBIndex);
@@ -3124,7 +3124,7 @@ namespace Orts.MultiPlayer
                     {
                         foreach (var s in MPManager.Simulator.Signals.SignalObjects)
                         {
-                            if (s != null && (s.isSignal || s.isSpeedSignal) && s.SignalHeads != null)
+                            if (s != null && (s.Type == SignalObjectType.Signal || s.Type == SignalObjectType.SpeedSignal) && s.SignalHeads != null)
                                 foreach (var h in s.SignalHeads)
                                 {
                                     //System.Console.WriteLine(h.TDBIndex);

perpetualKid/FreeTrainSimulator

(cherry picked from commit 939b2b2651efab7fe0f5f666d39b93dfc75a41ec)

Source/Orts.Simulation/MultiPlayer/Message.cs
@@ -3111,7 +3111,7 @@ namespace Orts.Simulation.MultiPlayer
                 {
                     foreach (var s in Simulator.Instance.SignalEnvironment.Signals)
                     {
-                        if (s != null && (s.IsSignal || s.IsSpeedSignal) && s.SignalHeads != null)
+                        if (s != null && (s.SignalType == SignalCategory.Signal || s.SignalType == SignalCategory.SpeedSignal) && s.SignalHeads != null)
                             foreach (var h in s.SignalHeads)
                             {
                                 //Trace.WriteLine(h.TDBIndex);
@@ -3182,7 +3182,7 @@ namespace Orts.Simulation.MultiPlayer
                     {
                         foreach (var s in Simulator.Instance.SignalEnvironment.Signals)
                         {
-                            if (s != null && (s.IsSignal || s.IsSpeedSignal) && s.SignalHeads != null)
+                            if (s != null && (s.SignalType == SignalCategory.Signal || s.SignalType == SignalCategory.SpeedSignal) && s.SignalHeads != null)
                                 foreach (var h in s.SignalHeads)
                                 {
                                     //Trace.WriteLine(h.TDBIndex);