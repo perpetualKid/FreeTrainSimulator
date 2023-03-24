openrails/openrails

https://blueprints.launchpad.net/or/+spec/cruise-control ; initiator and main contributor: Jindrich

Source/ORTS.Common/Input/UserCommand.cs
@@ -217,6 +217,20 @@
         [GetString("Control AI Fire Off")] ControlAIFireOff,
         [GetString("Control AI Fire Reset")] ControlAIFireReset,
 
+        // Cruise Control
+        [GetString("Control Speed Regulator Mode Increase")] ControlSpeedRegulatorModeIncrease,
+        [GetString("Control Speed Regulator Mode Descrease")] ControlSpeedRegulatorModeDecrease,
+        [GetString("Control Selected Speed Increase")] ControlSpeedRegulatorSelectedSpeedIncrease,
+        [GetString("Control Selected Speed Decrease")] ControlSpeedRegulatorSelectedSpeedDecrease,
+        [GetString("Control Speed Regulator Max Acceleration Increase")] ControlSpeedRegulatorMaxAccelerationIncrease,
+        [GetString("Control Speed Regulator Max Acceleration Decrease")] ControlSpeedRegulatorMaxAccelerationDecrease,
+        [GetString("Control Number Of Axles Increase")] ControlNumberOfAxlesIncrease,
+        [GetString("Control Number Of Axles Decrease")] ControlNumberOfAxlesDecrease,
+        [GetString("Control Restricted Speed Zone Active")] ControlRestrictedSpeedZoneActive,
+        [GetString("Control Cruise Control Mode Increase")] ControlCruiseControlModeIncrease,
+        [GetString("Control Cruise Control Mode Decrease")] ControlCruiseControlModeDecrease,
+        [GetString("Control Train Type Change (Passenger/Cargo)")] ControlTrainTypePaxCargo,
+        [GetString("Control Selected Speed To Zero")] ControlSpeedRegulatorSelectedSpeedToZero,
         //Distributed power
         [GetString("Control DP Move To Front")] ControlDPMoveToFront,
         [GetString("Control DP Move To Back")] ControlDPMoveToBack,

perpetualKid/FreeTrainSimulator

(cherry picked from commit 637f947b2159ccdbb74d1048b5919ca441a0ae0a)

Source/Orts.Common/Input/UserCommand.cs
@@ -226,6 +226,20 @@ namespace Orts.Common.Input
         [Description("Control AI Fire On")] ControlAIFireOn,
         [Description("Control AI Fire Off")] ControlAIFireOff,
         [Description("Control AI Fire Reset")] ControlAIFireReset,
+        // Cruise Control
+        [Description("Control Speed Regulator Mode Increase")] ControlSpeedRegulatorModeIncrease,
+        [Description("Control Speed Regulator Mode Descrease")] ControlSpeedRegulatorModeDecrease,
+        [Description("Control Selected Speed Increase")] ControlSpeedRegulatorSelectedSpeedIncrease,
+        [Description("Control Selected Speed Decrease")] ControlSpeedRegulatorSelectedSpeedDecrease,
+        [Description("Control Speed Regulator Max Acceleration Increase")] ControlSpeedRegulatorMaxAccelerationIncrease,
+        [Description("Control Speed Regulator Max Acceleration Decrease")] ControlSpeedRegulatorMaxAccelerationDecrease,
+        [Description("Control Number Of Axles Increase")] ControlNumberOfAxlesIncrease,
+        [Description("Control Number Of Axles Decrease")] ControlNumberOfAxlesDecrease,
+        [Description("Control Restricted Speed Zone Active")] ControlRestrictedSpeedZoneActive,
+        [Description("Control Cruise Control Mode Increase")] ControlCruiseControlModeIncrease,
+        [Description("Control Cruise Control Mode Decrease")] ControlCruiseControlModeDecrease,
+        [Description("Control Train Type Change (Passenger/Cargo)")] ControlTrainTypePaxCargo,
+        [Description("Control Selected Speed To Zero")] ControlSpeedRegulatorSelectedSpeedToZero,
         //Distributed power
         [Description("Control DP Move To Front")] ControlDistributedPowerMoveToFront,
         [Description("Control DP Move To Back")] ControlDistributedPowerMoveToBack,