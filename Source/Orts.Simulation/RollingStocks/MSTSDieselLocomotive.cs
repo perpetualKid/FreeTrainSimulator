openrails/openrails

Code refactoring for 3D cabs

Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -848,7 +848,7 @@ namespace Orts.Simulation.RollingStocks
         {
             float data = 0;
 
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.Type)
             {
                 case CABViewControlTypes.GEARS:
                     if (DieselEngines.HasGearBox)
@@ -1442,7 +1442,7 @@ namespace Orts.Simulation.RollingStocks
                         {
                             foreach ( var control in cabView.CVFFile.CabViewControls)
                             {
-                                if (control is CVCDiscrete && control.ControlType == CABViewControlTypes.THROTTLE && (control as CVCDiscrete).Values.Count > 0 && (control as CVCDiscrete).Values[(control as CVCDiscrete).Values.Count - 1] > 1)
+                                if (control is CVCDiscrete && control.ControlType.Type == CABViewControlTypes.THROTTLE && (control as CVCDiscrete).Values.Count > 0 && (control as CVCDiscrete).Values[(control as CVCDiscrete).Values.Count - 1] > 1)
                                 {
                                     var discreteControl = (CVCDiscrete)control;
                                     for (var i = 0; i < discreteControl.Values.Count; i++)

Source/Orts.Simulation/Simulation/RollingStocks/MSTSElectricLocomotive.cs
@@ -221,7 +221,7 @@ namespace Orts.Simulation.RollingStocks
         {
             float data = 0;
 
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.Type)
             {
                 case CABViewControlTypes.LINE_VOLTAGE:
                     data = ElectricPowerSupply.PantographVoltageV;

Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -634,8 +634,8 @@ public List<CabView> CabViewList = new List<CabView>();
                 {
                     msDisplay = (CVCMultiStateDisplay) cabView.CVFFile.CabViewControls.Where(
                         control => control is CVCMultiStateDisplay &&
-                        (((CVCMultiStateDisplay) control).ControlType == CABViewControlTypes.DYNAMIC_BRAKE_DISPLAY ||
-                        ((CVCMultiStateDisplay) control).ControlType == CABViewControlTypes.CPH_DISPLAY)).First();
+                        (((CVCMultiStateDisplay) control).ControlType.Type == CABViewControlTypes.DYNAMIC_BRAKE_DISPLAY ||
+                        ((CVCMultiStateDisplay) control).ControlType.Type == CABViewControlTypes.CPH_DISPLAY)).First();
                 }
                 catch
                 {
@@ -643,7 +643,7 @@ public List<CabView> CabViewList = new List<CabView>();
                 }
                 if (msDisplay != null)
                 {
-                    if (msDisplay.ControlType == CABViewControlTypes.DYNAMIC_BRAKE_DISPLAY)
+                    if (msDisplay.ControlType.Type == CABViewControlTypes.DYNAMIC_BRAKE_DISPLAY)
                     {
                         foreach (var switchval in msDisplay.Values)
                             dpDynController.AddNotch((float) switchval);
@@ -705,11 +705,8 @@ public List<CabView> CabViewList = new List<CabView>();
                         CabViewControls cvcList = CabViewList[0].CVFFile.CabViewControls;
                         foreach (CabViewControl cvc in cvcList)
                         {
-                            if (brakeSystemComponents.ContainsKey(cvc.ControlType) && pressureUnits.ContainsKey(cvc.Units))
+                            if (brakeSystemComponents.TryGetValue(cvc.ControlType.Type, out var component) && pressureUnits.TryGetValue(cvc.Units, out var unit))
                             {
-                                BrakeSystemComponent component = brakeSystemComponents[cvc.ControlType];
-                                PressureUnit unit = pressureUnits[cvc.Units];
-
                                 BrakeSystemPressureUnits[component] = unit;
                             }
                         }
@@ -4992,7 +4989,7 @@ public List<CabView> CabViewList = new List<CabView>();
         public virtual float GetDataOf(CabViewControl cvc)
         {
             float data = 0;
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.Type)
             {
                 case CABViewControlTypes.SPEEDOMETER:
                     {
@@ -5090,7 +5087,7 @@ public List<CabView> CabViewList = new List<CabView>();
                             if (DynamicBrakePercent > 0 && MaxDynamicBrakeForceN > 0)
                             {
                                 float rangeFactor;
-                                if (cvc.ControlType == CABViewControlTypes.AMMETER_ABS)
+                                if (cvc.ControlType.Type == CABViewControlTypes.AMMETER_ABS)
                                 {
                                     if (DynamicBrakeMaxCurrentA == 0)
                                         rangeFactor = direction == 0 ? (float)cvc.MaxValue : (float)cvc.MinValue;
@@ -5108,11 +5105,11 @@ public List<CabView> CabViewList = new List<CabView>();
                             }
                             if (direction == 1)
                                 data = -data;
-                            if (cvc.ControlType == CABViewControlTypes.AMMETER_ABS) data = Math.Abs(data);
+                            if (cvc.ControlType.Type == CABViewControlTypes.AMMETER_ABS) data = Math.Abs(data);
                             break;
                         }
                         data = this.MotiveForceN / MaxForceN * MaxCurrentA;
-                        if (cvc.ControlType == CABViewControlTypes.AMMETER_ABS) data = Math.Abs(data);
+                        if (cvc.ControlType.Type == CABViewControlTypes.AMMETER_ABS) data = Math.Abs(data);
                         break;
                     }
                 case CABViewControlTypes.LOAD_METER:
@@ -5819,7 +5816,7 @@ public List<CabView> CabViewList = new List<CabView>();
                     }
 
                 case CABViewControlTypes.ORTS_TCS:
-                    TrainControlSystem.CabDisplayControls.TryGetValue(cvc.ControlSubtype - 1, out data);
+                    TrainControlSystem.CabDisplayControls.TryGetValue(cvc.ControlType.Subtype - 1, out data);
                     break;
 
                 case CABViewControlTypes.ORTS_BATTERY_SWITCH_COMMAND_SWITCH:

Source/Orts.Simulation/Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -5856,7 +5856,7 @@ namespace Orts.Simulation.RollingStocks
         {
             float data;
 
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.Type)
             {
                 case CABViewControlTypes.WHISTLE:
                     data = Horn ? 1 : 0;

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/CruiseControl.cs
@@ -1414,7 +1414,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
         public float GetDataOf(CabViewControl cvc)
         {
             float data = 0;
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.Type)
             {
                 case CABViewControlTypes.ORTS_SELECTED_SPEED:
                 case CABViewControlTypes.ORTS_SELECTED_SPEED_DISPLAY:

perpetualKid/FreeTrainSimulator

(cherry picked from commit e2c841da0084207cad82b47991e4abd954f0e48e)

Source/Orts.Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -889,7 +889,7 @@ namespace Orts.Simulation.RollingStocks
         {
             float data = 0;
 
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.CabViewControlType)
             {
                 case CabViewControlType.Gears:
                     if (DieselEngines.GearBox is GearBox gearBox)
@@ -1400,7 +1400,7 @@ namespace Orts.Simulation.RollingStocks
                         {
                             foreach (var control in cabView.CVFFile.CabViewControls)
                             {
-                                if (control is CabViewDiscreteControl discreteCabControl && control.ControlType == CabViewControlType.Throttle && discreteCabControl.Values.Count > 0 && discreteCabControl.Values[^1] > 1)
+                                if (control is CabViewDiscreteControl discreteCabControl && control.ControlType.CabViewControlType == CabViewControlType.Throttle && discreteCabControl.Values.Count > 0 && discreteCabControl.Values[^1] > 1)
                                 {
                                     var discreteControl = discreteCabControl;
                                     for (var i = 0; i < discreteControl.Values.Count; i++)

Source/Orts.Simulation/RollingStocks/MSTSElectricLocomotive.cs
@@ -226,7 +226,7 @@ namespace Orts.Simulation.RollingStocks
         {
             float data = 0;
 
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.CabViewControlType)
             {
                 case CabViewControlType.Line_Voltage:
                     data = ElectricPowerSupply.PantographVoltageV;

Source/Orts.Simulation/RollingStocks/MSTSLocomotive.cs
@@ -635,10 +635,10 @@ namespace Orts.Simulation.RollingStocks
             if (cabView != null)
             {
                 msDisplay = cabView.CVFFile.CabViewControls.OfType<CabViewMultiStateDisplayControl>().Where(
-                    control => (control.ControlType == CabViewControlType.Dynamic_Brake_Display || control.ControlType == CabViewControlType.Cph_Display)).FirstOrDefault();
+                    control => (control.ControlType.CabViewControlType == CabViewControlType.Dynamic_Brake_Display || control.ControlType.CabViewControlType == CabViewControlType.Cph_Display)).FirstOrDefault();
                 if (msDisplay != null)
                 {
-                    if (msDisplay.ControlType == CabViewControlType.Dynamic_Brake_Display)
+                    if (msDisplay.ControlType.CabViewControlType == CabViewControlType.Dynamic_Brake_Display)
                     {
                         foreach (var switchval in msDisplay.Values)
                             dpDynController.AddNotch((float)switchval);
@@ -700,11 +700,9 @@ namespace Orts.Simulation.RollingStocks
                         CabViewControls cvcList = CabViewList[0].CVFFile.CabViewControls;
                         foreach (CabViewControl cvc in cvcList)
                         {
-                            if (brakeSystemComponents.ContainsKey(cvc.ControlType) && pressureUnits.ContainsKey(cvc.ControlUnit))
+                            if (brakeSystemComponents.TryGetValue(cvc.ControlType.CabViewControlType, out BrakeSystemComponent component) 
+                                && pressureUnits.TryGetValue(cvc.ControlUnit, out Pressure.Unit unit))
                             {
-                                BrakeSystemComponent component = brakeSystemComponents[cvc.ControlType];
-                                Pressure.Unit unit = pressureUnits[cvc.ControlUnit];
-
                                 BrakeSystemPressureUnits[component] = unit;
                             }
                         }
@@ -5117,7 +5115,7 @@ namespace Orts.Simulation.RollingStocks
         public virtual float GetDataOf(CabViewControl cvc)
         {
             float data = 0;
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.CabViewControlType)
             {
                 case CabViewControlType.Speedometer:
                     {
@@ -5216,7 +5214,7 @@ namespace Orts.Simulation.RollingStocks
                             if (DynamicBrakePercent > 0 && MaxDynamicBrakeForceN > 0)
                             {
                                 float rangeFactor;
-                                if (cvc.ControlType == CabViewControlType.AmMeter_Abs)
+                                if (cvc.ControlType.CabViewControlType == CabViewControlType.AmMeter_Abs)
                                 {
                                     if (DynamicBrakeMaxCurrentA == 0)
                                         rangeFactor = direction == 0 ? (float)cvc.ScaleRangeMax : (float)cvc.ScaleRangeMin;
@@ -5234,12 +5232,12 @@ namespace Orts.Simulation.RollingStocks
                             }
                             if (direction == 1)
                                 data = -data;
-                            if (cvc.ControlType == CabViewControlType.AmMeter_Abs)
+                            if (cvc.ControlType.CabViewControlType == CabViewControlType.AmMeter_Abs)
                                 data = Math.Abs(data);
                             break;
                         }
                         data = this.MotiveForceN / MaxForceN * MaxCurrentA;
-                        if (cvc.ControlType == CabViewControlType.AmMeter_Abs)
+                        if (cvc.ControlType.CabViewControlType == CabViewControlType.AmMeter_Abs)
                             data = Math.Abs(data);
                         break;
                     }
@@ -5902,7 +5900,7 @@ namespace Orts.Simulation.RollingStocks
                 case CabViewControlType.Orts_LeftDoor:
                 case CabViewControlType.Orts_RightDoor:
                     {
-                        bool right = (cvc.ControlType == CabViewControlType.Orts_RightDoor) ^ Flipped ^ GetCabFlipped();
+                        bool right = (cvc.ControlType.CabViewControlType == CabViewControlType.Orts_RightDoor) ^ Flipped ^ GetCabFlipped();
                         var state = Train.DoorState(right ? DoorSide.Right : DoorSide.Left);
                         data = state >= DoorState.Opening ? 1 : 0;
                     }
@@ -5943,7 +5941,7 @@ namespace Orts.Simulation.RollingStocks
                     }
 
                 case CabViewControlType.Orts_TCS:
-                    TrainControlSystem.CabDisplayControls.TryGetValue(cvc.ControlSubtype - 1, out data);
+                    TrainControlSystem.CabDisplayControls.TryGetValue(cvc.ControlType.Subtype - 1, out data);
                     break;
 
                 case CabViewControlType.Orts_Battery_Switch_Command_Switch:

Source/Orts.Simulation/RollingStocks/MSTSSteamLocomotive.cs
@@ -6248,7 +6248,7 @@ namespace Orts.Simulation.RollingStocks
         {
             float data;
 
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.CabViewControlType)
             {
                 case CabViewControlType.Whistle:
                     data = Horn ? 1 : 0;

Source/Orts.Simulation/RollingStocks/SubSystems/CruiseControl.cs
@@ -1673,7 +1673,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
         public float GetDataOf(CabViewControl cvc)
         {
             float data = 0;
-            switch (cvc.ControlType)
+            switch (cvc.ControlType.CabViewControlType)
             {
                 case CabViewControlType.Orts_Selected_Speed:
                 case CabViewControlType.Orts_Selected_Speed_Display: