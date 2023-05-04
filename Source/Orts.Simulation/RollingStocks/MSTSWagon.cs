openrails/openrails

Add STF parsing

Source/Orts.Simulation/Simulation/RollingStocks/MSTSWagon.cs
@@ -68,9 +68,9 @@ namespace Orts.Simulation.RollingStocks
     {
         public Pantographs Pantographs;
         public ScriptedPassengerCarPowerSupply PassengerCarPowerSupply => PowerSupply as ScriptedPassengerCarPowerSupply;
-        
-        public Door RightDoor;
-        public Door LeftDoor;
+        public Doors Doors;        
+        public Door RightDoor => Doors.RightDoor;
+        public Door LeftDoor => Doors.LeftDoor;
         public bool MirrorOpen;
         public bool UnloadingPartsOpen;
         public bool WaitForAnimationReady; // delay counter to start loading/unliading is on;
@@ -346,8 +346,7 @@ namespace Orts.Simulation.RollingStocks
             : base(simulator, wagFilePath)
         {
             Pantographs = new Pantographs(this);
-            RightDoor = new Door(this, true);
-            LeftDoor = new Door(this, false);
+            Doors = new Doors(this);
         }
 
         public void Load()
@@ -954,8 +953,7 @@ namespace Orts.Simulation.RollingStocks
         public override void Initialize()
         {
             Pantographs.Initialize();
-            RightDoor.Initialize();
-            LeftDoor.Initialize();
+            Doors.Initialize();
             PassengerCarPowerSupply?.Initialize();
 
             base.Initialize();
@@ -1380,6 +1378,10 @@ namespace Orts.Simulation.RollingStocks
                 case "wagon(ortspantographs":
                     Pantographs.Parse(lowercasetoken, stf);
                     break;
+                case "wagon(ortsdoors(closingdelay":
+                case "wagon(ortsdoors(openingdelay":
+                    Doors.Parse(lowercasetoken, stf);
+                    break;
                 case "wagon(ortspowersupply":
                 case "wagon(ortspowerondelay":
                 case "wagon(ortsbattery(mode":
@@ -1544,8 +1546,7 @@ namespace Orts.Simulation.RollingStocks
             foreach (MSTSCoupling coupler in copy.Couplers)
                 Couplers.Add(coupler);
             Pantographs.Copy(copy.Pantographs);
-            LeftDoor.Copy(copy.LeftDoor);
-            RightDoor.Copy(copy.RightDoor);
+            Doors.Copy(copy.Doors);
             if (copy.FreightAnimations != null)
             {
                 FreightAnimations = new FreightAnimations(copy.FreightAnimations, this);
@@ -1679,8 +1680,7 @@ namespace Orts.Simulation.RollingStocks
             foreach (MSTSCoupling coupler in Couplers)
                 coupler.Save(outf);
             Pantographs.Save(outf);
-            LeftDoor.Save(outf);
-            RightDoor.Save(outf);
+            Doors.Save(outf);
             PassengerCarPowerSupply?.Save(outf);
             if (FreightAnimations != null)
             {
@@ -1733,8 +1733,7 @@ namespace Orts.Simulation.RollingStocks
             MaxHandbrakeForceN = inf.ReadSingle();
             Couplers = ReadCouplersFromSave(inf).ToList();
             Pantographs.Restore(inf);
-            LeftDoor.Restore(inf);
-            RightDoor.Restore(inf);
+            Doors.Restore(inf);
             PassengerCarPowerSupply?.Restore(inf);
             if (FreightAnimations != null)
             {
@@ -1907,8 +1906,7 @@ namespace Orts.Simulation.RollingStocks
 
             Pantographs.Update(elapsedClockSeconds);
 
-            LeftDoor.Update(elapsedClockSeconds);
-            RightDoor.Update(elapsedClockSeconds);
+            Doors.Update(elapsedClockSeconds);
 
             MSTSBrakeSystem.Update(elapsedClockSeconds);
 

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Door.cs
@@ -31,12 +31,81 @@ namespace Orts.Simulation.RollingStocks.SubSystems
         Opening,
         Open,
     }
+    public class Doors : ISubSystem<Doors>
+    {
+        public Door RightDoor;
+        public Door LeftDoor;
+
+        public Doors(MSTSWagon wagon)
+        {
+            LeftDoor = new Door(wagon, false);
+            RightDoor = new Door(wagon, true);
+        }
+
+        public virtual void Parse(string lowercasetoken, STFReader stf)
+        {
+            switch (lowercasetoken)
+            {
+                case "wagon(ortsdoors(closingdelay": 
+                {
+                    float delayS = stf.ReadFloatBlock(STFReader.UNITS.Time, 0f);
+                    LeftDoor.ClosingDelayS = delayS;
+                    RightDoor.ClosingDelayS = delayS;
+                    break;
+                }
+                case "wagon(ortsdoors(openingdelay": 
+                {
+                    float delayS = stf.ReadFloatBlock(STFReader.UNITS.Time, 0f);
+                    LeftDoor.OpeningDelayS = delayS;
+                    RightDoor.OpeningDelayS = delayS;
+                    break;
+                }
+            }
+        }
+
+        public void Copy(Doors other)
+        {
+            LeftDoor.Copy(other.LeftDoor);
+            RightDoor.Copy(other.RightDoor);
+        }
+
+        public virtual void Initialize()
+        {
+            LeftDoor.Initialize();
+            RightDoor.Initialize();
+        }
+
+        /// <summary>
+        /// Initialization when simulation starts with moving train
+        /// </summary>
+        public virtual void InitializeMoving()
+        {
+        }
+
+        public virtual void Save(BinaryWriter outf)
+        {
+            LeftDoor.Save(outf);
+            RightDoor.Save(outf);
+        }
+
+        public virtual void Restore(BinaryReader inf)
+        {
+            LeftDoor.Restore(inf);
+            RightDoor.Restore(inf);
+        }
+
+        public virtual void Update(float elapsedClockSeconds)
+        {
+            LeftDoor.Update(elapsedClockSeconds);
+            RightDoor.Update(elapsedClockSeconds);
+        }
+    }
     public class Door : ISubSystem<Door>
     {
         
         // Parameters
-        public float OpeningDelayS { get; protected set; } = 0f;
-        public float ClosingDelayS { get; protected set; } = 0f;
+        public float OpeningDelayS { get; set; } = 0f;
+        public float ClosingDelayS { get; set; } = 0f;
 
         // Variables
         readonly MSTSWagon Wagon;
@@ -59,16 +128,6 @@ namespace Orts.Simulation.RollingStocks.SubSystems
 
         public virtual void Parse(string lowercasetoken, STFReader stf)
         {
-            switch (lowercasetoken)
-            {
-                case "wagon(ortsdoors(closingdelay":
-                    ClosingDelayS = stf.ReadFloatBlock(STFReader.UNITS.Time, 0f);
-                    break;
-
-                case "engine(ortsdoors(openingdelay":
-                    OpeningDelayS = stf.ReadFloatBlock(STFReader.UNITS.Time, 0f);
-                    break;
-            }
         }
 
         public void Copy(Door other)

perpetualKid/FreeTrainSimulator

(cherry picked from commit f786fff44e1b23d7d939777f52c7050290a3664a)

Source/Orts.Simulation/RollingStocks/MSTSWagon.cs
@@ -84,8 +84,9 @@ namespace Orts.Simulation.RollingStocks
 
         public Pantographs Pantographs;
         public ScriptedPassengerCarPowerSupply PassengerCarPowerSupply => PowerSupply as ScriptedPassengerCarPowerSupply;
-        public Door RightDoor { get; }
-        public Door LeftDoor { get; }
+        public Doors Doors { get; }        
+        public Door RightDoor => Doors.RightDoor;
+        public Door LeftDoor => Doors.LeftDoor;
         public bool MirrorOpen;
         public bool UnloadingPartsOpen;
         public bool WaitForAnimationReady; // delay counter to start loading/unliading is on;
@@ -349,8 +350,7 @@ namespace Orts.Simulation.RollingStocks
             : base(wagFilePath)
         {
             Pantographs = new Pantographs(this);
-            RightDoor = new Door(this, true);
-            LeftDoor = new Door(this, false);
+            Doors = new Doors(this);
         }
 
         public void Load()
@@ -969,8 +969,7 @@ namespace Orts.Simulation.RollingStocks
         public override void Initialize()
         {
             Pantographs.Initialize();
-            RightDoor.Initialize();
-            LeftDoor.Initialize();
+            Doors.Initialize();
             PassengerCarPowerSupply?.Initialize();
 
             base.Initialize();
@@ -1432,7 +1431,10 @@ namespace Orts.Simulation.RollingStocks
                 case "wagon(ortspantographs":
                     Pantographs.Parse(lowercasetoken, stf);
                     break;
-
+                case "wagon(ortsdoors(closingdelay":
+                case "wagon(ortsdoors(openingdelay":
+                    Doors.Parse(lowercasetoken, stf);
+                    break;
                 case "wagon(ortspowersupply":
                 case "wagon(ortspowerondelay":
                 case "wagon(ortsbattery(mode":
@@ -1609,8 +1611,8 @@ namespace Orts.Simulation.RollingStocks
             avancedCoupler = source.avancedCoupler;
             couplers = new EnumArray<Coupler, TrainCarLocation>(source.couplers);
             Pantographs.Copy(source.Pantographs);
-            LeftDoor.Copy(source.LeftDoor);
-            RightDoor.Copy(source.RightDoor);
+            Doors.Copy(source.Doors);
+
             if (source.FreightAnimations != null)
             {
                 FreightAnimations = new FreightAnimations(source.FreightAnimations, this);
@@ -1738,8 +1740,7 @@ namespace Orts.Simulation.RollingStocks
                 coupler?.Save(outf);
             }
             Pantographs.Save(outf);
-            LeftDoor.Save(outf);
-            RightDoor.Save(outf);
+            Doors.Save(outf);
             PassengerCarPowerSupply?.Save(outf);
             if (FreightAnimations != null)
             {
@@ -1793,8 +1794,7 @@ namespace Orts.Simulation.RollingStocks
             MaxHandbrakeForceN = inf.ReadSingle();
             couplers = ReadCouplersFromSave(inf);
             Pantographs.Restore(inf);
-            LeftDoor.Restore(inf);
-            RightDoor.Restore(inf);
+            Doors.Restore(inf);
             PassengerCarPowerSupply?.Restore(inf);
             if (FreightAnimations != null)
             {
@@ -1965,8 +1965,7 @@ namespace Orts.Simulation.RollingStocks
 
             Pantographs.Update(elapsedClockSeconds);
 
-            LeftDoor.Update(elapsedClockSeconds);
-            RightDoor.Update(elapsedClockSeconds);
+            Doors.Update(elapsedClockSeconds);
 
             MSTSBrakeSystem.Update(elapsedClockSeconds);
 

Source/Orts.Simulation/RollingStocks/SubSystems/Door.cs
@@ -23,12 +23,83 @@ using Orts.Scripting.Api;
 
 namespace Orts.Simulation.RollingStocks.SubSystems
 {
+    public class Doors : ISubSystem<Doors>
+    {
+        public Door RightDoor { get; }
+        public Door LeftDoor { get; }
+
+        public Doors(MSTSWagon wagon)
+        {
+            LeftDoor = new Door(wagon, false);
+            RightDoor = new Door(wagon, true);
+        }
+
+        public virtual void Parse(string lowercasetoken, STFReader stf)
+        {
+            switch (lowercasetoken)
+            {
+                case "wagon(ortsdoors(closingdelay": 
+                {
+                    float delayS = stf.ReadFloatBlock(STFReader.Units.Time, 0f);
+                    LeftDoor.ClosingDelayS = delayS;
+                    RightDoor.ClosingDelayS = delayS;
+                    break;
+                }
+                case "wagon(ortsdoors(openingdelay": 
+                {
+                    float delayS = stf.ReadFloatBlock(STFReader.Units.Time, 0f);
+                    LeftDoor.OpeningDelayS = delayS;
+                    RightDoor.OpeningDelayS = delayS;
+                    break;
+                }
+            }
+        }
+
+        public void Copy(Doors source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            LeftDoor.Copy(source.LeftDoor);
+            RightDoor.Copy(source.RightDoor);
+        }
+
+        public virtual void Initialize()
+        {
+            LeftDoor.Initialize();
+            RightDoor.Initialize();
+        }
+
+        /// <summary>
+        /// Initialization when simulation starts with moving train
+        /// </summary>
+        public virtual void InitializeMoving()
+        {
+        }
+
+        public virtual void Save(BinaryWriter outf)
+        {
+            LeftDoor.Save(outf);
+            RightDoor.Save(outf);
+        }
+
+        public virtual void Restore(BinaryReader inf)
+        {
+            LeftDoor.Restore(inf);
+            RightDoor.Restore(inf);
+        }
+
+        public virtual void Update(double elapsedClockSeconds)
+        {
+            LeftDoor.Update(elapsedClockSeconds);
+            RightDoor.Update(elapsedClockSeconds);
+        }
+    }
     public class Door : ISubSystem<Door>
     {
         
         // Parameters
-        public float OpeningDelayS { get; protected set; }
-        public float ClosingDelayS { get; protected set; }
+        public float OpeningDelayS { get; set; }
+        public float ClosingDelayS { get; set; }
 
         // Variables
         private readonly MSTSWagon wagon;
@@ -50,16 +121,6 @@ namespace Orts.Simulation.RollingStocks.SubSystems
 
         public virtual void Parse(string lowercasetoken, STFReader stf)
         {
-            switch (lowercasetoken)
-            {
-                case "wagon(ortsdoors(closingdelay":
-                    ClosingDelayS = stf.ReadFloatBlock(STFReader.Units.Time, 0f);
-                    break;
-
-                case "engine(ortsdoors(openingdelay":
-                    OpeningDelayS = stf.ReadFloatBlock(STFReader.Units.Time, 0f);
-                    break;
-            }
         }
 
         public void Copy(Door source)