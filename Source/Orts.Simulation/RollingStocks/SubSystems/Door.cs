openrails/openrails

Reduce unnecessary operations

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Door.cs
@@ -183,22 +183,20 @@ namespace Orts.Simulation.RollingStocks.SubSystems
             switch(State)
             {
                 case DoorState.Opening:
-                    ClosingTimer.Stop();
                     if (!OpeningTimer.Started) OpeningTimer.Start();
-                    if (OpeningTimer.Triggered) State = DoorState.Open;
+                    if (OpeningTimer.Triggered)
+                    {
+                        State = DoorState.Open;
+                        OpeningTimer.Stop();
+                    }
                     break;
                 case DoorState.Closing:
-                    OpeningTimer.Stop();
                     if (!ClosingTimer.Started) ClosingTimer.Start();
-                    if (ClosingTimer.Triggered) State = DoorState.Closed;
-                    break;
-                case DoorState.Closed:
-                    ClosingTimer.Stop();
-                    OpeningTimer.Stop();
-                    break;
-                case DoorState.Open:
-                    ClosingTimer.Stop();
-                    OpeningTimer.Stop();
+                    if (ClosingTimer.Triggered)
+                    {
+                        State = DoorState.Closed;
+                        ClosingTimer.Stop();
+                    }
                     break;
             }
         }
@@ -215,6 +213,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
                 case DoorState.Closing:
                     if (!Locked && open)
                     {
+                        ClosingTimer.Stop();
                         State = DoorState.Opening;
                         Wagon.SignalEvent(Event.DoorOpen);
                         bool driverRightSide = (Side == DoorSide.Right) ^ Wagon.GetCabFlipped();
@@ -225,6 +224,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
                 case DoorState.Opening:
                     if (!open)
                     {
+                        OpeningTimer.Stop();
                         State = DoorState.Closing;
                         Wagon.SignalEvent(Event.DoorClose);
                         bool driverRightSide = (Side == DoorSide.Right) ^ Wagon.GetCabFlipped();

perpetualKid/FreeTrainSimulator

(cherry picked from commit e4af8015f5deb360101f6c07873552e79f98a051)

Source/Orts.Simulation/RollingStocks/SubSystems/Door.cs
@@ -174,26 +174,22 @@ namespace Orts.Simulation.RollingStocks.SubSystems
             switch (State)
             {
                 case DoorState.Opening:
-                    closingTimer.Stop();
                     if (!openingTimer.Started)
                         openingTimer.Start();
                     if (openingTimer.Triggered)
+                    {
                         State = DoorState.Open;
+                        openingTimer.Stop();
+                    }
                     break;
                 case DoorState.Closing:
-                    openingTimer.Stop();
                     if (!closingTimer.Started)
                         closingTimer.Start();
                     if (closingTimer.Triggered)
+                    {
                         State = DoorState.Closed;
-                    break;
-                case DoorState.Closed:
-                    closingTimer.Stop();
-                    openingTimer.Stop();
-                    break;
-                case DoorState.Open:
-                    closingTimer.Stop();
-                    openingTimer.Stop();
+                        closingTimer.Stop();
+                    }
                     break;
             }
         }
@@ -211,6 +207,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
                 case DoorState.Closing:
                     if (!Locked && open)
                     {
+                        closingTimer.Stop();
                         State = DoorState.Opening;
                         wagon.SignalEvent(TrainEvent.DoorOpen);
                         bool driverRightSide = (DoorSide == DoorSide.Right) ^ wagon.GetCabFlipped();
@@ -221,6 +218,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems
                 case DoorState.Opening:
                     if (!open)
                     {
+                        openingTimer.Stop();
                         State = DoorState.Closing;
                         wagon.SignalEvent(TrainEvent.DoorClose);
                         bool driverRightSide = (DoorSide == DoorSide.Right) ^ wagon.GetCabFlipped();