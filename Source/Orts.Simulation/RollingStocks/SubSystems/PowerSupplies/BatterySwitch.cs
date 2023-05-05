openrails/openrails

Added new parameter for battery switch

Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/PowerSupplies/BatterySwitch.cs
@@ -34,6 +34,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
         }
         public ModeType Mode { get; protected set; } = ModeType.AlwaysOn;
         public float DelayS { get; protected set; } = 0f;
+        public bool DefaultOn { get; protected set; } = false;
 
         // Variables
         readonly MSTSWagon Wagon;
@@ -80,6 +81,11 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
                 case "wagon(ortsbattery(delay":
                     DelayS = stf.ReadFloatBlock(STFReader.UNITS.Time, 0f);
                     break;
+
+                case "engine(ortsbattery(defaulton":
+                case "wagon(ortsbattery(defaulton":
+                    DefaultOn = stf.ReadBoolBlock(false);
+                    break;
             }
         }
 
@@ -87,10 +93,21 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
         {
             Mode = other.Mode;
             DelayS = other.DelayS;
+            DefaultOn = other.DefaultOn;
         }
 
         public virtual void Initialize()
         {
+
+            if (DefaultOn)
+            {
+                On = true;
+
+                if (Mode == ModeType.Switch)
+                {
+                    CommandSwitch = true;
+                }
+            }
         }
 
         /// <summary>
@@ -98,8 +115,12 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
         /// </summary>
         public virtual void InitializeMoving()
         {
-            CommandSwitch = true;
             On = true;
+
+            if (Mode == ModeType.Switch)
+            {
+                CommandSwitch = true;
+            }
         }
 
         public virtual void Save(BinaryWriter outf)

perpetualKid/FreeTrainSimulator

(cherry picked from commit b9a0ea302c6d3cb1912c100db02ae3c082c6407c)

Source/Orts.Simulation/RollingStocks/SubSystems/PowerSupplies/BatterySwitch.cs
@@ -35,6 +35,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
         }
         public ModeType Mode { get; protected set; } = ModeType.AlwaysOn;
         public float DelayS { get; protected set; }
+        public bool DefaultOn { get; protected set; }
 
         // Variables
         readonly MSTSWagon Wagon;
@@ -80,6 +81,11 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
                 case "wagon(ortsbattery(delay":
                     DelayS = stf.ReadFloatBlock(STFReader.Units.Time, 0f);
                     break;
+
+                case "engine(ortsbattery(defaulton":
+                case "wagon(ortsbattery(defaulton":
+                    DefaultOn = stf.ReadBoolBlock(false);
+                    break;
             }
         }
 
@@ -87,11 +93,22 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
         {
             Mode = source.Mode;
             DelayS = source.DelayS;
+            DefaultOn = source.DefaultOn;
         }
 
         public virtual void Initialize()
         {
             Timer.Setup(DelayS);
+
+            if (DefaultOn)
+            {
+                On = true;
+
+                if (Mode == ModeType.Switch)
+                {
+                    CommandSwitch = true;
+                }
+            }
         }
 
         /// <summary>
@@ -99,8 +116,12 @@ namespace Orts.Simulation.RollingStocks.SubSystems.PowerSupplies
         /// </summary>
         public virtual void InitializeMoving()
         {
-            CommandSwitch = true;
             On = true;
+
+            if (Mode == ModeType.Switch)
+            {
+                CommandSwitch = true;
+            }
         }
 
         public virtual void Save(BinaryWriter outf)