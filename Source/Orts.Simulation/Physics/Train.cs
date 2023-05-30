openrails/openrails

Remove forced processed ClearSection actions

Source/Orts.Simulation/Simulation/Physics/Train.cs
@@ -11213,19 +11213,17 @@ namespace Orts.Simulation.Physics
             LastReservedSection[0] = -1;
             LastReservedSection[1] = -1;
 
-            // clear outstanding clear sections
+            // clear outstanding clear sections and remove them from queue as they are no longer required
 
-            foreach (DistanceTravelledItem thisAction in requiredActions)
+            List<DistanceTravelledItem> activeActions = requiredActions.GetActions(99999999f, typeof(ClearSectionItem));
+            foreach (DistanceTravelledItem thisAction in activeActions)
             {
-                if (thisAction is ClearSectionItem)
-                {
-                    ClearSectionItem thisItem = thisAction as ClearSectionItem;
-                    TrackCircuitSection thisSection = signalRef.TrackCircuitList[thisItem.TrackSectionIndex];
-                    thisSection.ClearOccupied(this, true);
-                }
+                ClearSectionItem thisItem = thisAction as ClearSectionItem;
+                TrackCircuitSection thisSection = signalRef.TrackCircuitList[thisItem.TrackSectionIndex];
+                thisSection.ClearOccupied(this, true);
             }
         }
-
+    
         //================================================================================================//
         //
         // Update track actions after coupling

perpetualKid/FreeTrainSimulator

(cherry picked from commit 87c34a748706e6bf74356d3218aa1ca197e2f53b)

Source/Orts.Simulation/Physics/Train.cs
@@ -8676,9 +8676,10 @@ namespace Orts.Simulation.Physics
             LastReservedSection[0] = -1;
             LastReservedSection[1] = -1;
 
-            // clear outstanding clear sections
+            // clear outstanding clear sections and remove them from queue as they are no longer required
 
-            foreach (DistanceTravelledItem actionItem in RequiredActions)
+            List<DistanceTravelledItem> activeActions = RequiredActions.GetActions(99999999f, typeof(ClearSectionItem));
+            foreach (DistanceTravelledItem actionItem in activeActions)
             {
                 if (actionItem is ClearSectionItem clearSectionItem)
                 {