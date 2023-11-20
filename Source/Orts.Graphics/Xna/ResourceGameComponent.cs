61626fe12c34c89ef4f527c998f535f71862d08b
perpetualKid <perpetualKid@azuros.net>
2023-11-20 09:23:02 +0100

    cleanup packages

--- Source/Orts.Graphics/Xna/ResourceGameComponent.cs
+++ Source/Orts.Graphics/Xna/ResourceGameComponent.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Xna.Framework;
 
 namespace Orts.Graphics.Xna
