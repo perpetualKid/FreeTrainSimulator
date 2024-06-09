fad6156abb8544f537ae2d357015885fbf99a5d4
perpetualKid <perpetualKid@azuros.net>
2024-06-09 10:54:05 +0200

    Adding WaitPointDetails

---
+++ Source/Orts.Simulation/Track/WaitingPointDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Orts.Simulation.Track
+{
+    public class WaitingPointDetail
+    {
+        internal int[] Values { get; } = new int[6];
+        public int SubListIndex { get => Values[0]; set { Values[0] = value; } }
+        public int WaitingPointSection { get => Values[1]; set { Values[1] = value; } }
+        public int WaitTime { get => Values[2]; set { Values[2] = value; } }
+        public int DepartTime { get => Values[3]; set { Values[3] = value; } }
+        public int HoldSignal { get => Values[4]; set { Values[4] = value; } }
+        public int Offset { get => Values[5]; set { Values[5] = value; } }
+
+        public WaitingPointDetail() { }
+
+        public WaitingPointDetail(WaitingPointDetail source): this(source?.Values ?? throw new ArgumentNullException(nameof(source)))
+        {
+        }
+
+        public WaitingPointDetail(int[] source)
+        {
+            source.CopyTo(Values, 0);
+        }
+
+    }
+}
