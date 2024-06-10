97114ca1d21c61cd0f82cfc41363c6118baacdf4
perpetualKid <perpetualKid@azuros.net>
2024-06-10 08:46:45 +0200

    Update DeadlockInfo.cs

--- Source/Orts.Simulation/Signalling/DeadlockInfo.cs
+++ Source/Orts.Simulation/Signalling/DeadlockInfo.cs
@@ -28,7 +28,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
