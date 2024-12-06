49dc26e05b49f98dfbbcc3001be07a45d2771e5e
perpetualKid <perpetualKid@azuros.net>
2024-12-06 11:42:58 +0100

    fixing unit test

--- Source/FreeTrainSimulator.Common/Logging/LoggingUtil.cs
+++ Source/FreeTrainSimulator.Common/Logging/LoggingUtil.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
