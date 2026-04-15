8e3701d33924214a9684b42a3f9b0af220e0aff6
perpetualKid <perpetualKid@azuros.net>
2026-04-15 13:21:52 +0200

    Update ContentModel.cs

--- Source/FreeTrainSimulator.Models/Content/ContentModel.cs
+++ Source/FreeTrainSimulator.Models/Content/ContentModel.cs
@@ -20,7 +20,7 @@ namespace FreeTrainSimulator.Models.Content
         public override ModelBase Parent => null; // Content is root and does not implement a parent
 
         [MemoryPackIgnore]
-        public const string MinimumVersion = "2.0.1-dev.346";
+        public const string MinimumVersion = "2.0.1-dev.352";
 
         [MemoryPackIgnore]
         public static ContentModel None { get; } = default(ContentModel);
